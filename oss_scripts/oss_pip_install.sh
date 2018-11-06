964fa56193eeb63eb14ee4b8d434319a2464bce4
Ryan Sepassi <rsepassi@google.com>
2018-11-06 10:13:09 -0800

    Add --user flag to pip commands
    PiperOrigin-RevId: 220303949

--- oss_scripts/oss_pip_install.sh
+++ oss_scripts/oss_pip_install.sh
@@ -7,18 +7,18 @@ set -e  # fail and exit on any command erroring
 
 if [[ "$TF_VERSION" == "tf-nightly"  ]]
 then
-  pip install tf-nightly;
+  pip install --user tf-nightly;
 else
-  pip install -q "tensorflow==$TF_VERSION"
+  pip install -q --user "tensorflow==$TF_VERSION"
 fi
 
 # Make sure we have the latest version of numpy - avoid problems we were
 # seeing with Python 3
-pip install -q -U numpy
+pip install -q -U --user numpy
 
 # First ensure that the base dependencies are sufficient for a full import
-pip install -q -e .
+pip install --user -q -e .
 python -c "import tensorflow_datasets as tfds"
 
 # Then install the test dependencies
-pip install -q -e .[tests]
+pip install --user -q -e .[tests]
