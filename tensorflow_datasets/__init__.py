e2d52416899e15cad8a4e43564c6dc4bc7c11c3c
Ryan Sepassi <rsepassi@google.com>
2018-12-03 10:55:02 -0800

    Add TF version check to __init__.py
    PiperOrigin-RevId: 223823583

--- tensorflow_datasets/__init__.py
+++ tensorflow_datasets/__init__.py
@@ -21,6 +21,53 @@ with TensorFlow.
 Each dataset is defined as a `tfds.core.DatasetBuilder`.
 """
 
+
+# Copied from tensorflow/probability
+# Ensure TensorFlow is importable and its version is sufficiently recent. This
+# needs to happen before anything else, since the imports below will try to
+# import tensorflow, too.
+def _ensure_tf_install():  # pylint: disable=g-statement-before-imports
+  """Attempt to import tensorflow, and ensure its version is sufficient.
+
+  Raises:
+    ImportError: if either tensorflow is not importable or its version is
+    inadequate.
+  """
+  # pylint: disable=g-import-not-at-top
+  try:
+    import tensorflow as tf
+  except ImportError:
+    # Re-raise with more informative error message.
+    raise ImportError(
+        "Failed to import TensorFlow. Please note that TensorFlow is not "
+        "installed by default when you install TensorFlow Datasets. This is "
+        "so that users can decide whether to install the GPU-enabled "
+        "TensorFlow package. To use TensorFlow Datasets, please install the "
+        "most recent version of TensorFlow, by following instructions at "
+        "https://tensorflow.org/install. Usually `pip install tensorflow` "
+        "or `pip install tensorflow-gpu`.")
+
+  import distutils.version
+
+  #
+  # Update this whenever we need to depend on a newer TensorFlow release.
+  #
+  required_tensorflow_version = "1.13.0"
+
+  if (distutils.version.LooseVersion(tf.__version__) <
+      distutils.version.LooseVersion(required_tensorflow_version)):
+    raise ImportError(
+        "This version of TensorFlow Datasets requires TensorFlow "
+        "version >= {required}; Detected an installation of version {present}. "
+        "Please upgrade TensorFlow to proceed.".format(
+            required=required_tensorflow_version,
+            present=tf.__version__))
+  # pylint: enable=g-import-not-at-top
+
+
+_ensure_tf_install()
+
+
 # Imports for registration
 from tensorflow_datasets import image
 from tensorflow_datasets import text_sentiment
