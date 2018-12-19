faf8e41fe5c805a0a24b6a5324a05e4eb85c60b4
Ryan Sepassi <rsepassi@google.com>
2018-12-19 02:21:09 -0800

    Update usage block at top of datasets.md
    PiperOrigin-RevId: 226142264

--- tensorflow_datasets/scripts/document_datasets.py
+++ tensorflow_datasets/scripts/document_datasets.py
@@ -45,8 +45,12 @@ DOC = """\
 # See all registered datasets
 tfds.list_builders()
 
-# Load a given dataset by name
-mnist_train_dataset = tfds.load(name="mnist")
+# Load a given dataset by name, along with the DatasetInfo
+data, info = tfds.load("mnist", with_info=True)
+train_data, test_data = data['test'], data['train']
+assert isinstance(train_data, tf.data.Dataset)
+assert info.features['label'].num_classes == 10
+assert info.splits['train'].num_examples == 60000
 ```
 
 ---
