e41659e981d9cf2beb322f5a21c0f48c3d9e802f
Ryan Sepassi <rsepassi@google.com>
2018-12-18 19:10:56 -0800

    Don't compute stats if they're already there
    PiperOrigin-RevId: 226101300

--- tensorflow_datasets/core/dataset_builder.py
+++ tensorflow_datasets/core/dataset_builder.py
@@ -248,13 +248,19 @@ class DatasetBuilder(object):
 
         # Update the DatasetInfo metadata by computing statistics from the data.
         if compute_stats:
-          self.info.compute_dynamic_properties()
+          already_has_stats = bool(self.info.num_examples)
+          if already_has_stats:
+            tf.logging.info("Skipping computing stats because they are already "
+                            "populated.")
+          else:
+            self.info.compute_dynamic_properties()
 
-        # Set checksums for all files downloaded
-        self.info.download_checksums = dl_manager.recorded_download_checksums
-        # Set size of all files downloaded
-        self.info.size_in_bytes = sum(
-            [v for _, v in dl_manager.download_sizes.items()])
+            # Set checksums for all files downloaded
+            self.info.download_checksums = (
+                dl_manager.recorded_download_checksums)
+            # Set size of all files downloaded
+            self.info.size_in_bytes = sum(
+                [v for _, v in dl_manager.download_sizes.items()])
         # Write DatasetInfo to disk, even if we haven't computed the statistics.
         self.info.write_to_directory(self._data_dir)
 
