1d2eb792bfb31aa6016623b9e1b9cafab34f14da
TensorFlow Datasets Team <no-reply@google.com>
2019-01-07 10:20:38 -0800

    use packages@tensorflow.org as author email for python package
    PiperOrigin-RevId: 228188940

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(
     description=DOCLINES[0],
     long_description='\n'.join(DOCLINES[2:]),
     author='Google Inc.',
-    author_email='opensource@google.com',
+    author_email='packages@tensorflow.org',
     url='http://github.com/tensorflow/datasets',
     download_url='https://github.com/tensorflow/datasets/tags',
     license='Apache 2.0',
