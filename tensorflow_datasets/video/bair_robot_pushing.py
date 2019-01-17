c44733a60abe9d92bbee8ab0769898a4e06729ac
Marcin Michalski <michalski@google.com>
2019-01-17 02:17:05 -0800

    fix typo in number of objects in bair dataset.
    PiperOrigin-RevId: 229708567

--- tensorflow_datasets/video/bair_robot_pushing.py
+++ tensorflow_datasets/video/bair_robot_pushing.py
@@ -76,7 +76,7 @@ class BairRobotPushingSmall(tfds.core.GeneratorBasedBuilder):
 
     return tfds.core.DatasetInfo(
         builder=self,
-        description="This data set contains roughly 59,000 examples of robot "
+        description="This data set contains roughly 44,000 examples of robot "
         "pushing motions, including one training set (train) and "
         "two test sets of previously seen (testseen) and unseen "
         "(testnovel) objects. This is the small 64x64 version.",
