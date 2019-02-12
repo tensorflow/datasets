dfb0c003b99ae44afefe33d089d6afdbe5a656c1
Pierre Ruyssen <pierrot@google.com>
2019-02-12 06:27:08 -0800

    internal.
    PiperOrigin-RevId: 233595414

--- tensorflow_datasets/core/dataset_utils.py
+++ tensorflow_datasets/core/dataset_utils.py
@@ -19,6 +19,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+
 import tensorflow as tf
 from tensorflow_datasets.core import api_utils
 from tensorflow_datasets.core import tf_compat
