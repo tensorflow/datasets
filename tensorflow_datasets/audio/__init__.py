4b2c85b75729d124b4e2944f084e8b44f59be884
captain-pool <rickdey1998@gmail.com>
2019-03-02 22:26:46 +0530

    Commit 1 for Mozilla Common Voice #107

--- tensorflow_datasets/audio/__init__.py
+++ tensorflow_datasets/audio/__init__.py
@@ -18,3 +18,4 @@
 from tensorflow_datasets.audio.librispeech import Librispeech
 from tensorflow_datasets.audio.librispeech import LibrispeechConfig
 from tensorflow_datasets.audio.nsynth import Nsynth
+from tensorflow_datasets.audio.commonvoice import commonvoice
--- tensorflow_datasets/audio/commonvoice.py
+++ tensorflow_datasets/audio/commonvoice.py
@@ -1,22 +1,47 @@
 import tensorflow as tf
 import numpy as np
 import tensorflow_datasets.public_api as tfds
-
-_DOWNLOAD_PATH = {"en":"https://voice-prod-bundler-ee1969a6ce8178826482b88e843c335139bd3fb4.s3.amazonaws.com/cv-corpus-1/en.tar.gz"}
+import tensorflow as tf
+_DOWNLOAD_LINK = {"en":"https://voice-prod-bundler-ee1969a6ce8178826482b88e843c335139bd3fb4.s3.amazonaws.com/cv-corpus-1/en.tar.gz"}
+_SPLITS = {"train":"train","test":"test","validate":"validated"}
 class commonvoice(tfds.core.GeneratorBasedBuilder):
+    VERSION = tfds.core.Version("1.0.0")
     def _info(self):
        return tfds.core.DatasetInfo(
-       name = "commonvoice",
-       version = "1.0.0",
        description = "Mozilla Common Voice Dataset (English)",
        builder = self,
        urls = [u"https://voice.mozilla.org/en/datasets"]
        )
     def _split_generators(self,dl_manager):
-        dl_path = dl_manager.download_and_extract(_DOWNLOAD_PATH)
-        clip_folder = os.path.join(dl_path,"clips") # Need to Check for replacement
-
-        pass
-    def _generate_examples(self):
-        pass
-
+        dl_path = dl_manager.download_and_extract(_DOWNLOAD_LINK)
+        clip_folder = os.path.join(dl_path["en"],"clips") # Need to Check for replacement
+        return [tfds.core.SplitGenerator(
+        name = k,
+        num_shards = 40,
+        gen_kwargs = {
+        "image_path":clip_folder,
+        "label_path":os.path.join(dl_path["en"],"%s.tsv"%v)
+        }) for k,v in _SPLITS]
+    def _generate_examples(self,image_path,label_path):
+        def decode_tsv(line):
+            record_defaults = [""]*8
+            data = tf.decode_csv(line,record_defaults,field_delim = "\t")
+            return data
+        def convert(byte,type_):
+            assert callable(type_)
+            v = byte.decode("utf-8")
+            return type_(v) if len(v)>0 else 0
+        ds = tfds.as_numpy(tf.data.TextLineDataset(label_path).skip(1).map(decode_tsv))
+        ffmpeg = tfds.features.Audio(file_format = "mp3")
+        with tf.Graph().as_default():
+            for id_,path,sentence,upvotes,downvotes,age,gender,accent in ds:
+                wave = ffmpeg.encode_example(os.path.join(image_path,"%s.mp3"%path.decode("utf-8")))
+                yield {
+                "client_id":id_.decode("utf-8"),
+                "voice": wave,
+                "sentence":sentence.decode("utf-8"),
+                "upvotes": convert(upvotes,int),
+                "downvotes":convert(downvotes,int),
+                "age":convert(age,float),
+                "gender":gender.decode("utf-8"),
+                "accent":accent.decode("utf-8")}
