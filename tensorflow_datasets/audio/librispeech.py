29510e2edffb7ddefa261268f57484a5af39eaa3
K Yasaswi Sri Chandra Gandhi <yasaswisrichandragandhi@gmail.com>
2019-03-03 17:46:13 +0530

    Cleaned up description

--- tensorflow_datasets/audio/librispeech.py
+++ tensorflow_datasets/audio/librispeech.py
@@ -39,7 +39,9 @@ _CITATION = """\
 """
 
 _DESCRIPTION = """\
-LibriSpeech is a corpus of approximately 1000 hours of 16kHz read English speech, prepared by Vassil Panayotov with the assistance of Daniel Povey. The data is derived from read audiobooks from the LibriVox project, and has been carefully segmented and aligned.
+LibriSpeech is a corpus of approximately 1000 hours of read English speech of frequency 16 KHz, prepared by Vassil 
+Panayotov with the assistance of Daniel Povey. The data is derived from read audiobooks from the LibriVox
+project, and has been carefully segmented and aligned.
 """
 
 _URL = "http://www.openslr.org/12"
