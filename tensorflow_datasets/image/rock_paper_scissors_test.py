d19649cee51eb05c541fd586bcc07732c4510fed
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2019-03-03 10:52:52 +0530

    Update rock_paper_scissors_test.py

--- tensorflow_datasets/image/rock_paper_scissors_test.py
+++ tensorflow_datasets/image/rock_paper_scissors_test.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for rocks, papers, scissors data loading."""
+"""Tests for rocks or papers or scissors data loading."""
 
 from __future__ import absolute_import
 from __future__ import division
