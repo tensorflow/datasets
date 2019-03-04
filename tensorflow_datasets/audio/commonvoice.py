b21b56d4aef381d25c93d2f98819d955261295ef
captain-pool <rickdey1998@gmail.com>
2019-03-04 06:37:43 +0000

    Added tfds.features.FeatureDict to _info() and fixed _generate_examples #107

--- tensorflow_datasets/audio/commonvoice.py
+++ tensorflow_datasets/audio/commonvoice.py
@@ -4,17 +4,26 @@ import tensorflow_datasets.public_api as tfds
 import os
 import tensorflow as tf
 _DOWNLOAD_LINK = {"en":"https://voice-prod-bundler-ee1969a6ce8178826482b88e843c335139bd3fb4.s3.amazonaws.com/cv-corpus-1/en.tar.gz"}
-_SPLITS = {"train":"train","test":"test","validate":"validated"}
+_SPLITS = {tfds.Split.TRAIN:"train",tfds.Split.TEST:"test",tfds.Split.VALIDATION:"validated"}
 class commonvoice(tfds.core.GeneratorBasedBuilder):
     VERSION = tfds.core.Version("1.0.0")
     def _info(self):
        return tfds.core.DatasetInfo(
-       description = "Mozilla Common Voice Dataset (English)",
+       description = ("Mozilla Common Voice Dataset (English)"),
        builder = self,
+       features = tfds.features.FeaturesDict({
+       "client_id":tfds.features.Text(),
+       "upvotes":tf.int32,
+       "downvotes":tf.int32,
+       "age":tfds.features.Text(),
+       "gender":tfds.features.Text(),
+       "accent":tfds.features.Text(),
+       "sentence":tfds.features.Text(),
+       "voice":tfds.features.Tensor(shape = (None,),dtype = tf.float32)
+       }),
        urls = [u"https://voice.mozilla.org/en/datasets"]
        )
     def _split_generators(self,dl_manager):
-        dl_manager.force_download = False
         dl_path = dl_manager.extract(dl_manager.download(_DOWNLOAD_LINK))
         clip_folder = os.path.join(dl_path["en"],"clips") # Need to Check for replacement
         return [tfds.core.SplitGenerator(
@@ -35,15 +44,14 @@ class commonvoice(tfds.core.GeneratorBasedBuilder):
             return type_(v) if len(v)>0 else 0
         ds = tfds.as_numpy(tf.data.TextLineDataset(label_path).skip(1).map(decode_tsv))
         ffmpeg = tfds.features.Audio(file_format = "mp3")
-        with tf.Graph().as_default():
-            for id_,path,sentence,upvotes,downvotes,age,gender,accent in ds:
-                wave = ffmpeg.encode_example(os.path.join(image_path,"%s.mp3"%path.decode("utf-8")))
-                yield {
-                "client_id":id_.decode("utf-8"),
-                "voice": wave,
-                "sentence":sentence.decode("utf-8"),
-                "upvotes": convert(upvotes,int),
-                "downvotes":convert(downvotes,int),
-                "age":convert(age,float),
-                "gender":gender.decode("utf-8"),
-                "accent":accent.decode("utf-8")}
+        for id_,path,sentence,upvotes,downvotes,age,gender,accent in ds:
+            wave = ffmpeg.encode_example(os.path.join(image_path,"%s.mp3"%path.decode("utf-8"))).astype("float32")
+            yield {
+            "client_id":id_.decode("utf-8"),
+            "voice": wave,
+            "sentence":sentence.decode("utf-8"),
+            "upvotes": convert(upvotes,int),
+            "downvotes":convert(downvotes,int),
+            "age":age.decode("utf-8"),
+            "gender":gender.decode("utf-8"),
+            "accent":accent.decode("utf-8")}
