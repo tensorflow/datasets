02ccf29b446caebc140943d7fbb9f7b274861470
captain-pool <rickdey1998@gmail.com>
2019-03-05 19:24:10 +0530

    Added tf.gfile.GFile with csv.DictReader #107

--- tensorflow_datasets/audio/commonvoice.py
+++ tensorflow_datasets/audio/commonvoice.py
@@ -53,19 +53,19 @@ class CommonVoice(tfds.core.GeneratorBasedBuilder):
             Generate Voice Samples and Statements Given the Path to the Shared Audio Folder
             and Path to the Train/Test/Validation File (.tsv)
         """
-        file_ = tf.gfile.GFile(label_path)
-        dataset = csv.DictReader(file_, delimiter="\t")
-        ffmpeg = tfds.features.Audio(file_format="mp3")
-        for row in dataset:
-            wave = ffmpeg.encode_example(
-                os.path.join(
-                    audio_path, "%s.mp3" % row["path"]))
-            yield {
-                "client_id": row["client_id"],
-                "voice": wave,
-                "sentence": row["sentence"],
-                "upvotes": int(row["up_votes"]) if len(row["up_votes"]) > 0 else 0,
-                "downvotes": int(row["down_votes"]) if len(row["down_votes"]) > 0 else 0,
-                "age": row["age"],
-                "gender": row["gender"],
-                "accent": row["accent"]}
+        with tf.gfile.GFile(label_path) as file_:
+            dataset = csv.DictReader(file_, delimiter="\t")
+            ffmpeg = tfds.features.Audio(file_format="mp3")
+            for row in dataset:
+                wave = ffmpeg.encode_example(
+                    os.path.join(
+                        audio_path, "%s.mp3" % row["path"]))
+                yield {
+                    "client_id": row["client_id"],
+                    "voice": wave,
+                    "sentence": row["sentence"],
+                    "upvotes": int(row["up_votes"]) if len(row["up_votes"]) > 0 else 0,
+                    "downvotes": int(row["down_votes"]) if len(row["down_votes"]) > 0 else 0,
+                    "age": row["age"],
+                    "gender": row["gender"],
+                    "accent": row["accent"]}
