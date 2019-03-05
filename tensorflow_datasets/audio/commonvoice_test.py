ffd516971ed9f8b25f571c010badf0eedd1f52f2
captain-pool <rickdey1998@gmail.com>
2019-03-05 23:02:42 +0530

    Linted Code according to PEP8 Specification #107

--- tensorflow_datasets/audio/commonvoice_test.py
+++ tensorflow_datasets/audio/commonvoice_test.py
@@ -10,7 +10,9 @@ class CommonVoiceTest(tfds_test.DatasetBuilderTestCase):
     CommonVoice Tester Class
     """
     DATASET_CLASS = commonvoice.CommonVoice
-    SPLITS = {"train":10, "test":10,"validation":10}
-    DL_EXTRACT_RESULT = {"en":"."}
+    SPLITS = {"train": 10, "test": 10, "validation": 10}
+    DL_EXTRACT_RESULT = {"en": "."}
+
+
 if __name__ == "__main__":
     tfds_test.test_main()
