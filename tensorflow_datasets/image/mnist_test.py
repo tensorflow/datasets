976bb826994b05333322d4265b71a7a76ac316c3
us <rahmetsaritekin@gmail.com>
2019-03-09 05:54:53 +0300

    Added EMNIST Test

--- tensorflow_datasets/image/mnist_test.py
+++ tensorflow_datasets/image/mnist_test.py
@@ -50,5 +50,10 @@ class KMNISTTest(MNISTTest):
     DATASET_CLASS = mnist.KMNIST
 
 
+class EMNISTTest(MNISTTest):
+    DATASET_CLASS = mnist.EMNIST
+    BUILDER_CONFIG_NAMES_TO_TEST = ["test"]
+
+
 if __name__ == "__main__":
   testing.test_main()
