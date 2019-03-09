7d9d2d5e6404902e5ba4b81fc92234202250d454
Soumyadeep Jana <sjana646@gmail.com>
2019-03-09 10:32:36 +0530

    Fixed ham10000_test.py

--- tensorflow_datasets/image/ham10000_test.py
+++ tensorflow_datasets/image/ham10000_test.py
@@ -1,9 +1,13 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import tensorflow as tf
 from tensorflow_datasets.image import ham10000
 import tensorflow_datasets.testing as tfds_test
 
 
-class MyDatasetTest(tfds_test.DatasetBuilderTestCase):
+class Ham10000Test(tfds_test.DatasetBuilderTestCase):
   DATASET_CLASS = ham10000.ham10000
   SPLITS = {  
       "train": 2,
