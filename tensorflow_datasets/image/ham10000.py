8b04d64fa47751a96196f84d3e14363e91ee834a
Soumyadeep Jana <sjana646@gmail.com>
2019-03-11 20:18:35 +0530

    Implemented changes for HAM10000 dataset

--- tensorflow_datasets/image/ham10000.py
+++ tensorflow_datasets/image/ham10000.py
@@ -1,29 +1,33 @@
+"""HAM10000 Dataset"""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow as tf
-import tensorflow_datasets.public_api as tfds
 import os
-import numpy as np
 import csv
 
+import tensorflow as tf
+import tensorflow_datasets.public_api as tfds
+
+_URL = ("https://www.kaggle.com/kmader/skin-cancer-mnist-ham10000/"
+"downloads/skin-cancer-mnist-ham10000.zip/2")
+
+_DESCRIPTION = ("The dataset consists of 10015 dermatoscopic images which"
+"can serve as a training set for academic machine learning purposes."
+"Cases include a representative collection of all important diagnostic "
+"categories in the realm of pigmented lesions: Actinic keratoses and " 
+"intraepithelial carcinoma / Bowen's disease (akiec), basal cell carcinoma (bcc), "
+"benign keratosis-like lesions (solar lentigines / seborrheic "
+"keratoses and lichen-planus like keratoses, bkl), dermatofibroma (df), "
+"melanoma (mel), melanocytic nevi (nv) and vascular lesions "
+"(angiomas, angiokeratomas, pyogenic granulomas and hemorrhage, vasc)")
 
 
-_URL = "https://www.kaggle.com/kmader/skin-cancer-mnist-ham10000/downloads/skin-cancer-mnist-ham10000.zip/2"
 
-_DESCRIPTION = ("The dataset consists of 10015 dermatoscopic images which can serve as a training set for academic machine learning purposes."
-                "Cases include a representative collection of all important diagnostic categories in the realm of pigmented lesions: Actinic keratoses and " 
-                "intraepithelial carcinoma / Bowen's disease (akiec), basal cell carcinoma (bcc), benign keratosis-like lesions (solar lentigines / seborrheic "
-                "keratoses and lichen-planus like keratoses, bkl), dermatofibroma (df), melanoma (mel), melanocytic nevi (nv) and vascular lesions "
-                "(angiomas, angiokeratomas, pyogenic granulomas and hemorrhage, vasc)")
+_IMAGE_SHAPE = (450, 600, 3)
 
-
-
-_IMAGE_SHAPE = (450,600,3)
-
-_NAMES = ['akiec','bcc','bkl','df','mel','nv','vasc'] 
-
+_NAMES = ['akiec', 'bcc', 'bkl', 'df', 'mel', 'nv', 'vasc'] 
 
 _CITATION = """\
 @data{DVN/DBW86T_2018,
@@ -39,71 +43,76 @@ url = {https://doi.org/10.7910/DVN/DBW86T}
 """
 
 class Ham10000(tfds.core.GeneratorBasedBuilder):
-  """HAM10000 Dermatoscopic images"""
+    """HAM10000 Dermatoscopic images"""
 
-  VERSION = tfds.core.Version('1.0.0')
+    VERSION = tfds.core.Version('1.0.0')
 
 
-  def _info(self):
-     return tfds.core.DatasetInfo(
-        builder=self,
-  
-        description=(_DESCRIPTION),
-        
-        features=tfds.features.FeaturesDict({
-            "image":tfds.features.Image(shape=_IMAGE_SHAPE),
-            "label": tfds.features.ClassLabel(names = _NAMES),
-        }),
-
-       
-        
-        supervised_keys=("image", "label"),
-        
-        urls=[_URL],
-        
-        citation=_CITATION
-     )
-
-  def _split_generators(self, dl_manager):
-    # TODO: implement download using kaggle API.
+    def _info(self):
+        return tfds.core.DatasetInfo(
+            builder=self,
     
-    path = dl_manager.manual_dir
-    return [
-        tfds.core.SplitGenerator(
-            name="train",
-            num_shards=10,
-            gen_kwargs={
-                "images_dir_path": path,
-                "labels_dir_path":os.path.join(path,"HAM10000_metadata.csv")
-            },
+            description=(_DESCRIPTION),
+            
+            features=tfds.features.FeaturesDict({
+                "image":tfds.features.Image(shape=_IMAGE_SHAPE),
+                "label": tfds.features.ClassLabel(names=_NAMES),
+            }),
+
+            supervised_keys=("image", "label"),
+            
+            urls=[_URL],
+            
+            citation=_CITATION
         )
-       
-    ]
 
-  def _generate_examples(self,images_dir_path,labels_dir_path):
-
-     data_folder_list = [os.path.join(images_dir_path,'HAM10000_images_part_1'),os.path.join(images_dir_path,'HAM10000_images_part_2')]
-     for data_folder in data_folder_list:
-        for file in tf.io.gfile.listdir(data_folder):
-                
+    def _split_generators(self, dl_manager):
+        """Function to split the images into training set as no other set is specified"""
         
-                    image_path = os.path.join(data_folder,file)
-                    label = return_label(images_dir_path,file.split(".")[0],labels_dir_path)
-                   
-                    yield {
-                    
-                            "label": label,
-                            "image": image_path
-                        }
+        path = dl_manager.manual_dir
+        return [
+            tfds.core.SplitGenerator(
+                name="train",
+                num_shards=10,
+                gen_kwargs={
+                    "images_dir_path": path,
+                    "labels_dir_path":os.path.join(path, "HAM10000_metadata.csv")
+                },
+            )
+        
+        ]
 
-                      
-def return_label(path,image_name,labels_dir_path):
-    
+    def _generate_examples(self, images_dir_path, labels_dir_path):
+        """ Function to extract images and labels"""
+
+        csv_file = readCsv(labels_dir_path)
+        
+        data_folder_list = [os.path.join(images_dir_path, 'HAM10000_images_part_1'), os.path.join(images_dir_path, 'HAM10000_images_part_2')]
+        for data_folder in data_folder_list:
+            for image_file in tf.io.gfile.listdir(data_folder):
+                        
+                
+                image_path = os.path.join(data_folder, image_file)
+                label = return_label(image_file.split(".")[0], csv_file)
+                
+                yield {
+                
+                    "label": label,
+                    "image": image_path
+                    }
+
+
+def readCsv(labels_dir_path):
+    """Function to read labels.csv file and store in memory"""
+
     with tf.io.gfile.GFile(labels_dir_path) as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
-        for row in readCSV:
-            if row[1] == image_name:
-                return row[2]
+    return readCSV
 
+def return_label(image_name, csv_file):
+    """Function to return the corresponding label from filename"""
 
-
+    for row in csv_file:
+        if row[1] == image_name:
+            return row[2]
+                
--- tensorflow_datasets/image/ham10000_test.py
+++ tensorflow_datasets/image/ham10000_test.py
@@ -1,3 +1,5 @@
+"""Test for HAM10000 dataset"""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -8,11 +10,13 @@ import tensorflow_datasets.testing as tfds_test
 
 
 class Ham10000Test(tfds_test.DatasetBuilderTestCase):
-  DATASET_CLASS = ham10000.Ham10000
-  SPLITS = {  
-      "train": 2,
-  }
+    """HAM10000 Test Class"""
+    DATASET_CLASS = ham10000.Ham10000
+    SPLITS = {  
+        "train": 2,
+    }
  
 
 if __name__ == "__main__":
-  tfds_test.test_main()
+    tfds_test.test_main()
+    
