04b8abde8e44dd6f1ccd9bbec55d0c03e89dfd49
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2019-03-12 18:25:46 +0530

    Update librispeech_test.py

--- tensorflow_datasets/audio/librispeech_test.py
+++ tensorflow_datasets/audio/librispeech_test.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for lm1b dataset module."""
+"""Tests for librispeech dataset module."""
 
 from __future__ import absolute_import
 from __future__ import division
