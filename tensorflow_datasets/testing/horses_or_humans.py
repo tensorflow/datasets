74926a70e4765e62ad09fd480b3ecd94283f5eaf
Parth Shandilya <parth1989shandilya@gmail.com>
2019-03-14 02:58:46 +0530

    Fixed Typo in Horses Humans Script

--- tensorflow_datasets/testing/horses_or_humans.py
+++ tensorflow_datasets/testing/horses_or_humans.py
@@ -13,9 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-r"""Generate fake data for horses or humans dataset.
-
-"""
+r"""Generate fake data for horses or humans dataset."""
 
 from __future__ import absolute_import
 from __future__ import division
