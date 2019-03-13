ad590154c8f070f9d05bf8828b63e818e3b1ae1a
captain-pool <rickdey1998@gmail.com>
2019-03-13 13:49:09 +0530

    Changed Indentations to 2 Space, Linted Code according to PEP8 Specs

--- tensorflow_datasets/image/stanford_online_products_test.py
+++ tensorflow_datasets/image/stanford_online_products_test.py
@@ -3,9 +3,9 @@ from tensorflow_datasets.image import stanford_online_products
 
 
 class StanfordOnlineProductsTest(tfds_test.DatasetBuilderTestCase):
-    DATASET_CLASS = stanford_online_products.StanfordOnlineProducts
-    SPLITS = {"train": 3, "test": 3}
+  DATASET_CLASS = stanford_online_products.StanfordOnlineProducts
+  SPLITS = {"train": 3, "test": 3}
 
 
 if __name__ == "__main__":
-    tfds_test.test_main()
+  tfds_test.test_main()
