2168f99b2631147b1bd45f781444f171166b5aa6
captain-pool <rickdey1998@gmail.com>
2019-03-13 12:58:33 +0530

    Added Stanford Online Products

--- tensorflow_datasets/image/stanford_online_products.py
+++ tensorflow_datasets/image/stanford_online_products.py
@@ -1,27 +1,49 @@
 import csv
 import os
+import tensorflow as tf
 import tensorflow_datasets.public_api as tfds
-_DOWNLOAD_LINK = {
-    "folder_path": "ftp://cs.stanford.edu/cs/cvgl/Stanford_Online_Products.zip"}
+_DOWNLOAD_LINK = "ftp://cs.stanford.edu/cs/cvgl/Stanford_Online_Products.zip"
 _SPLITS = {
-        tfds.Split.TRAIN: "Ebay_train",
-        tfds.Split.TEST: "Ebay_test"}
+    tfds.Split.TRAIN: "Ebay_train",
+    tfds.Split.TEST: "Ebay_test"}
 
 
 class StanfordOnlineProducts(tfds.core.GeneratorBasedBuilder):
-  VERSION = tfds.core.Version("1.0.0")
+    VERSION = tfds.core.Version("1.0.0")
 
-  def_info(self):
-    return tfds.core.DatasetInfo(
-    description=("Stanford Online Products Dataset"),
-    builder=self,
-    urls=["http://cvgl.stanford.edu/projects/lifted_struct/"],
-    features=tfds.features.FeatureDict({
+    def _info(self):
+        return tfds.core.DatasetInfo(
+            description=("Stanford Online Products Dataset"),
+            builder=self,
+            urls=["http://cvgl.stanford.edu/projects/lifted_struct/"],
+            features=tfds.features.FeaturesDict({
+                "class_id": tfds.features.ClassLabel(num_classes=22364),
+                "super_class_id": tfds.features.ClassLabel(num_classes=12),
+                "image": tfds.features.Image()
+            })
+        )
 
-    })
-    )
+    def _split_generators(self, dl_manager):
+        dl_path = dl_manager.download_and_extract(_DOWNLOAD_LINK)
+        folder_path = os.path.join(
+            dl_path,
+            "Stanford_Online_Products")
+        return [
+            tfds.core.SplitGenerator(
+                name=k,
+                num_shards=4,
+                gen_kwargs={
+                    "file_path": os.path.join(
+                        folder_path,
+                        "%s.txt" %
+                        v)}) for k,
+            v in _SPLITS.items()]
 
-  def _split_generators(self, dl_manager):
-    path = dl_manager.download_and_extract(_DOWNLOAD_LINK)
-    path = os.path.join(path["folder_path"], "Stanford_Online_Products")
-    return [tfds.core.SplitGenerator(name=k, num_shards=40, gen_kwargs={"file_path": os.path.join(path, v)} for k, v in _SPLITS.items()]
+    def _generate_examples(self, file_path):
+        with tf.io.gfile.GFile(file_path, "r") as file_:
+            dataset = csv.DictReader(file_, delimiter=" ")
+            for row in dataset:
+                yield{
+                    "class_id": int(row["class_id"]),
+                    "super_class_id": int(row["super_class_id"]),
+                    "image": os.path.join(os.path.dirname(file_path), row["path"])}
---
+++ tensorflow_datasets/image/stanford_online_products_test.py
@@ -0,0 +1,11 @@
+import tensorflow_datasets.testing as tfds_test
+from tensorflow_datasets.image import stanford_online_products
+
+
+class StanfordOnlineProductsTest(tfds_test.DatasetBuilderTestCase):
+    DATASET_CLASS = stanford_online_products.StanfordOnlineProducts
+    SPLITS = {"train": 3, "test": 3}
+
+
+if __name__ == "__main__":
+    tfds_test.test_main()
