04bf65fa025902f92cd80b87f95c276e32487af0
Soumyadeep Jana <sjana646@gmail.com>
2019-03-14 11:49:01 +0530

    Fix in test file for Binary Alpha Digit Dataset Issue-189

--- tensorflow_datasets/image/binary_alpha_digits_test.py
+++ tensorflow_datasets/image/binary_alpha_digits_test.py
@@ -13,7 +13,7 @@ class BinaryAlphaDigitsTest(tfds_test.DatasetBuilderTestCase):
       }
 
     DL_EXTRACT_RESULT = {
-        "train": "binaryalphadigs.mat",
+        "train": tf.compat.as_text("binaryalphadigs.mat"),
       }
  
 
