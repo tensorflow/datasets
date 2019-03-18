fe95ff453ca2acb057df2a3aa1f3c10e08ad71b2
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2019-03-19 00:43:47 +0530

    added version.py

---
+++ tensorflow_datasets/version.py
@@ -0,0 +1,36 @@
+# Copyright 2018 The TensorFlow Probability Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""Define TensorFlow Datasets version information."""
+
+# We follow Semantic Versioning (https://semver.org/)
+_MAJOR_VERSION = '1'
+_MINOR_VERSION = '0'
+_PATCH_VERSION = '1'
+
+# When building releases, we can update this value on the release branch to
+# reflect the current release candidate ('rc0', 'rc1') or, finally, the official
+# stable release (indicated by `_VERSION_SUFFIX = ''`). Outside the context of a
+# release branch, the current version is by default assumed to be a
+# 'development' version, labeled 'dev'.
+_VERSION_SUFFIX = 'dev'
+
+# Example, '0.4.0-dev'
+__version__ = '.'.join([
+    _MAJOR_VERSION,
+    _MINOR_VERSION,
+    _PATCH_VERSION,
+])
+if _VERSION_SUFFIX:
+  __version__ = '{}-{}'.format(__version__, _VERSION_SUFFIX)
