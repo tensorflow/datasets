d8133b83eafa3e4c060a63c2a07049b2dbe4c6b6
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2019-03-18 10:09:32 +0530

    update __init__.py

--- tensorflow_datasets/structured/__init__.py
+++ tensorflow_datasets/structured/__init__.py
@@ -16,3 +16,4 @@
 """Structured datasets."""
 
 from tensorflow_datasets.structured.titanic import Titanic
+from tensorflow_datasets.structured.amazon_multilingual_reviews import AmazonMultilingualReviews
