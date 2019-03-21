71c8ddd88f43d45fc3891a3811f357657069a7f8
Soumyadeep Jana <sjana646@gmail.com>
2019-03-21 13:04:12 +0530

    Added dataset for Places365 small --issue-288

--- tensorflow_datasets/image/places365_small.py
+++ tensorflow_datasets/image/places365_small.py
@@ -20,7 +20,7 @@ _IMAGE_SHAPE = (256,256,3)
 _DESCRIPTION = ("The Places365-Standard dataset contains 1.8 million train images from 365 scene categories,which are used to train the Places365 CNNs."
 "There are 50 images per category in the validation set and 900 images per category in the testing set.")
 
-_LABELS_FNAME = 'categories_places365.txt'
+_LABELS_FNAME = 'image/categories_places365.txt'
 
 _CITATION = """\
 
