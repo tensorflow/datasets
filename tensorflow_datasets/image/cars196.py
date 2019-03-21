247803393c4c3ac430a539086c8824ef542327ea
Soumyadeep Jana <sjana646@gmail.com>
2019-03-21 08:13:47 +0530

    Pylinted

--- tensorflow_datasets/image/cars196.py
+++ tensorflow_datasets/image/cars196.py
@@ -1,13 +1,12 @@
+"""Dataset class for Cars196 Dataset"""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 import six.moves.urllib as urllib
+import os
 import tensorflow as tf
 import tensorflow_datasets.public_api as tfds
-import os
-import numpy as np
-
-
 
 
 _URL = "http://imagenet.stanford.edu/internal/car196/"
@@ -63,7 +62,7 @@ _NAMES = ['AM General Hummer SUV 2000', 'Acura RL Sedan 2012', 'Acura TL Sedan 2
  'Spyker C8 Convertible 2009', 'Spyker C8 Coupe 2009', 'Suzuki Aerio Sedan 2007', 'Suzuki Kizashi Sedan 2012', 'Suzuki SX4 Hatchback 2012',
  'Suzuki SX4 Sedan 2012', 'Tesla Model S Sedan 2012', 'Toyota Sequoia SUV 2012', 'Toyota Camry Sedan 2012', 'Toyota Corolla Sedan 2012',
  'Toyota 4Runner SUV 2012', 'Volkswagen Golf Hatchback 2012', 'Volkswagen Golf Hatchback 1991', 'Volkswagen Beetle Hatchback 2012',
- 'Volvo C30 Hatchback 2012', 'Volvo 240 Sedan 1993', 'Volvo XC90 SUV 2007', 'smart fortwo Convertible 2012','test']
+ 'Volvo C30 Hatchback 2012', 'Volvo 240 Sedan 1993', 'Volvo XC90 SUV 2007', 'smart fortwo Convertible 2012', 'test']
 
 
 _CITATION = """\
@@ -79,85 +78,88 @@ _CITATION = """\
 """
 
 class Cars196(tfds.core.GeneratorBasedBuilder):
-  """Car Images dataset"""
+    """Car Images dataset"""
 
-  VERSION = tfds.core.Version('1.0.0')
+    VERSION = tfds.core.Version('1.0.0')
 
 
-  def _info(self):
-     return tfds.core.DatasetInfo(
-        builder=self,
-  
-        description=(_DESCRIPTION),
-        
-        features=tfds.features.FeaturesDict({
-            "image":tfds.features.Image(),
-            "label": tfds.features.ClassLabel(names = _NAMES),
-            "bbox":tfds.features.BBoxFeature(),
-        }),
+    def _info(self):
+        """Define the dataset info"""
 
-       
-        
-        supervised_keys=("image", "label"),
-        
-        urls=[_URL],
-        
-        citation=_CITATION
-     )
-
-  def _split_generators(self, dl_manager):
+        return tfds.core.DatasetInfo(
+            builder=self,
     
-    output_files = dl_manager.download_and_extract({
-        "train":urllib.parse.urljoin(_URL, "cars_train.tgz"),
-        "test":urllib.parse.urljoin(_URL, "cars_test.tgz"),
-        "extra":_EXTRA_URL,
-    })
+            description=(_DESCRIPTION),
+            
+            features=tfds.features.FeaturesDict({
+                "image":tfds.features.Image(),
+                "label": tfds.features.ClassLabel(names=_NAMES),
+                "bbox":tfds.features.BBoxFeature(),
+            }),
 
-    return [
-        tfds.core.SplitGenerator(
-            name="train",
-            num_shards=10,
-            gen_kwargs={
-                "data_dir_path": os.path.join(output_files['train'],'cars_train'),
-                "data_annotations_path":os.path.join(output_files['extra'], os.path.join('devkit','cars_train_annos.mat')),
-            },
-        ),
+        
+            
+            supervised_keys=("image", "label"),
+            
+            urls=[_URL],
+            
+            citation=_CITATION
+        )
 
-        tfds.core.SplitGenerator(
-            name="test",
-            num_shards=10,
-            gen_kwargs={
-                "data_dir_path": os.path.join(output_files['test'],'cars_test'),
-                "data_annotations_path":os.path.join(output_files['extra'], os.path.join('devkit','cars_test_annos.mat')),
-            },
-        ),
-       
-    ]
+    def _split_generators(self, dl_manager):
+        """ Define the train and test split"""
+        output_files = dl_manager.download_and_extract({
+            "train":urllib.parse.urljoin(_URL, "cars_train.tgz"),
+            "test":urllib.parse.urljoin(_URL, "cars_test.tgz"),
+            "extra":_EXTRA_URL,
+        })
 
-  def _generate_examples(self,data_dir_path,data_annotations_path):
-      
-      image_dict = returnImageDict(data_dir_path)
-      bbox_dict = returnBbox(data_annotations_path,image_dict)
-      mat = tfds.core.lazy_imports.scipy_io.loadmat(data_annotations_path)
-      for i in range(len(mat['annotations'][0])):
-          #-1 becuase class labels are index-1 based
-          image_name = mat['annotations'][0][i][4].item().split(".")[0] if "cars_test_annos" in data_annotations_path  else mat['annotations'][0][i][5].item().split(".")[0]
-          label = 'test' if 'cars_test_annos' in data_annotations_path else _NAMES[mat['annotations'][0][i][4].item()-1] 
-          image = image_dict[image_name]
-          bbox = bbox_dict[image_name]
-               
-          yield {
-                    "label": label,
-                    "image": image,
-                    "bbox":bbox,
-                }
+        return [
+            tfds.core.SplitGenerator(
+                name="train",
+                num_shards=10,
+                gen_kwargs={
+                    "data_dir_path": os.path.join(output_files['train'], 'cars_train'),
+                    "data_annotations_path":os.path.join(output_files['extra'], os.path.join('devkit', 'cars_train_annos.mat')),
+                },
+            ),
+
+            tfds.core.SplitGenerator(
+                name="test",
+                num_shards=10,
+                gen_kwargs={
+                    "data_dir_path": os.path.join(output_files['test'], 'cars_test'),
+                    "data_annotations_path":os.path.join(output_files['extra'], os.path.join('devkit', 'cars_test_annos.mat')),
+                },
+            ),
+        
+        ]
+
+    def _generate_examples(self, data_dir_path, data_annotations_path):
+        """Generate training and testing samples"""
+
+        image_dict = returnImageDict(data_dir_path)
+        bbox_dict = returnBbox(data_annotations_path, image_dict)
+        mat = tfds.core.lazy_imports.scipy_io.loadmat(data_annotations_path)
+        for i in range(len(mat['annotations'][0])):
+            #-1 becuase class labels are index-1 based
+            image_name = mat['annotations'][0][i][4].item().split(".")[0] if "cars_test_annos" in data_annotations_path  else mat['annotations'][0][i][5].item().split(".")[0]
+            label = 'test' if 'cars_test_annos' in data_annotations_path else _NAMES[mat['annotations'][0][i][4].item()-1] 
+            image = image_dict[image_name]
+            bbox = bbox_dict[image_name]
+                
+            yield {
+                "label": label,
+                "image": image,
+                "bbox": bbox,
+                    }
 
                       
 def returnImageDict(path):
     print(path)
-    return {filename.split(".")[0]: os.path.join(path,filename) for filename in tf.io.gfile.listdir(path)}
+    return {filename.split(".")[0]: os.path.join(path, filename) for filename in tf.io.gfile.listdir(path)}
 
-def returnBbox(filename,image_dict):
+def returnBbox(filename, image_dict):
     bbox_dict = {}
     data = tfds.core.lazy_imports.scipy_io.loadmat(filename)
     for i in range(len(data['annotations'][0])):
@@ -165,14 +167,13 @@ def returnBbox(filename,image_dict):
             image_name = data['annotations'][0][i][4].item().split(".")[0]
         else:
             image_name = data['annotations'][0][i][5].item().split(".")[0]
-        #image_name = data['annotations'][0][i][4].item().split(".")[0] if "test" in filename  else data['annotations'][0][i][5].item().split(".")[0]
-        ymin= float(data['annotations'][0][i][1].item())
-        xmin= float(data['annotations'][0][i][0].item())
-        ymax= float(data['annotations'][0][i][3].item())
-        xmax= float(data['annotations'][0][i][2].item())
-        #width,height = tfds.core.lazy_imports.pyplot.imread(image_dict[image_name]).size
+        
+        ymin = float(data['annotations'][0][i][1].item())
+        xmin = float(data['annotations'][0][i][0].item())
+        ymax = float(data['annotations'][0][i][3].item())
+        xmax = float(data['annotations'][0][i][2].item())
         with tfds.core.lazy_imports.PIL_Image.open(image_dict[image_name]) as img:
-            width,height=img.size
+            width, height=img.size
         bbox_dict[image_name] = tfds.features.BBox(ymin/height, xmin/width, ymax/height, xmax/width)
     return bbox_dict
 
