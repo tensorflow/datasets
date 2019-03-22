3f02d7cc105481c676cbb99674bb96e20efc2905
Kislay Singh <43761382+SinghKislay@users.noreply.github.com>
2019-03-23 00:18:46 +0530

    pull2

---
+++ tensorflow_datasets/image/lfw_test.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from tensorflow_datasets import testing
+from tensorflow_datasets.image import lfw
+
+
+class LFWTest(testing.DatasetBuilderTestCase):
+  DATASET_CLASS = lfw.LFW
+
+  SPLITS = {
+      "train": 3,
+      "test": 1,
+  }
+
+
+
+if __name__ == "__main__":
+  testing.test_main()
