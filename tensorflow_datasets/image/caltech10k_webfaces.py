73228aa30d47be28ecd238cfb947c3d08b3a3197
mandroid6 <razzormandar@gmail.com>
2019-03-23 16:56:14 +0530

    added and updated _split generators for caltech10k_webfaces

--- tensorflow_datasets/image/caltech10k_webfaces.py
+++ tensorflow_datasets/image/caltech10k_webfaces.py
@@ -10,7 +10,8 @@ import tensorflow_datasets.public_api as tfds
 
 LANDMARK_HEADINGS = ("lefteye_x lefteye_y righteye_x righteye_y "
                      "nose_x nose_y mouth_x mouth_y").split()
-
+IMAGE_DATA = ("http://www.vision.caltech.edu/Image_Datasets/Caltech_10K_WebFaces/Caltech_WebFaces.tar")
+LANDMARK_DATA = ("http://www.vision.caltech.edu/Image_Datasets/Caltech_10K_WebFaces/WebFaces_GroundThruth.txt")
 class Caltech10K_WebFaces(tfds.core.GeneratorBasedBuilder):
 
     VERSION = tfds.core.Version("0.1.0")
@@ -23,6 +24,37 @@ class Caltech10K_WebFaces(tfds.core.GeneratorBasedBuilder):
                 "image": 
                     tfds.features.Image(encoding_format='jpeg'),
                 "landmarks": {name: tf.int64 for name in LANDMARK_HEADINGS}
-
-            })
+            }),
+            urls=['http://www.vision.caltech.edu/Image_Datasets/Caltech_10K_WebFaces/'],
+            citation=""
         )
+
+
+    def _split_generator(self, dl_manager):
+        extracted_dirs = dl_manager.download_and_extract({
+            "images": IMAGE_DATA,
+            "landmarks" LANDMARK_DATA
+        })
+        return [
+            tfds.core.SplitGenerator(
+                name=tfds.Split.TRAIN,
+                num_shards=10,
+                gen_kwargs={
+                    "file_id": 0,
+                    "extracted_dirs": extracted_dirs,
+                }),
+            tfds.core.SplitGenerator(
+                name=tfds.Split.VALIDATION,
+                num_shards=4,
+                gen_kwargs={
+                    "file_id": 1,
+                    "extracted_dirs": extracted_dirs,
+                }),
+            tfds.core.SplitGenerator(
+                name=tfds.Split.TEST,
+                num_shards=4,
+                gen_kwargs={
+                    "file_id": 2,
+                    "extracted_dirs": extracted_dirs,
+                })
+        ]
