80173b9e6d98fd7aa15405762e5b33362e2029f7
Kislay Singh <43761382+SinghKislay@users.noreply.github.com>
2019-03-23 15:40:47 +0530

    final style changes

--- tensorflow_datasets/image/lfw.py
+++ tensorflow_datasets/image/lfw.py
@@ -1,14 +1,33 @@
+# coding=utf-8
+# Copyright 2019 The TensorFlow Datasets Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Labeled faces in wild."""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
 
 import os
 import tensorflow as tf
 import tensorflow_datasets.public_api as tfds
 
+
 _URL="http://vis-www.cs.umass.edu/lfw/lfw.tgz"
+
 LFW_IMAGE_SHAPE=(250,250,3)
+
 LFW_CITATION="""\
 @TechReport{LFWTech,
     author = {Gary B. Huang and Manu Ramesh and Tamara Berg and Erik Learned-Miller},
@@ -37,9 +56,6 @@ class LFW(tfds.core.GeneratorBasedBuilder):
             urls=[self.URL],
             citation=LFW_CITATION,
         )
-    
-
-
     
     def _split_generators(self, dl_manager):
         path=dl_manager.download_and_extract(_URL)
