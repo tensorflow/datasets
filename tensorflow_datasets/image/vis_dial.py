c2de6bab6627edb7d584984d539ca7e816814338
Parth Shandilya <parth1989shandilya@gmail.com>
2019-03-23 14:33:31 +0530

    Capitalize Constants and Added tf.io.gfile

--- tensorflow_datasets/image/vis_dial.py
+++ tensorflow_datasets/image/vis_dial.py
@@ -23,6 +23,8 @@ import json
 import os
 import re
 import fnmatch
+
+import tensorflow as tf
 import tensorflow_datasets.public_api as tfds
 
 _CITATION = """\
@@ -35,12 +37,12 @@ _CITATION = """\
 }
 """
 
-_Train_Dialogs = "https://dl.dropboxusercontent.com/s/ix8keeudqrd8hn8/visdial_1.0_train.zip"
-_Train_Images = "http://images.cocodataset.org/zips/train2014.zip"
-_Validation_Dialogs = "https://dl.dropboxusercontent.com/s/ibs3a0zhw74zisc/visdial_1.0_val.zip"
-_Validation_Images = "https://dl.dropboxusercontent.com/s/twmtutniktom7tu/VisualDialog_val2018.zip"
-_Test_Dialogs = "https://dl.dropboxusercontent.com/s/o7mucbre2zm7i5n/visdial_1.0_test.zip"
-_Test_Images = "https://dl.dropboxusercontent.com/s/mwlrg31hx0430mt/VisualDialog_test2018.zip"
+_TRAIN_DIALOGS = "https://dl.dropboxusercontent.com/s/ix8keeudqrd8hn8/visdial_1.0_train.zip"
+_TRAIN_IMAGES = "http://images.cocodataset.org/zips/train2014.zip"
+_VALIDATION_DIALOGS = "https://dl.dropboxusercontent.com/s/ibs3a0zhw74zisc/visdial_1.0_val.zip"
+_VALIDATION_IMAGES = "https://dl.dropboxusercontent.com/s/twmtutniktom7tu/VisualDialog_val2018.zip"
+_TEST_DIALOGS = "https://dl.dropboxusercontent.com/s/o7mucbre2zm7i5n/visdial_1.0_test.zip"
+_TEST_IMAGES = "https://dl.dropboxusercontent.com/s/mwlrg31hx0430mt/VisualDialog_test2018.zip"
 
 
 class VisualDialog(tfds.core.GeneratorBasedBuilder):
@@ -53,8 +55,11 @@ class VisualDialog(tfds.core.GeneratorBasedBuilder):
             builder = self,
             description="A large set of Images and Dialogs",
             features=tfds.features.FeaturesDict({
-                "image": tfds.features.Image(encoding_format="jpeg"),
-                "label": tfds.features.Text(),
+                "image": tfds.features.Image(),
+                "dialog": tfds.features.SequenceDict({
+                    'answer': tf.int8,
+                    'question': tf.int8,
+                }),
             }),
             urls=["https://visualdialog.org/"],
             citation=_CITATION
@@ -64,14 +69,14 @@ class VisualDialog(tfds.core.GeneratorBasedBuilder):
     def _split_generators(self, dl_manager):
 
         extracted_json = dl_manager.download_and_extract({
-            "Train_Dialogs": _Train_Dialogs,
-            "Validation_Dialogs": _Validation_Dialogs,
-            "Test_Dialogs": _Test_Dialogs,
+            "Train_Dialogs": _TRAIN_DIALOGS,
+            "Validation_Dialogs": _VALIDATION_DIALOGS,
+            "Test_Dialogs": _TEST_DIALOGS,
         })
         extracted_images = dl_manager.download_and_extract({
-            "Train_Images": _Train_Images,
-            "Validation_Images": _Validation_Images,
-            "Test_Images": _Test_Images,
+            "Train_Images": _TRAIN_IMAGES,
+            "Validation_Images": _VALIDATION_IMAGES,
+            "Test_Images": _TEST_IMAGES,
         })
 
         return [
@@ -111,22 +116,22 @@ class VisualDialog(tfds.core.GeneratorBasedBuilder):
 
         def find(pattern, path):
             result = []
-            for root, dirs, files in os.walk(path):
+            for root, dirs, files in tf.io.gfile.walk(path):
                 for name in files:
                     if fnmatch.fnmatch(name, pattern):
                         result.append(os.path.join(root, name))
             return result
 
-        image_filedir = os.path.join(image["Train_Images"],"VisualDialog_train2018")
-        dialog_filedir = os.path.join(dialog["Train_Dialogs"],"visdial_1.0_train.json")
+        image_filedir = os.path.join(image["Test_Images"],"VisualDialog_test2018")
+        dialog_filedir = os.path.join(dialog["Test_Dialogs"],"visdial_1.0_test.json")
 
-        with open(dialog_filedir) as data_file:
+        with tf.io.gfile.GFile(dialog_filedir) as data_file:
             data_f = json.load(data_file)
             for details in data_f["data"]["dialogs"]:
                 pattern = '*'+ str(details["image_id"]) + '.jpg'
                 image_ = find(pattern, image_filedir.format(details["image_id"]))
                 dialog_ = details["dialog"]
                 yield {
-                    "image": image_,
-                    "label": dialog_
+                  "image": image_,
+                  "dialog": dialog_
                 }
