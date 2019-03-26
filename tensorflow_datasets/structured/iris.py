0d44a4bacac686035c75e230ab9f27ed17c214a2
Alex Park <alex@cerebras.net>
2019-03-26 08:26:49 -0700

    Address review comments

--- tensorflow_datasets/structured/iris.py
+++ tensorflow_datasets/structured/iris.py
@@ -30,63 +30,64 @@ linearly separable from each other.
 
 
 class Iris(tfds.core.GeneratorBasedBuilder):
-    """Iris flower dataset"""
+  """Iris flower dataset"""
 
-    NUM_CLASSES = 3
-    VERSION = tfds.core.Version("0.1.0")
+  NUM_CLASSES = 3
+  VERSION = tfds.core.Version("0.1.0")
 
-    def _info(self):
-        return tfds.core.DatasetInfo(
-            builder=self,
-            description=_DESCRIPTION,
-            # tfds.features.FeatureConnectors
-            features=tfds.features.FeaturesDict(
-                {
-                    "features": tfds.features.Tensor(shape=(4,), dtype=tf.float32),
-                    # Here, labels can be one of 3 classes
-                    "label": tfds.features.ClassLabel(
-                        names=["Iris-setosa", "Iris-versicolor", "Iris-virginica"]
-                    ),
-                }
-            ),
-            supervised_keys=("features", "label"),
-            urls=["https://archive.ics.uci.edu/ml/datasets/iris"],
-            citation=_CITATION,
-        )
+  def _info(self):
+    return tfds.core.DatasetInfo(
+        builder=self,
+        description=_DESCRIPTION,
+        # tfds.features.FeatureConnectors
+        features=tfds.features.FeaturesDict({
+            "features":
+                tfds.features.Tensor(shape=(4,), dtype=tf.float32),
+            # Here, labels can be one of 3 classes
+            "label":
+                tfds.features.ClassLabel(
+                    names=["Iris-setosa", "Iris-versicolor", "Iris-virginica"]),
+        }),
+        supervised_keys=("features", "label"),
+        urls=["https://archive.ics.uci.edu/ml/datasets/iris"],
+        citation=_CITATION,
+    )
 
-    def _split_generators(self, dl_manager):
-        iris_file = dl_manager.download(IRIS_URL)
-        all_lines = tf.io.gfile.GFile(iris_file).read().split("\n")
-        records = [l for l in all_lines if l]  # get rid of empty lines
+  def _split_generators(self, dl_manager):
+    iris_file = dl_manager.download(IRIS_URL)
+    all_lines = tf.io.gfile.GFile(iris_file).read().split("\n")
+    records = [l for l in all_lines if l]  # get rid of empty lines
 
-        split = 0.8
-        total = len(records)
-        per_class = total // self.NUM_CLASSES
-        train_per_class = int(per_class * split)
-        test_per_class = per_class - train_per_class
+    split = 0.8
+    total = len(records)
+    per_class = total // self.NUM_CLASSES
+    train_per_class = int(per_class * split)
+    test_per_class = per_class - train_per_class
 
-        train_lines = []
-        for offset in range(0, total, per_class):
-            train_lines += records[offset : (offset + train_per_class)]
+    train_lines = []
+    for offset in range(0, total, per_class):
+      train_lines += records[offset:(offset + train_per_class)]
 
-        test_lines = []
-        for offset in range(train_per_class, total, per_class):
-            test_lines += records[offset : (offset + test_per_class)]
+    test_lines = []
+    for offset in range(train_per_class, total, per_class):
+      test_lines += records[offset:(offset + test_per_class)]
 
-        # Specify the splits
-        return [
-            tfds.core.SplitGenerator(
-                name="train", num_shards=1, gen_kwargs={"records": train_lines}
-            ),
-            tfds.core.SplitGenerator(
-                name="test", num_shards=1, gen_kwargs={"records": test_lines}
-            ),
-        ]
+    # Specify the splits
+    return [
+        tfds.core.SplitGenerator(
+            name=tfds.Split.TRAIN,
+            num_shards=1,
+            gen_kwargs={"records": train_lines}),
+        tfds.core.SplitGenerator(
+            name=tfds.Split.TEST,
+            num_shards=1,
+            gen_kwargs={"records": test_lines}),
+    ]
 
-    def _generate_examples(self, records):
-        for record in records:
-            elems = record.split(",")
-            yield {
-                "features": np.fromstring(" ".join(elems[:4]), dtype="f4", sep=" "),
-                "label": elems[4],
-            }
+  def _generate_examples(self, records):
+    for record in records:
+      elems = record.split(",")
+      yield {
+          "features": np.fromstring(" ".join(elems[:4]), dtype="f4", sep=" "),
+          "label": elems[4],
+      }
