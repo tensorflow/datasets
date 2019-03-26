e94fc4b533dc8ed2300a78a8b4cff2cd39be1705
Soumyadeep Jana <sjana646@gmail.com>
2019-03-26 16:35:45 +0530

    Added dataset class,test file and fake examples

--- tensorflow_datasets/image/ham10000_test.py
+++ tensorflow_datasets/image/ham10000_test.py
@@ -1,10 +1,13 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 import tensorflow as tf
 from tensorflow_datasets.image import ham10000
 import tensorflow_datasets.testing as tfds_test
 
 
-class MyDatasetTest(tfds_test.DatasetBuilderTestCase):
-  DATASET_CLASS = ham10000.ham10000
+class Ham10000Test(tfds_test.DatasetBuilderTestCase):
+  DATASET_CLASS = ham10000.Ham10000
   SPLITS = {  
       "train": 2,
   }
@@ -12,3 +15,4 @@ class MyDatasetTest(tfds_test.DatasetBuilderTestCase):
 
 if __name__ == "__main__":
   tfds_test.test_main()
+    
