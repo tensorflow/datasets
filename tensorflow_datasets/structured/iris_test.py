aefe51725007c44cf5e6601ef6badef420bc8f6b
Alex Park <alex@cerebras.net>
2019-03-25 23:20:51 -0700

    Reclassify as structured vs. image Clean up copy paste errors

--- tensorflow_datasets/structured/iris_test.py
+++ tensorflow_datasets/structured/iris_test.py
@@ -1,10 +1,10 @@
-"""Tests for tensorflow_datasets.image.lsun."""
+"""Tests for tensorflow_datasets.structured.iris."""
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from tensorflow_datasets import testing
-from tensorflow_datasets.image import iris
+from tensorflow_datasets.structured import iris
 
 
 class IrisTest(testing.DatasetBuilderTestCase):
