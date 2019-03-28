0af9124bf8a8abc75958018bb4f258ede117ce1f
Kislay Singh <43761382+SinghKislay@users.noreply.github.com>
2019-03-28 23:07:55 +0530

    Added lfw

--- tensorflow_datasets/image/__init__.py
+++ tensorflow_datasets/image/__init__.py
@@ -45,3 +45,4 @@ from tensorflow_datasets.image.rock_paper_scissors import RockPaperScissors
 from tensorflow_datasets.image.shapes3d import Shapes3d
 from tensorflow_datasets.image.svhn import SvhnCropped
 from tensorflow_datasets.image.voc import Voc2007
+from tensorflow_datasets.image.lfw import LFW
