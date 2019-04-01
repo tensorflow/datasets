bae6cb3078a2726c610ca1b6fb5501a65aaad82f
Soumyadeep Jana <sjana646@gmail.com>
2019-04-01 19:22:14 +0530

    Registered dataset class in __init__ file

--- tensorflow_datasets/image/__init__.py
+++ tensorflow_datasets/image/__init__.py
@@ -36,6 +36,7 @@ from tensorflow_datasets.image.mnist import EMNIST
 from tensorflow_datasets.image.mnist import FashionMNIST
 from tensorflow_datasets.image.mnist import KMNIST
 from tensorflow_datasets.image.mnist import MNIST
+from tensorflow_datasets.image.mura import Mura
 from tensorflow_datasets.image.omniglot import Omniglot
 from tensorflow_datasets.image.open_images import OpenImagesV4
 from tensorflow_datasets.image.quickdraw import QuickdrawBitmap
