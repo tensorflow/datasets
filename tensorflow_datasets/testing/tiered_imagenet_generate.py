a108fab45bf29c82272ad9bdd84660eb5d60793d
Suvarna Kadam <suvarna.kadam@gmail.com>
2019-04-11 23:52:52 +0530

    randomized list generation for the fake example generation

--- tensorflow_datasets/testing/tiered_imagenet_generate.py
+++ tensorflow_datasets/testing/tiered_imagenet_generate.py
@@ -13,18 +13,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from absl import app
-from absl import flags
-
-import numpy as np
+r"""Generate tiered Imagenet files, smaller and with random data.
+"""
 import os
+
+import pickle
+from absl import flags
+import numpy as np
+
 import tensorflow as tf
 from tensorflow_datasets.core.utils import py_utils
-import tensorflow_datasets.public_api as tfds
+
 flags.DEFINE_string("tfds_dir", py_utils.tfds_dir(),
                     "Path to tensorflow_datasets directory")
 FLAGS = flags.FLAGS
-pickle = tfds.core.lazy_imports.pickle
 
 _NUM_CATEGORIES_TRAIN = 5  # Original size in dataset is 20
 _NUM_CATEGORIES_VAL = 2  # Original size in dataset is 6
@@ -39,6 +41,15 @@ _NUM_IMAGES_TEST = 10  # Original size in dataset is 206209
 _PATH_DUMMY_DATA = "tensorflow_datasets/testing/test_data/fake_examples/tiered_imagenet"
 
 
+def randomized_list(size, low, high):
+    """ Returns a randomized list of labels in the range between low and high 
+    """
+    labels_list = []
+    for i in range(size):
+        labels_list.append(str(np.random.randint(low, high)))
+    return labels_list
+
+
 def generate_train_data(path):
     """Generates dummy train, validation and test images file and corresponding labels file   .
     """
@@ -47,21 +58,23 @@ def generate_train_data(path):
     # generate dummy train data
     train_images_data = np.random.randint(
         0, 255, (_NUM_IMAGES_TRAIN, 84, 84, 3), np.uint8)
-    train_specific_labels = [str(i) for i in range(_NUM_CLASSES_TRAIN)]
-    train_general_labels = [str(i) for i in range(_NUM_CATEGORIES_TRAIN)]
-    train_specific_labels_str = [str(i) for i in range(_NUM_CLASSES_TRAIN)]
-    train_general_labels_str = [str(i) for i in range(_NUM_CATEGORIES_TRAIN)]
+    train_specific_labels = randomized_list(
+        _NUM_IMAGES_TRAIN, 0, _NUM_CLASSES_TRAIN)
+    train_general_labels = randomized_list(
+        _NUM_IMAGES_TRAIN, 0, _NUM_CATEGORIES_TRAIN)
+    train_specific_labels_str = train_specific_labels
+    train_general_labels_str = train_general_labels
     train_images = {"images": train_images_data}
     train_labels = {"label_general": train_general_labels, "label_general_str": train_general_labels_str,
                     "label_specific": train_specific_labels, "label_specific_str": train_specific_labels_str}
     # save train images
     path_train = os.path.join(path, "train_images_png.pkl")
-    with tf.io.gfile.GFile(path_train, "wb") as f:
-        pickle.dump(train_images, f, pickle.HIGHEST_PROTOCOL)
+    with tf.io.gfile.GFile(path_train, "wb") as train_img_file:
+        pickle.dump(train_images, train_img_file, pickle.HIGHEST_PROTOCOL)
     # save train labels
     path_train = os.path.join(path, "train_labels.pkl")
-    with tf.io.gfile.GFile(path_train, "wb") as f:
-        pickle.dump(train_labels, f, pickle.HIGHEST_PROTOCOL)
+    with tf.io.gfile.GFile(path_train, "wb") as train_label_file:
+        pickle.dump(train_labels, train_label_file, pickle.HIGHEST_PROTOCOL)
 
 
 def generate_validation_data(path):
@@ -70,23 +83,24 @@ def generate_validation_data(path):
     # generate dummy validation data
     validation_images_data = np.random.randint(
         0, 255, (_NUM_IMAGES_VAL, 84, 84, 3), np.uint8)
-    validation_specific_labels = [str(i) for i in range(_NUM_CLASSES_VAL)]
-    validation_general_labels = [str(i) for i in range(_NUM_CATEGORIES_VAL)]
-    validation_specific_labels_str = [str(i) for i in range(_NUM_CLASSES_VAL)]
-    validation_general_labels_str = [str(i)
-                                     for i in range(_NUM_CATEGORIES_VAL)]
+    validation_specific_labels = randomized_list(
+        _NUM_IMAGES_VAL, 0, _NUM_CLASSES_VAL)
+    validation_general_labels = randomized_list(
+        _NUM_IMAGES_VAL, 0, _NUM_CATEGORIES_VAL)
+    validation_specific_labels_str = validation_specific_labels
+    validation_general_labels_str = validation_general_labels
     val_images = {"images": validation_images_data}
     val_labels = {"label_general": validation_general_labels, "label_general_str": validation_general_labels_str,
                   "label_specific": validation_specific_labels, "label_specific_str": validation_specific_labels_str}
 
     # save validation images
     path_validation = os.path.join(path, "val_images_png.pkl")
-    with tf.io.gfile.GFile(path_validation, "wb") as f:
-        pickle.dump(val_images, f, pickle.HIGHEST_PROTOCOL)
+    with tf.io.gfile.GFile(path_validation, "wb") as val_img_file:
+        pickle.dump(val_images, val_img_file, pickle.HIGHEST_PROTOCOL)
     # save validation labels
     path_validation = os.path.join(path, "val_labels.pkl")
-    with tf.io.gfile.GFile(path_validation, "wb") as f:
-        pickle.dump(val_labels, f, pickle.HIGHEST_PROTOCOL)
+    with tf.io.gfile.GFile(path_validation, "wb") as val_label_file:
+        pickle.dump(val_labels, val_label_file, pickle.HIGHEST_PROTOCOL)
 
 
 def generate_test_data(path):
@@ -95,21 +109,23 @@ def generate_test_data(path):
     # generate dummy test data
     test_images_data = np.random.randint(
         0, 255, (_NUM_IMAGES_TEST, 84, 84, 3), np.uint8)
-    test_specific_labels = [str(i) for i in range(_NUM_CLASSES_TEST)]
-    test_general_labels = [str(i) for i in range(_NUM_CATEGORIES_TEST)]
-    test_specific_labels_str = [str(i) for i in range(_NUM_CLASSES_TEST)]
-    test_general_labels_str = [str(i) for i in range(_NUM_CATEGORIES_TEST)]
+    test_specific_labels = randomized_list(
+        _NUM_IMAGES_TEST, 0, _NUM_CLASSES_TEST)
+    test_general_labels = randomized_list(
+        _NUM_IMAGES_TEST, 0, _NUM_CATEGORIES_TEST)
+    test_specific_labels_str = test_specific_labels
+    test_general_labels_str = test_general_labels
     test_images = {"images": test_images_data}
     test_labels = {"label_general": test_general_labels, "label_general_str": test_general_labels_str,
                    "label_specific": test_specific_labels, "label_specific_str": test_specific_labels_str}
     # save test images
     path_test = os.path.join(path, "test_images_png.pkl")
-    with tf.io.gfile.GFile(path_test, "wb") as f:
-        pickle.dump(test_images, f, pickle.HIGHEST_PROTOCOL)
+    with tf.io.gfile.GFile(path_test, "wb") as test_img_file:
+        pickle.dump(test_images, test_img_file, pickle.HIGHEST_PROTOCOL)
     # save test labels
     path_test = os.path.join(path, "test_labels.pkl")
-    with tf.io.gfile.GFile(path_test, "wb") as f:
-        pickle.dump(test_labels, f, pickle.HIGHEST_PROTOCOL)
+    with tf.io.gfile.GFile(path_test, "wb") as test_label_file:
+        pickle.dump(test_labels, test_label_file, pickle.HIGHEST_PROTOCOL)
 
 
 if __name__ == "__main__":
