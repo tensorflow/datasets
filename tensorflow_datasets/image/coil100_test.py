9d839a071142f5c3f9631d90aa357229e2b0630a
Soumyadeep Jana <sjana646@gmail.com>
2019-04-18 11:14:04 +0530

    Added unit test file

---
+++ tensorflow_datasets/image/coil100_test.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import tensorflow as tf
+from tensorflow_datasets.image import coil100
+import tensorflow_datasets.testing as tfds_test
+
+
+class Coil100Test(tfds_test.DatasetBuilderTestCase):
+  DATASET_CLASS = coil100.Coil100
+  SPLITS = {  
+      "train":5,
+  }
+ 
+
+if __name__ == "__main__":
+  tfds_test.test_main()
+    
