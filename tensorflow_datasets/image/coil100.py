6ba362c9bb74a5e3ed44bb88548b0784f3392eaa
Soumyadeep Jana <sjana646@gmail.com>
2019-04-18 11:12:31 +0530

    Added dataset class

---
+++ tensorflow_datasets/image/coil100.py
@@ -0,0 +1,87 @@
+"""Dataset class for COIL-100 dataset"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import tensorflow as tf
+import tensorflow_datasets.public_api as tfds
+import os
+import fnmatch
+
+_URL = "http://www.cs.columbia.edu/CAVE/databases/SLAM_coil-20_coil-100/coil-100/coil-100.zip"
+
+_DESCRIPTION = ("""The dataset contains 7200 color images of 100 objects 
+(72 images per object). The objects have a wide variety of complex geometric and reflectance characteristics.
+The objects were placed on a motorized turntable against a black background.
+The turntable was rotated through 360 degrees to vary object pose with respect to a fxed color camera.
+Images of the objects were taken at pose intervals of	5 degrees.This corresponds to
+72 poses per object""")
+
+_LABELS = [str(x) for x in range(0,360,5)]
+
+_IMAGE_SHAPE = (128,128,3)
+
+_CITATION = """\
+
+@article{nene1996columbia,
+  title={Columbia object image library (coil-20)},
+  author={Nene, Sameer A and Nayar, Shree K and Murase, Hiroshi and others},
+  year={1996},
+  publisher={Technical report CUCS-005-96}
+}
+
+"""
+
+class Coil100(tfds.core.GeneratorBasedBuilder):
+  """COIL-100 Image Dataset Class"""
+
+  VERSION = tfds.core.Version('1.0.0')
+
+  def _info(self):
+        """Define Dataset Info"""
+
+        return tfds.core.DatasetInfo(
+            builder=self,
+    
+            description=(_DESCRIPTION),
+            
+            features=tfds.features.FeaturesDict({
+                "image":tfds.features.Image(shape=_IMAGE_SHAPE),
+                "label": tfds.features.ClassLabel(names=_LABELS),
+                "object_id":tfds.features.Text()
+            }),
+
+            supervised_keys=("image", "label"),
+            
+            urls=[_URL],
+            
+            citation=_CITATION
+        )
+
+  def _split_generators(self, dl_manager):
+        """Define Splits"""
+        path = dl_manager.download_and_extract(_URL)
+
+        return [
+            tfds.core.SplitGenerator(
+                name=tfds.Split.TRAIN,
+                num_shards=10,
+                gen_kwargs={
+                    "data_dir_path": path,
+                },
+            ),
+        ]
+
+  def _generate_examples(self,data_dir_path):
+      """Generate images and labels for splits"""
+
+      for file_name in tf.io.gfile.listdir(data_dir_path):
+        if fnmatch.fnmatch(file_name,'*.png'):
+          image = os.path.join(data_dir_path,file_name)
+          label = file_name.split("_")[1]
+          object_id = file_name.split("_")[0]
+          yield{
+            "image":image,
+            "label":label,
+            "object_id":object_id,
+          }
