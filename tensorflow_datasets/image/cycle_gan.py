293dd112dec55c48f18a3bfcaf64acadd1656b71
Etienne Pot <epot@google.com>
2019-04-25 14:58:17 -0700

    Minor doc update for Cycle GAN
    PiperOrigin-RevId: 245312829

--- tensorflow_datasets/image/cycle_gan.py
+++ tensorflow_datasets/image/cycle_gan.py
@@ -83,8 +83,8 @@ class CycleGAN(tfds.core.GeneratorBasedBuilder):
   BUILDER_CONFIGS = [
       CycleGANConfig(  # pylint: disable=g-complex-comprehension
           name=config_name,
-          description=("A dataset consisting of images from two classes: "
-                       "A and B for example: horses and zebras."),
+          description=("A dataset consisting of images from two classes A and "
+                       "B (For example: horses/zebras, apple/orange,...)"),
           version="0.1.0",
           data=config_name,
       ) for config_name in _DATA_OPTIONS
