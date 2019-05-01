ace4a035e19d91feafd6a74292c03508fa6e6066
Etienne Pot <epot@google.com>
2019-05-01 11:29:35 -0700

    Release tfds 1.0.3
    PiperOrigin-RevId: 246167015

--- tensorflow_datasets/version.py
+++ tensorflow_datasets/version.py
@@ -18,7 +18,7 @@
 # We follow Semantic Versioning (https://semver.org/)
 _MAJOR_VERSION = '1'
 _MINOR_VERSION = '0'
-_PATCH_VERSION = '2'
+_PATCH_VERSION = '3'
 
 __version__ = '.'.join([
     _MAJOR_VERSION,
