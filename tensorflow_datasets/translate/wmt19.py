51f356917cd31e30e1168a23ff6986e4d9604999
Adam Roberts <adarob@google.com>
2019-05-10 14:27:46 -0700

    Limit validation sets for WMT to those recommended by the website.
    PiperOrigin-RevId: 247676177

--- tensorflow_datasets/translate/wmt19.py
+++ tensorflow_datasets/translate/wmt19.py
@@ -42,7 +42,7 @@ class Wmt19Translate(wmt.WmtTranslate):
           url=_URL,
           citation=_CITATION,
           language_pair=(l1, l2),
-          version="0.0.2")
+          version="0.0.3")
       for l1, l2 in _LANGUAGE_PAIRS
   ]
 
@@ -56,10 +56,5 @@ class Wmt19Translate(wmt.WmtTranslate):
             "czeng_17", "yandexcorpus", "wikititles_v1", "uncorpus_v1",
             "rapid_2016_ltfi", "rapid_2019"] + wmt.CWMT_SUBSET_NAMES,
         tfds.Split.VALIDATION: [
-            "euelections_dev2019", "newsdev2014", "newsdev2015", "newsdev2016",
-            "newsdev2017", "newsdev2018", "newsdev2019", "newsdiscussdev2015",
-            "newsdiscusstest2015", "newssyscomb2009", "newstest2008",
-            "newstest2009", "newstest2010", "newstest2011", "newstest2012",
-            "newstest2013", "newstest2014", "newstest2015", "newstest2016",
-            "newstestB2016", "newstest2017", "newstestB2017", "newstest2018"]
+            "euelections_dev2019", "newsdev2019", "newstest2018"]
     }
