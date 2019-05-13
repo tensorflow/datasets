558f5f81d15c52e910095ce9e146ad2e70c84dc2
Alexander Kolesnikov <akolesnikov@google.com>
2019-05-13 10:27:30 -0700

    Add CLEVR dataset.
    PiperOrigin-RevId: 247962763

--- tensorflow_datasets/image/__init__.py
+++ tensorflow_datasets/image/__init__.py
@@ -24,6 +24,7 @@ from tensorflow_datasets.image.chexpert import Chexpert
 from tensorflow_datasets.image.cifar import Cifar10
 from tensorflow_datasets.image.cifar import Cifar100
 from tensorflow_datasets.image.cifar10_corrupted import Cifar10Corrupted
+from tensorflow_datasets.image.clevr import CLEVR
 from tensorflow_datasets.image.coco import Coco2014
 from tensorflow_datasets.image.colorectal_histology import ColorectalHistology
 from tensorflow_datasets.image.colorectal_histology import ColorectalHistologyLarge
