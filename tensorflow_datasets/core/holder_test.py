de2840ddf4c59af43453aa21b7cd80141d378b2b
us <rahmetsaritekin@gmail.com>
2019-07-24 14:49:19 +0300

    Move all create operations to `setUpClass`.

--- tensorflow_datasets/core/holder_test.py
+++ tensorflow_datasets/core/holder_test.py
@@ -25,17 +25,17 @@ class TestImageHolder(testing.TestCase):
 		cls.TEST_DIR = testing.make_tmp_dir()
 		cls._image_path = os.path.join(cls.TEST_DIR, 'test.png')
 		cls._image_size = (11, 11)
+		with mock.patch.object(holder.ImageHolder, 'image_size',
+													 return_value=cls._image_size):
+			image = holder.ImageHolder(None, 'test', 'png', None, cls._image_path)
+			image.create_fakes()
+		cls.im_size = lazy_imports.PIL_Image.open(cls._image_path).size
 
 	def test_create_fakes(self):
-		with mock.patch.object(holder.ImageHolder, 'image_size',
-													 return_value=self._image_size):
-			image = holder.ImageHolder(None, 'test', 'png', None, self._image_path)
-			image.create_fakes()
-			self.assertTrue(tf.io.gfile.exists(self._image_path))
+		self.assertTrue(tf.io.gfile.exists(self._image_path))
 
 	def test_image_size(self):
-		im = lazy_imports.PIL_Image.open(self._image_path)
-		self.assertEqual(im.size, self._image_size)
+		self.assertEqual(self.im_size, self._image_size)
 
 	@classmethod
 	def tearDownClass(cls):
@@ -53,14 +53,13 @@ class TestPlainTextHolder(testing.TestCase):
 		with tf.io.gfile.GFile(cls._target_file_path, 'w') as f:
 			for i in range(10):
 				f.write("This is line %d\r\n" % (i + 1))
+		file = holder.PlainTextHolder(None, 'test', 'txt', cls._target_file_path,
+																	cls._output_file_path)
+		file.create_fakes()
 
 	def test_create_fakes(self):
-		file = holder.PlainTextHolder(None, 'test', 'txt', self._target_file_path,
-																	self._output_file_path)
-		file.create_fakes()
-		with tf.io.gfile.GFile(self._target_file_path,
-													 'r') as target_file, tf.io.gfile.GFile(
-			self._output_file_path, 'r') as fake_file:
+		with tf.io.gfile.GFile(self._target_file_path, 'r') as target_file, \
+				tf.io.gfile.GFile(self._output_file_path, 'r') as fake_file:
 			for x, y in zip(target_file, fake_file):
 				self.assertEqual(x, y)
 
@@ -80,12 +79,9 @@ class TestZipHolder(tf.test.TestCase):
 		cls._image_folder = os.path.join(cls.TEST_DIR, 'image')
 		tf.io.gfile.makedirs(cls._image_folder)
 		cls.image_file_path = os.path.join(cls._image_folder, 'image.png')
-		with mock.patch.object(holder.ImageHolder, 'image_size',
-													 return_value=(11, 11)):
-			image = holder.ImageHolder(None, 'image', 'png', None,
-																 cls.image_file_path)
+		with mock.patch.object(holder.ImageHolder, 'image_size', return_value=(11, 11)):
+			image = holder.ImageHolder(None, 'image', 'png', None, cls.image_file_path)
 			image.create_fakes()
-
 		# create fake text file
 		cls.text_folder = os.path.join(cls.TEST_DIR, 'text')
 		tf.io.gfile.makedirs(cls.text_folder)
@@ -97,11 +93,10 @@ class TestZipHolder(tf.test.TestCase):
 		shutil.make_archive(cls.TEST_DIR, 'zip', cls.TEST_DIR)
 		cls.zip_path = cls.TEST_DIR + '.zip'
 		cls.out_zip_path = cls.TEST_DIR + '_auto_gen.zip'
+		zip_gen = holder.ZipHolder('test_folder', 'zip', cls.zip_path, cls.out_zip_path)
+		zip_gen.create_fakes()
 
 	def test_create_fakes(self):
-		zip_gen = holder.ZipHolder('test_folder', 'zip', self.zip_path,
-															 self.out_zip_path)
-		zip_gen.create_fakes()
 		original_zip_files = zipfile.ZipFile(self.zip_path).namelist()
 		copied_zip_files = zipfile.ZipFile(self.out_zip_path).namelist()
 		self.assertEqual(original_zip_files, copied_zip_files)
