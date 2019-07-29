c5d99567afd5bed21b0d912f90058a8f8b264470
us <rahmetsaritekin@gmail.com>
2019-07-29 21:52:19 +0300

    Add generate_fake_example to tfds.scripts

---
+++ tensorflow_datasets/scripts/generate_fake_examples.py
@@ -0,0 +1,36 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from absl import app
+from absl import flags
+
+from tensorflow_datasets.core import holder
+
+r"""Generate dataset fake examples and tests.
+
+python -m tensorflow_datasets.scripts.generate_fake.examples \
+  --dataset_name dataset_name \
+  --file_path /Users/user1/Desktop/test.zip
+  --dataset_type dataset_type
+
+"""
+
+FLAGS = flags.FLAGS
+
+_DATASET_TYPE = ['image', 'video', 'audio', 'text', 'structured', 'translate']
+
+flags.DEFINE_string('dataset_name', None, 'Dataset name')
+flags.DEFINE_string('file_path', None, 'File to generated examples.')
+flags.DEFINE_enum('dataset_type', 'image', _DATASET_TYPE, 'Dataset type')
+
+
+def main(_):
+	print(FLAGS.dataset_name)
+	holder.Generator(dataset_name=FLAGS.dataset_name,
+									 dataset_path=FLAGS.file_path,
+									 dataset_type=FLAGS.dataset_type).generator()
+
+
+if __name__ == '__main__':
+	app.run(main)
