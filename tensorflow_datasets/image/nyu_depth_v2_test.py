36be5a68df18879277048123c456e4dd0b9714bb
Lukas Geiger <lukas.geiger94@gmail.com>
2019-08-08 20:26:26 +0100

    Add tests for NYU Depth V2 Dataset

---
+++ tensorflow_datasets/image/nyu_depth_v2_test.py
@@ -0,0 +1,17 @@
+"""Tests for NYU Depth V2 Dataset."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from tensorflow_datasets import testing
+from tensorflow_datasets.image import nyu_depth_v2
+
+
+class NyuDepthV2Test(testing.DatasetBuilderTestCase):
+  DATASET_CLASS = nyu_depth_v2.NyuDepthV2
+  SPLITS = {"train": 4, "test": 2}
+
+
+if __name__ == "__main__":
+  testing.test_main()
