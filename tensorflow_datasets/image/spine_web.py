b05360c892f8f5140e4bd375c77c4711af221d43
chriszhou0916 <12502570+chriszhou0916@users.noreply.github.com>
2019-09-17 10:29:14 -0400

    Changed image shape to one channel

--- tensorflow_datasets/image/spine_web.py
+++ tensorflow_datasets/image/spine_web.py
@@ -37,7 +37,7 @@ class SpineWeb(tfds.core.GeneratorBasedBuilder):
       builder=self,
       description=_DESCRIPTION,
       features=tfds.features.FeaturesDict({
-        "image": tfds.features.Image(shape=(None, None, 1), encoding_format='jpeg'),  # b&w jpeg
+        "image": tfds.features.Image(shape=(None, None, 1)),  # b&w jpeg
         # main thoracic, proximal thoracic, thoracolumbar/lumbar cobb angles
         "label": tfds.features.Tensor(shape=(3,), dtype=tf.float32)
       }),
