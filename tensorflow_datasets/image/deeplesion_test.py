bd59fcfb92b88f3b3651cfe7250ad4e679856a30
zl190 <zl190@duke.edu>
2019-09-27 12:23:00 -0400

    resolve suggested changes from us

--- tensorflow_datasets/image/deeplesion_test.py
+++ tensorflow_datasets/image/deeplesion_test.py
@@ -1,4 +1,19 @@
-"""TODO(deeplesion): Add a description here."""
+# coding=utf-8
+# Copyright 2019 The TensorFlow Datasets Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for deeplesion dataset module."""
 
 from __future__ import absolute_import
 from __future__ import division
@@ -9,18 +24,11 @@ from tensorflow_datasets.image import deeplesion
 
 
 class DeeplesionTest(testing.DatasetBuilderTestCase):
-  # TODO(deeplesion):
   DATASET_CLASS = deeplesion.Deeplesion
   SPLITS = {
       "train": 5,
       "validation": 5,
   }
-
-  # If you are calling `download/download_and_extract` with a dict, like:
-  #   dl_manager.download({'some_key': 'http://a.org/out.txt', ...})
-  # then the tests needs to provide the fake output paths relative to the
-  # fake data directory
-  # DL_EXTRACT_RESULT = {'some_key': 'output_file1.txt', ...}
 
 
 if __name__ == "__main__":
