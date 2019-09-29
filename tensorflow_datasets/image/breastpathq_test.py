0be0a5398adbae20e70543868dce4f9a8846724c
Ziyuan Shen <ziyuan.shen@duke.edu>
2019-09-29 19:49:18 -0400

    delete comment in test file

--- tensorflow_datasets/image/breastpathq_test.py
+++ tensorflow_datasets/image/breastpathq_test.py
@@ -16,13 +16,6 @@ class BreastpathqTest(testing.DatasetBuilderTestCase):
       "validation": 1,  # Number of fake test example
   }
 
-  # If you are calling `download/download_and_extract` with a dict, like:
-  #   dl_manager.download({'some_key': 'http://a.org/out.txt', ...})
-  # then the tests needs to provide the fake output paths relative to the
-  # fake data directory
-  # DL_EXTRACT_RESULT = {'some_key': 'output_file1.txt', ...}
-
 
 if __name__ == "__main__":
   testing.test_main()
-
