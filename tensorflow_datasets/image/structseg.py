f870617b526529a8874ac0c3be19ff8d7c26207a
zil317 <lzhgo1994@gmail.com>
2019-10-01 01:27:04 -0400

    run pylint, specify path and add checksum

--- tensorflow_datasets/image/structseg.py
+++ tensorflow_datasets/image/structseg.py
@@ -1,16 +1,17 @@
-##add structure segmentation dataset 
+"""
+Organ-at-risk segmentation dataset from head & neck CT scans
+"""
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
+import re
 import tensorflow_datasets.public_api as tfds
 import tensorflow as tf
-import os
-import glob
 import nibabel as nib
-import re
-import numpy 
+import numpy
 
 _CITATION = """
 @misc{li_zhou_deng_chen, title={StructSeg2019 - Grand Challenge}, 
@@ -18,7 +19,6 @@ url={https://structseg2019.grand-challenge.org/},
 journal={Automatic Structure Segmentation for Radiotherapy Planning Challenge }, 
 author={Li, Hongsheng and Zhou, Jinghao and Deng, Jincheng and Chen, Ming}}
 """
-
 
 _DESCRIPTION = """
 The dataset is used for evaluating automatic algorithms on segmentation of organ-at-risk(OAR)
@@ -30,126 +30,111 @@ _BASE_URL = """https://structseg2019.grand-challenge.org/"""
 
 
 class Structseg(tfds.core.GeneratorBasedBuilder):
+    """
+    Organ-at-risk segmentation dataset from head & neck CT scans
+    """
+    # VERSION = tfds.core.Version('0.1.0')
 
-
-  #VERSION = tfds.core.Version('0.1.0')
-
-  VERSION = tfds.core.Version("1.0.0",
-                              experiments={tfds.core.Experiment.S3: False})
-  SUPPORTED_VERSIONS = [
-      tfds.core.Version("2.0.0"),
-  ]
-
-  def get_all_file_paths(self,directory): 
-
-    # initializing empty file paths list 
-    file_paths = [] 
-
-    # crawling through directory and subdirectories 
-    for root, directories, files in os.walk(directory): 
-      for filename in files: 
-          # join the two strings in order to form the full filepath. 
-          filepath = os.path.join(root, filename) 
-          file_paths.append(filepath) 
-
-    # returning all file paths 
-    return file_paths
-
-
-  def _info(self):
-    return tfds.core.DatasetInfo(
-        builder=self,
-        description=_DESCRIPTION,
-        # tfds.features.FeatureConnectors
-        features=tfds.features.FeaturesDict({
-            "image": tfds.features.Image(shape=(512, 512,1)),
-            "label": tfds.features.Image(shape=(512, 512,1))
-            # These are the features of your dataset like images, labels ...
-        }),
-        #specify feature tuples 
-        supervised_keys=("image", "label"),
-        # Homepage of the dataset for documentation
-        urls=[_BASE_URL],
-        citation=_CITATION
-    )
-
-  def _split_generators(self, dl_manager):
-    path = os.path.join(dl_manager.manual_dir, 'HaN_OAR') 
-    if not tf.io.gfile.exists(path):
-      raise AssertionError('You must download the dataset manually from {}, ''extract it, and place it in {}.'.format( _BASE_URL, dl_manager.manual_dir))
-
-    return [
-        tfds.core.SplitGenerator(
-            name=tfds.Split.TRAIN,
-            num_shards = 1,
-            gen_kwargs={
-                'filepath':path
-
-            }
-        )
+    VERSION = tfds.core.Version("1.0.0",
+                                experiments={tfds.core.Experiment.S3: False})
+    SUPPORTED_VERSIONS = [
+        tfds.core.Version("2.0.0"),
     ]
 
-  def _generate_examples(self, filepath):
-    file_path = filepath  #define file path
-    
-    
-    dirs = self.get_all_file_paths(file_path)
+    def get_all_file_paths(self, directory):
+        """
+        Images stored in the following format:
+        HaN_OAR/1/image.nii.gz
+        HaN_OAR/1/label.nii.gz
+        HaN_OAR/2/image.nii.gz
+        HaN_OAR/2/label.nii.gz ...
+        Get the file path to all images
+        """
 
-    label_dirs = []
-    data_dirs = []
+        file_paths = []
 
-    #separte the directory paths into label subdir and data subdir
+        # crawling through directory and subdirectories
+        for root, directories, files in os.walk(directory):
+            for filename in files:
+                # join the two strings in order to form the full filepath.
+                filepath = os.path.join(root, filename)
+                file_paths.append(filepath)
 
-    for dir in dirs:
-        if dir.endswith("/label.nii.gz"): 
-          label_dirs.append(dir)
-        else:
-          data_dirs.append(dir)
+        return file_paths
 
-    #get index_list from label_dirs
-    index_list = []
+    def _info(self):
+        return tfds.core.DatasetInfo(
+            builder=self,
+            description=_DESCRIPTION,
+            # tfds.features.FeatureConnectors
+            features=tfds.features.FeaturesDict({
+                "image": tfds.features.Image(shape=(512, 512, 1)),
+                "label": tfds.features.Image(shape=(512, 512, 1))
+            }),
+            # specify feature tuples
+            supervised_keys=("image", "label"),
+            # Homepage of the dataset for documentation
+            urls=[_BASE_URL],
+            citation=_CITATION
+        )
 
-    for dir_name in label_dirs:
-      index_ = re.search("(?<=HaN_OAR\/)(\d+)\/",dir_name)
-      index_list.append(index_.group(1))
+    def _split_generators(self, dl_manager):
+        path = os.path.join(dl_manager.manual_dir, 'HaN_OAR')
+        if not tf.io.gfile.exists(path):
+            raise AssertionError(
+                'You must download the dataset manually from {},' \
+                'extract it as a folder named HaN_OAR,' \
+                ' and place it in {}.'.format(_BASE_URL, dl_manager.manual_dir))
 
+        return [
+            tfds.core.SplitGenerator(
+                name=tfds.Split.TRAIN,
+                num_shards=20,
+                gen_kwargs={
+                    'file_path': path  # pylint: disable=no-value-for-parameter
 
-    for patient_index,label_dir, data_dir in zip(index_list,label_dirs, data_dirs) :
-      label_temp = nib.load(label_dir)
-      data_temp = nib.load(data_dir)
-      label_array = label_temp.get_fdata()
-      label_array = numpy.array(label_array,dtype = numpy.uint8)
-      data_array = data_temp.get_fdata()
-      data_array = numpy.array(data_array,dtype = numpy.uint8)
+                }
+            )
+        ]
 
-      for slice_idx in range(label_array.shape[2]):
-        patient = patient_index+"_"+str(slice_idx)
+    def _generate_examples(self, file_path):
+        # file_path = filepath
+        dirs = self.get_all_file_paths(file_path)
 
-        record = {
-            "image": data_array[:,:,slice_idx].reshape(512, 512, 1),
-            "label": label_array[:,:,slice_idx].reshape(512, 512, 1)
-        }
+        label_dirs = []
+        data_dirs = []
 
-        yield patient, record
+        # separate the directory paths into label subdir and data subdir
 
+        for each_dir in dirs:
+            if each_dir.endswith("/label.nii.gz"):
+                label_dirs.append(each_dir)
+            else:
+                data_dirs.append(each_dir)
 
+        # get index_list from label_dirs
+        index_list = []
 
+        for dir_name in label_dirs:
+            index_ = re.search(r"(?<=HaN_OAR\/)(\d+)\/", dir_name)
+            index_list.append(index_.group(1))
 
+        for patient_index, label_dir, data_dir in zip(index_list, label_dirs, data_dirs):
+            label_temp = nib.load(label_dir)
+            data_temp = nib.load(data_dir)
 
+            label_array = label_temp.get_fdata()
+            label_array = numpy.array(label_array, dtype=numpy.uint8)
+            data_array = data_temp.get_fdata()
+            data_array = numpy.array(data_array, dtype=numpy.uint8)
 
+            for slice_idx in range(label_array.shape[2]):
+                patient = patient_index + "_" + str(slice_idx)
 
+                record = {
+                    "image": data_array[:, :, slice_idx].reshape(512, 512, 1),
+                    "label": label_array[:, :, slice_idx].reshape(512, 512, 1)
 
+                }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+                yield patient, record
--- tensorflow_datasets/image/structseg_test.py
+++ tensorflow_datasets/image/structseg_test.py
@@ -1,4 +1,6 @@
-##structure segmentation test script 
+'''
+Structure Segmentation unit test
+'''
 
 from __future__ import absolute_import
 from __future__ import division
@@ -9,19 +11,19 @@ from tensorflow_datasets.image import structseg
 
 
 class StructsegTest(testing.DatasetBuilderTestCase):
-  
-  DATASET_CLASS = structseg.Structseg
-  SPLITS = {
-      "train": 1,  # Number of fake train example
-  }
+    '''
+    Structure Segmentation unit test
+    '''
+    DATASET_CLASS = structseg.Structseg
+    SPLITS = {
+        "train": 1,  # Number of fake train example
+        "test": 1, 
+    }
 
-
-  DL_EXTRACT_RESULT = {
-      "name1": "/tensorflow_datasets/testing/test_data/fake_examples/structseg"  
-  }
-
+    DL_EXTRACT_RESULT = {
+        "name1": "/tensorflow_datasets/testing/test_data/fake_examples/structseg"
+    }
 
 
 if __name__ == "__main__":
-  testing.test_main()
-
+    testing.test_main()
