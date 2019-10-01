57e990d25ab960100da07f057869c5b9e8829f55
drw48 <drw48@duke.edu>
2019-09-30 20:50:56 -0400

    Update to fastmri_test.py file to add a test data config and also added singlecoil test tar.gz file to test_data/fake_examples/fast_mri (renamed) directory for use with testing.

--- tensorflow_datasets/image/fastmri_test.py
+++ tensorflow_datasets/image/fastmri_test.py
@@ -8,12 +8,14 @@ from tensorflow_datasets import testing
 from tensorflow_datasets.image import fastmri
 
 class FastMRITest(testing.DatasetBuilderTestCase):
-    DATASET_CLASS = fastmri.fastMRI
+    DATASET_CLASS = fastmri.FastMRI
     SPLITS = {
-      "train": 1
+      "train": 1,
+      "test": 1
     }
-    DL_EXTRACT_RESULT = ['singlecoil_train.tar.gz']
-
+    DL_EXTRACT_RESULT = ['extracted/singlecoil_train.tar.gz',
+                         'extracted/singlecoil_test_v2.tar.gz']
+    BUILDER_CONFIG_NAMES_TO_TEST = ["singlecoil"]
 
 if __name__ == "__main__":
   testing.test_main()
