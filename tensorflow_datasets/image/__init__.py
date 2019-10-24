0f3cf156d9e06ecae02cc08a888ac1e1111f0ca9
TensorFlow Datasets Team <no-reply@google.com>
2019-10-24 13:12:05 -0700

    Citrus healthy and unhealthy leaves dataset.
    PiperOrigin-RevId: 276550554

--- tensorflow_datasets/image/__init__.py
+++ tensorflow_datasets/image/__init__.py
@@ -33,6 +33,7 @@ from tensorflow_datasets.image.cifar import Cifar10
 from tensorflow_datasets.image.cifar import Cifar100
 from tensorflow_datasets.image.cifar10_1 import Cifar10_1
 from tensorflow_datasets.image.cifar10_corrupted import Cifar10Corrupted
+from tensorflow_datasets.image.citrus import CitrusLeaves
 from tensorflow_datasets.image.clevr import CLEVR
 from tensorflow_datasets.image.cmaterdb import Cmaterdb
 from tensorflow_datasets.image.coco import Coco
