828684e12d69aa1e0e8b1679470fdae8a56e414c
TensorFlow Datasets Team <no-reply@google.com>
2019-11-13 19:58:04 -0800

    Add mathematical dataset
    PiperOrigin-RevId: 280337000

--- tensorflow_datasets/text/__init__.py
+++ tensorflow_datasets/text/__init__.py
@@ -23,6 +23,7 @@ from tensorflow_datasets.text.imdb import IMDBReviews
 from tensorflow_datasets.text.imdb import IMDBReviewsConfig
 from tensorflow_datasets.text.lm1b import Lm1b
 from tensorflow_datasets.text.lm1b import Lm1bConfig
+from tensorflow_datasets.text.math_dataset import MathDataset
 from tensorflow_datasets.text.multi_nli import MultiNLI
 from tensorflow_datasets.text.multi_nli_mismatch import MultiNLIMismatch
 from tensorflow_datasets.text.snli import Snli
