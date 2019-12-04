4ea5bd375fbe5d283c55e8d34e21c0e804c516f8
Lukas Geiger <lukas.geiger94@gmail.com>
2019-12-04 11:21:56 +0000

    NYU Depth: Use homepage instead of urls in data info

--- tensorflow_datasets/image/nyu_depth_v2.py
+++ tensorflow_datasets/image/nyu_depth_v2.py
@@ -76,7 +76,7 @@ class NyuDepthV2(tfds.core.GeneratorBasedBuilder):
         }),
         supervised_keys=('image', 'depth'),
         # Homepage of the dataset for documentation
-        urls=['https://cs.nyu.edu/~silberman/datasets/nyu_depth_v2.html'],
+        homepage='https://cs.nyu.edu/~silberman/datasets/nyu_depth_v2.html',
         citation=_CITATION,
     )
 
