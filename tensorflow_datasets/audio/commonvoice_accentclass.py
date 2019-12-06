2f53aa85a0e4888509bfe103b8f84d8d62161d8f
TheMadScientist1234 <joshua.t.woodie@gmail.com>
2019-12-06 09:44:00 -0500

    Move Accent Classes to different file

---
+++ tensorflow_datasets/audio/commonvoice_accentclass.py
@@ -0,0 +1,123 @@
+"""Accent classes for the Mozilla Common Voice Dataset"""
+ACCENT_CLASSES = {
+  "en": [
+    'canada',
+    'None',
+    'us',
+    'indian',
+    'philippines',
+    'england',
+    'scotland',
+    'other',
+    'australia',
+    'hongkong',
+    'african',
+    'newzealand',
+    'wales',
+    'ireland',
+    'malaysia',
+    'bermuda',
+    'singapore',
+    'southatlandtic'
+  ],
+  "de": [
+    'germany',
+    'switzerland',
+    'None',
+    'austria',
+    'other',
+    'liechtenstein',
+    'france',
+    'netherlands',
+    'united_kingdom',
+    'hungary',
+    'poland'
+  ],
+  "fr": [
+    'france',
+    'None',
+    'switzerland',
+    'belgium',
+    'algeria',
+    'canada',
+    'other',
+    'germany',
+    'tunisia',
+    'senegal',
+    'united_states',
+    'st_pierre_et_miquelon',
+    'monaco',
+    'mayotte',
+    'cote_d_ivoire',
+    'guadeloupe',
+    'martinique',
+    'reunion',
+    'portugal',
+    'netherlands'
+  ],
+  "cy": [
+    'None',
+    'united_kingdom',
+    'other'
+  ],
+  "br": [
+    'None',
+    'other'
+  ],
+  "cv": ['None'],
+  "tr": [
+    'None',
+    'other'
+  ],
+  "tt": ['None'],
+  "ky": [
+    'None',
+    'other'
+  ],
+  "ga-IE": [
+    'None',
+    'other',
+    'ulaidh',
+    'connachta'
+  ],
+  "kab": [
+    'None',
+    'other'
+  ],
+  "ca": [
+    'northwestern',
+    'None',
+    'central',
+    'other',
+    'valencian',
+    'balearic',
+    'northern'
+  ],
+  "zh-TW": [
+    'None',
+    'other'
+  ],
+  "sl": [
+    'None',
+    'other'
+  ],
+  "it": [
+    'None',
+    'other'
+  ],
+  "nl": [
+    'netherlands',
+    'None',
+    'belgium',
+    'other'
+  ],
+  "cnh": [
+    'None',
+    'other'
+  ],
+  "eo": [
+    'None',
+    'internacia',
+    'other'
+  ]
+}
