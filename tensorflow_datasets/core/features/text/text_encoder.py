f888a49154a1b0a001dd847a0aa96f470c94883b
HuaYZhao <921203405@qq.com>
2019-12-26 14:14:52 +0800

    fix TextTokenEncoder load_from_file

--- tensorflow_datasets/core/features/text/text_encoder.py
+++ tensorflow_datasets/core/features/text/text_encoder.py
@@ -344,12 +344,12 @@ class TokenTextEncoder(TextEncoder):
     self._write_lines_to_file(filename, self._vocab_list, kwargs)
 
   @classmethod
-  def load_from_file(cls, filename_prefix):
+  def load_from_file(cls, filename_prefix, tokenizer_cls=None):
     filename = cls._filename(filename_prefix)
     vocab_lines, kwargs = cls._read_lines_from_file(filename)
     has_tokenizer = kwargs.pop("has_tokenizer", False)
     if has_tokenizer:
-      kwargs["tokenizer"] = Tokenizer.load_from_file(filename)
+      kwargs["tokenizer"] = (tokenizer_cls or Tokenizer).load_from_file(filename)
     return cls(vocab_list=vocab_lines, **kwargs)
 
 
