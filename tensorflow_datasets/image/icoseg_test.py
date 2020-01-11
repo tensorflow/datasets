e91ef7dab9223f7b18b1015170a1dd598241049e
Richard Xiao <52942327+RichardXiao13@users.noreply.github.com>
2020-01-10 22:35:40 -0600

    Update icoseg_test.py

--- tensorflow_datasets/image/icoseg_test.py
+++ tensorflow_datasets/image/icoseg_test.py
@@ -11,16 +11,9 @@ class IcosegTest(testing.DatasetBuilderTestCase):
   # TODO(icoseg):
   DATASET_CLASS = icoseg.Icoseg
   SPLITS = {
-      "train": 3,  # Number of fake train example
-      "test": 1,  # Number of fake test example
+      "train": 3, 
+      "test": 1,  
   }
-
-  # If you are calling `download/download_and_extract` with a dict, like:
-  #   dl_manager.download({'some_key': 'http://a.org/out.txt', ...})
-  # then the tests needs to provide the fake output paths relative to the
-  # fake data directory
-  # DL_EXTRACT_RESULT = {'some_key': 'output_file1.txt', ...}
-
 
 if __name__ == "__main__":
   testing.test_main()
