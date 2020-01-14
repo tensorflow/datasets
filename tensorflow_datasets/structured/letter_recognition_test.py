c9276585f4c8d4c12651170bb9ece5af8d2532ec
boron <31139873+boronhub@users.noreply.github.com>
2020-01-15 01:14:58 +0530

    Update letter_recognition_test.py

--- tensorflow_datasets/structured/letter_recognition_test.py
+++ tensorflow_datasets/structured/letter_recognition_test.py
@@ -1,4 +1,4 @@
-"""TODO(letter_recognition): Add a description here."""
+"""Database of character image features; try to identify the letter."""
 
 from __future__ import absolute_import
 from __future__ import division
@@ -7,16 +7,16 @@ from __future__ import print_function
 from tensorflow_datasets import testing
 from tensorflow_datasets.structured import letter_recognition
 
-
 class LetterRecognitionTest(testing.DatasetBuilderTestCase):
 
   DATASET_CLASS = letter_recognition.LetterRecognition
   SPLITS = {
+
       "train": 1,
+
   }
 
   DL_EXTRACT_RESULT = {'letter-recognition.data'}
-
 
 if __name__ == "__main__":
   testing.test_main()
