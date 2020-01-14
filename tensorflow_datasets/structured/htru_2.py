3aeb92a29f7631131ed35aaff728fd4742ba1b7c
jedlimlx <42632089+jedlimlx@users.noreply.github.com>
2020-01-14 20:48:15 +0800

    Update and rename HTRU2_test.py to htru_2.py

--- tensorflow_datasets/structured/htru_2.py
+++ tensorflow_datasets/structured/htru_2.py
@@ -1,14 +1,14 @@
-"""Test for tensorflow_datasets.structured.HTRU2"""
+"""Test for tensorflow_datasets.structured.htru_2"""
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 from tensorflow_datasets import testing
-from tensorflow_datasets.structured import HTRU2
+from tensorflow_datasets.structured import htru2
 
 class Htru2Test(testing.DatasetBuilderTestCase):
-  DATASET_CLASS = HTRU2.Htru2
+  DATASET_CLASS = htru2.Htru2
   SPLITS = {
       "train": 10
   }
@@ -18,4 +18,3 @@ class Htru2Test(testing.DatasetBuilderTestCase):
 
 if __name__ == "__main__":
   testing.test_main()
-
