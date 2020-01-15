2f9d9a07f4e2f98a85daaaeae23dc7e5751f152d
boron <31139873+boronhub@users.noreply.github.com>
2020-01-15 17:42:28 +0530

    Update letter_recognition.py

--- tensorflow_datasets/structured/letter_recognition.py
+++ tensorflow_datasets/structured/letter_recognition.py
@@ -5,6 +5,9 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow_datasets.public_api as tfds
+import csv
+import tensorflow.compat.v2 as tf
+import tensorflow_datasets.public_api as tfds
 
 _CITATION = """
 @misc{Dua:2019 ,
@@ -20,10 +23,6 @@ The objective is to identify each of a large number of black-and-white rectangul
 The character images were based on 20 different fonts and each letter within these 20 fonts was randomly distorted to produce a file of 20,000 unique stimuli. Each stimulus was converted into 16 primitive numerical attributes (statistical moments and edge counts) which were then scaled to fit into a range of integer values from 0 through 15. We typically train on the first 16000 items and then use the resulting model to predict the letter category for the remaining 4000. See the article cited above for more details.
 """
 
-import csv
-import tensorflow.compat.v2 as tf
-import tensorflow_datasets.public_api as tfds
-
 class LetterRecognition(tfds.core.GeneratorBasedBuilder):
   """Database of character image features; try to identify the letter"""
 
@@ -37,7 +36,6 @@ class LetterRecognition(tfds.core.GeneratorBasedBuilder):
 
             "label": tfds.features.ClassLabel(names=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
                                                      'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']),
-          
             "features": tfds.features.Tensor(shape=(16,), dtype=tf.int32)
 
         }),
@@ -52,13 +50,11 @@ class LetterRecognition(tfds.core.GeneratorBasedBuilder):
         tfds.core.SplitGenerator(
             name=tfds.Split.TRAIN,
             gen_kwargs={
-
                 "file_path": data
-              
             },
         ),
     ]
-
+  
   def _generate_examples(self, file_path):
     with tf.io.gfile.GFile(file_path) as f:
       raw_data = csv.reader(f)
