e8def4cafd977a69ee1331d19a4d677a1b61b70f
Rachin Kalakheti <41832161+akalakheti@users.noreply.github.com>
2020-01-16 18:13:10 +0545

    Update wisconsin_breast_cancer.py

--- tensorflow_datasets/structured/wisconsin_breast_cancer.py
+++ tensorflow_datasets/structured/wisconsin_breast_cancer.py
@@ -23,6 +23,7 @@ import tensorflow.compat.v2 as tf
 import tensorflow_datasets.public_api as tfds
 
 DATA_URL = "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data"
+
 _CITATION = """\
 @misc{Dua:2019 ,
 author = "Dua, Dheeru and Graff, Casey",
@@ -31,16 +32,14 @@ title = "{UCI} Machine Learning Repository",
 url = "http://archive.ics.uci.edu/ml",
 institution = "University of California, Irvine, School of Information and Computer Sciences" }
 """
+
 _DESCRIPTION = """\
 Features are computed from a digitized image of a fine needle aspirate (FNA) of a breast mass. They describe characteristics of the cell nuclei present in the image. n the 3-dimensional space is that described in: [K. P. Bennett and O. L. Mangasarian: "Robust Linear Programming Discrimination of Two Linearly Inseparable Sets", Optimization Methods and Software 1, 1992, 23-34].
 This database is also available through the UW CS ftp server: ftp ftp.cs.wisc.edu cd math-prog/cpo-dataset/machine-learn/WDBC/
 Also can be found on UCI Machine Learning Repository: https://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+%28Diagnostic%29
 
 Attribute Information:
-
-1) ID number 2) Diagnosis (M = malignant, B = benign) 3-32)
-
-Ten real-valued features are computed for each cell nucleus:
+1) ID number 2) Diagnosis (M = malignant, B = benign) 3-32) Ten real-valued features are computed for each cell nucleus:
 a) radius (mean of distances from center to points on the perimeter) b) texture (standard deviation of gray-scale values) c) perimeter d) area e) smoothness (local variation in radius lengths) f) compactness (perimeter^2 / area - 1.0) g) concavity (severity of concave portions of the contour) h) concave points (number of concave portions of the contour) i) symmetry j) fractal dimension ("coastline approximation" - 1)
 The mean, standard error and "worst" or largest (mean of the three largest values) of these features were computed for each image, resulting in 30 features. For instance, field 3 is Mean Radius, field 13 is Radius SE, field 23 is Worst Radius.
 All feature values are recoded with four significant digits.
@@ -64,7 +63,6 @@ class WisconsinBreastCancer(tfds.core.GeneratorBasedBuilder):
     return tfds.core.DatasetInfo(
         builder=self,
         description=_DESCRIPTION,
-
         features=tfds.features.FeaturesDict({
             "features":
                 tfds.features.Tensor(shape=(32,), dtype=tf.float32),
@@ -83,12 +81,10 @@ class WisconsinBreastCancer(tfds.core.GeneratorBasedBuilder):
     records = [l for l in all_lines if l]  
 
 
-    return [
-        tfds.core.SplitGenerator(
+    return [ tfds.core.SplitGenerator(
             name=tfds.Split.TRAIN,
             num_shards=1,
-            gen_kwargs={"records": records}),
-    ]
+            gen_kwargs={"records": records})]
 
   def _generate_examples(self, records):
     for i, row in enumerate(records):
