79722b4472b63bd21181ed4494e94ae3d2e8b791
Richard Xiao <richard.xiao13@gmail.com>
2020-01-20 14:51:20 -0600

    Update icoseg.py

--- tensorflow_datasets/image/icoseg.py
+++ tensorflow_datasets/image/icoseg.py
@@ -29,32 +29,31 @@ _HOMEPAGE_URL = "http://chenlab.ece.cornell.edu/projects/touch-coseg/"
 class Icoseg(tfds.core.GeneratorBasedBuilder):
   """iCoseg dataset for segmentation."""
 
-  VERSION = tfds.core.Version('2.0.0')
+  VERSION = tfds.core.Version('0.0.1')
 
   def _info(self):
     return tfds.core.DatasetInfo(
-      builder=self,
-      description=_DESCRIPTION,
-      features=tfds.features.FeaturesDict({
+        builder=self,
+        description=_DESCRIPTION,
+        features=tfds.features.FeaturesDict({
             "image": tfds.features.Image(shape=(None, None, 3), encoding_format="jpeg"),
-        "label_png": tfds.features.Image(shape=(None, None, 1), encoding_format="png"),
-      }),
-      supervised_keys=("image", "label_png"),
-      homepage=_HOMEPAGE_URL,
-      citation=_CITATION,
+            "label": tfds.features.Image(shape=(None, None, 1), encoding_format="png"),
+        }),
+        supervised_keys=("image", "label_png"),
+        homepage=_HOMEPAGE_URL,
+        citation=_CITATION,
     )
 
   def _split_generators(self, dl_manager):
     extracted_dir = dl_manager.download_and_extract(_URL)
     return [
-      tfds.core.SplitGenerator(
-        name=tfds.Split.TRAIN,
-        num_shards=4,
-        gen_kwargs={
-          "imgs_dir_path": os.path.join(extracted_dir, "dataset_public", "images"),
-          "labels_dir_path": os.path.join(extracted_dir, "dataset_public", "ground_truth"),
-        }
-      ),
+        tfds.core.SplitGenerator(
+            name=tfds.Split.TRAIN,
+            gen_kwargs={
+                "imgs_dir_path": os.path.join(extracted_dir, "dataset_public", "images"),
+                "labels_dir_path": os.path.join(extracted_dir, "dataset_public", "ground_truth"),
+            }
+        ),
     ]
 
   def _generate_examples(self, imgs_dir_path, labels_dir_path):
@@ -67,8 +66,7 @@ class Icoseg(tfds.core.GeneratorBasedBuilder):
           no_ext = os.path.splitext(img_name)
           label_img_name = no_ext[0] + ".png"
           label_img_path = os.path.join(label_dir_path, label_img_name)
-          features = {
-                "image": img_path,
-                "label_png": label_img_path,
+          yield img_name, {
+              "image": img_path,
+              "label": label_img_path,
           }
-          yield img_name, features
