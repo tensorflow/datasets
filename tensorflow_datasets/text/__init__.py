1987f1059b8b4e05f990e2168e9ac6d7f789e4d6
TensorFlow Datasets Team <no-reply@google.com>
2020-01-27 08:59:51 -0800

    Add eraser_multi_rc dataset
    PiperOrigin-RevId: 291732980

--- tensorflow_datasets/text/__init__.py
+++ tensorflow_datasets/text/__init__.py
@@ -19,6 +19,7 @@ from tensorflow_datasets.text.c4 import C4
 from tensorflow_datasets.text.civil_comments import CivilComments
 from tensorflow_datasets.text.cos_e import CosE
 from tensorflow_datasets.text.definite_pronoun_resolution import DefinitePronounResolution
+from tensorflow_datasets.text.eraser_multi_rc import EraserMultiRc
 from tensorflow_datasets.text.esnli import Esnli
 from tensorflow_datasets.text.gap import Gap
 from tensorflow_datasets.text.glue import Glue
