74b016523f568766bc27fa36104bd7edbfc47a60
Somdev Basu <somdevbasu100@gmail.com>
2020-01-29 01:50:11 +0530

    Update wikipedia_dump_test.py

--- tensorflow_datasets/text/wikipedia_dump_test.py
+++ tensorflow_datasets/text/wikipedia_dump_test.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for tiny Shakespeare dataset."""
+"""Tests for Wikipedia Dump(Sentences) dataset."""
 
 from __future__ import absolute_import
 from __future__ import division
