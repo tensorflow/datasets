f576e986266d8a79013d79cbf5d34547c0eb4ec8
vijayphoenix <cs17btech11040@iith.ac.in>
2020-01-28 23:56:16 +0530

    Add fake affnist dataset

--- tensorflow_datasets/image/affnist_test.py
+++ tensorflow_datasets/image/affnist_test.py
@@ -1,28 +1,21 @@
-"""TODO(affnist): Add a description here."""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 from tensorflow_datasets import testing
-from tensorflow_datasets.image import affnist
-
 
 class AffnistTest(testing.DatasetBuilderTestCase):
-  # TODO(affnist):
   DATASET_CLASS = affnist.Affnist
   SPLITS = {
-      "train": 3,  # Number of fake train example
-      "test": 1,  # Number of fake test example
+      "train": 8,
+      "validation": 1,
+      "test": 1,
   }
-
-  # If you are calling `download/download_and_extract` with a dict, like:
-  #   dl_manager.download({'some_key': 'http://a.org/out.txt', ...})
-  # then the tests needs to provide the fake output paths relative to the
-  # fake data directory
-  # DL_EXTRACT_RESULT = {'some_key': 'output_file1.txt', ...}
-
+  DL_EXTRACT_RESULT = {
+      "train_data": "training_batches",
+      "validation_data": "validation_batches",
+      "test_data": "test_batches",
+  }
 
 if __name__ == "__main__":
   testing.test_main()
-
