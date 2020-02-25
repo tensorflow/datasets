af68c5d2d2f1e0974d48027c54d84abd33b630bb
TensorFlow Datasets Team <no-reply@google.com>
2020-02-25 10:02:04 -0800

    Add Speech Commands to TFDS
    PiperOrigin-RevId: 297141265

--- tensorflow_datasets/audio/__init__.py
+++ tensorflow_datasets/audio/__init__.py
@@ -19,3 +19,4 @@ from tensorflow_datasets.audio.groove import Groove
 from tensorflow_datasets.audio.librispeech import Librispeech
 from tensorflow_datasets.audio.librispeech import LibrispeechConfig
 from tensorflow_datasets.audio.nsynth import Nsynth
+from tensorflow_datasets.audio.speech_commands import SpeechCommands
