db14ea61e5631a2fa1eb6bf9df8bf3523e4d4289
Eshan-Agarwal <eshanagarwal12@gmail.com>
2020-02-28 21:14:10 +0530

    Update QMNIST

--- tensorflow_datasets/image/qmnist.py
+++ tensorflow_datasets/image/qmnist.py
@@ -15,20 +15,17 @@
 
 
 
-"""Dataset class for qmnist Dataset."""
+"""QMNIST."""
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import os
+import codecs
 import numpy as np
 from six.moves import urllib
 import tensorflow.compat.v2 as tf
-from keras import backend as K
 import tensorflow_datasets.public_api as tfds
-import codecs
-
 
 _QMNIST_URL = "https://raw.githubusercontent.com/facebookresearch/qmnist/master/"
 _QMNIST_TRAIN_DATA_FILENAME = "qmnist-train-images-idx3-ubyte.gz"
@@ -45,8 +42,7 @@ _TEST_EXAMPLES = 60000
 
 
 _CITATION = """\
-
-  @article{DBLP:journals/corr/abs-1905-10498,
+@article{DBLP:journals/corr/abs-1905-10498,
   author    = {Chhavi Yadav and
                L{\'{e}}on Bottou},
   title     = {Cold Case: The Lost {MNIST} Digits},
@@ -74,28 +70,20 @@ to provide meaninful confidence intervals.
 
 
 class Qmnist(tfds.core.GeneratorBasedBuilder):
-  """QMNIST Dataset"""
+  """QMNIST."""
 
   URL = _QMNIST_URL
   VERSION = tfds.core.Version('1.0.0')
 
   def _info(self):
-    # Specifies the tfds.core.DatasetInfo object
     return tfds.core.DatasetInfo(
         builder=self,
-        # This is the description that will appear on the datasets page.
         description=_DESCRIPTION,
-        # tfds.features.FeatureConnectors
         features=tfds.features.FeaturesDict({
-            # These are the features of your dataset like images, labels ...
-            "image": tfds.features.Image(shape = QMNIST_IMAGE_SHAPE),
-            "label": tfds.features.ClassLabel(num_classes = QMNIST_NUM_CLASSES),
+            "image": tfds.features.Image(shape=QMNIST_IMAGE_SHAPE),
+            "label": tfds.features.ClassLabel(num_classes=QMNIST_NUM_CLASSES),
         }),
-        # If there's a common (input, target) tuple from the features,
-        # specify them here. They'll be used if as_supervised=True in
-        # builder.as_dataset.
         supervised_keys=("image", "label"),
-        # Homepage of the dataset for documentation
         homepage="https://github.com/facebookresearch/qmnist/blob/master/README.md",
         citation=_CITATION,
     )
@@ -103,8 +91,6 @@ class Qmnist(tfds.core.GeneratorBasedBuilder):
   def _split_generators(self, dl_manager):
     """Returns SplitGenerators."""
     # Download the full QMNIST Database
-    # dl_manager is a tfds.download.DownloadManager that can be used to
-    # download and extract URLs
     filenames = {
         "train_data": _QMNIST_TRAIN_DATA_FILENAME,
         "train_labels": _QMNIST_TRAIN_LABELS_FILENAME,
@@ -116,7 +102,6 @@ class Qmnist(tfds.core.GeneratorBasedBuilder):
 
     # QMNIST provides TRAIN and TEST splits, not a VALIDATION split, so we only
     # write the TRAIN and TEST splits to disk.
-
     return [
         tfds.core.SplitGenerator(
             name=tfds.Split.TRAIN,
@@ -147,11 +132,10 @@ class Qmnist(tfds.core.GeneratorBasedBuilder):
     Yields:
       Generator yielding the next examples
     """
-    images = _extract_qmnist_images(data_path,num_examples)
-    labels = _extract_qmnist_labels(label_path,num_examples)
+    images = _extract_qmnist_images(data_path, num_examples)
+    labels = _extract_qmnist_labels(label_path)
     data = list(zip(images, labels))
 
-  
     # Using index as key since data is always loaded in same order.
     for index, (image, label) in enumerate(data):
       record = {"image": image, "label": label}
@@ -159,27 +143,24 @@ class Qmnist(tfds.core.GeneratorBasedBuilder):
 
 
 def _extract_qmnist_images(image_filepath, num_images):
-    with tf.io.gfile.GFile(image_filepath, "rb") as f:
-      f.read(16)  # header
-      buf = f.read(_QMNIST_IMAGE_SIZE * _QMNIST_IMAGE_SIZE * num_images)
-      data = np.frombuffer(
-          buf,
-          dtype=np.uint8,
-      ).reshape(num_images, _QMNIST_IMAGE_SIZE, _QMNIST_IMAGE_SIZE, 1)
-      return data
+  with tf.io.gfile.GFile(image_filepath, "rb") as f:
+    f.read(16)
+    buf = f.read(_QMNIST_IMAGE_SIZE * _QMNIST_IMAGE_SIZE * num_images)
+    data = np.frombuffer(
+        buf,
+        dtype=np.uint8,
+    ).reshape(num_images, _QMNIST_IMAGE_SIZE, _QMNIST_IMAGE_SIZE, 1)
+    return data
 
-def _extract_qmnist_labels(labels_filepath, num_labels):
-    with tf.io.gfile.GFile(labels_filepath, "rb") as f:
-        #f.read(8)  # header
-        data = f.read()
-        assert get_int(data[:4]) == 12*256 + 2
-        length = get_int(data[4:8])
-        width = get_int(data[8:12])
-        labels = np.frombuffer(data, dtype=np.dtype('>i4'), offset=12).reshape(length, width).astype(np.dtype('i4'))
-
-        return labels[:,0]
+def _extract_qmnist_labels(labels_filepath):
+  with tf.io.gfile.GFile(labels_filepath, "rb") as f:
+    data = f.read()
+    assert get_int(data[:4]) == 12*256 + 2
+    length = get_int(data[4:8])
+    width = get_int(data[8:12])
+    labels = np.frombuffer(data, dtype=np.dtype('>i4'), offset=12)
+    labels = labels.reshape(length, width).astype(np.dtype('i4'))
+    return labels[:, 0]
 
 def get_int(b):
-    return int(codecs.encode(b, 'hex'), 16)
-
-
+  return int(codecs.encode(b, 'hex'), 16)
--- tensorflow_datasets/image/qmnist_test.py
+++ tensorflow_datasets/image/qmnist_test.py
@@ -1,6 +1,19 @@
+# coding=utf-8
+# Copyright 2020 The TensorFlow Datasets Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-
-"""Tests for qmnist dataset module."""
+"""TEST FOR QMNIST DATASET"""
 
 from __future__ import absolute_import
 from __future__ import division
@@ -15,7 +28,6 @@ qmnist._TRAIN_EXAMPLES = 10  # pylint: disable=protected-access
 qmnist._TEST_EXAMPLES = 2  # pylint: disable=protected-access
 
 class QmnistTest(testing.DatasetBuilderTestCase):
-  # TODO(qmnist):
   DATASET_CLASS = qmnist.QMNIST
   SPLITS = {
       "train": 10,
@@ -28,13 +40,5 @@ class QmnistTest(testing.DatasetBuilderTestCase):
       "test_labels": "test-label",
   }
 
-  # If you are calling `download/download_and_extract` with a dict, like:
-  #   dl_manager.download({'some_key': 'http://a.org/out.txt', ...})
-  # then the tests needs to provide the fake output paths relative to the
-  # fake data directory
-  # DL_EXTRACT_RESULT = {'some_key': 'output_file1.txt', ...}
-
-
 if __name__ == "__main__":
   testing.test_main()
-
