cba454dda581d287300105a366e8b64bd7f34797
Rohan Reddy <43641071+rohanreddych@users.noreply.github.com>
2020-03-01 15:30:28 +0530

    Update cleanup_script.py

--- tensorflow_datasets/scripts/cleanup_script.py
+++ tensorflow_datasets/scripts/cleanup_script.py
@@ -1,2 +1,9 @@
 import os
-
+#to import the file registered.py
+file_path = os.path.join(os.pardir, "core")
+sys.path.insert(1, os.path.abspath(1, file_path))
+import registered
+registered_versions =[]
+a = registered.iter_dataset_full_names()
+for i in a:
+  registered_version.append(i)
