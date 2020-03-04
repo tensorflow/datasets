48222b0e45be5b115c2f57a209ad374b9628ed4b
Varan Rohila <varansinghrohila@gmail.com>
2020-03-04 13:36:01 +0530

    Added UCI Adult Dataset with proper ClassLabels

---
+++ tensorflow_datasets/structured/adult_test.py
@@ -0,0 +1,24 @@
+"""UCI Adult Dataset, contains various information of adults"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from tensorflow_datasets import testing
+from tensorflow_datasets.structured import adult
+
+
+class AdultTest(testing.DatasetBuilderTestCase):
+  # TODO(adult):
+  DATASET_CLASS = adult.Adult
+  SPLITS = {
+      "train": 10,  # Number of fake train example
+      "test": 5,  # Number of fake test example
+  }
+  DL_EXTRACT_RESULT = {"train_data": "adult.data", "test_data": "adult.test"}
+
+
+
+if __name__ == "__main__":
+  testing.test_main()
+
