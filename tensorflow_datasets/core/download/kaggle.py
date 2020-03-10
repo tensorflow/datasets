4b8deb5b3c90afe25e307e7829e41ed726158193
Eshan Agarwal <eshanagarwal12@gmail.com>
2020-03-10 21:07:30 +0530

    remove from __future__ import *

--- tensorflow_datasets/core/download/kaggle.py
+++ tensorflow_datasets/core/download/kaggle.py
@@ -15,10 +15,6 @@
 
 """Data downloads using the Kaggle CLI."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import os
 import subprocess as sp
 
