f2d00a2e30b1756b886b736c20fa583b28bbe2da
Eshan Agarwal <eshanagarwal12@gmail.com>
2020-03-10 20:09:35 +0530

    Update qmnist.py

--- tensorflow_datasets/image/qmnist.py
+++ tensorflow_datasets/image/qmnist.py
@@ -13,13 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-
+# Lint as: python3
 """QMNIST."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
 import codecs
 import numpy as np
