83a9bea166aceee086a2561180c2e0513688e962
TensorFlow Datasets Team <no-reply@google.com>
2020-03-11 12:25:24 -0700

    Add tests for TFCO colabs
    PiperOrigin-RevId: 300381880

--- tensorflow_datasets/core/utils/gcs_utils.py
+++ tensorflow_datasets/core/utils/gcs_utils.py
@@ -25,7 +25,7 @@ import tensorflow.compat.v2 as tf
 
 from tensorflow_datasets.core import utils
 
-GCS_URL = "http://storage.googleapis.com"
+GCS_URL = "https://storage.googleapis.com"
 
 # for dataset_info/
 GCS_BUCKET = posixpath.join(GCS_URL, "tfds-data")
@@ -59,12 +59,16 @@ def gcs_files(prefix_filter=None):
   return filenames
 
 
+
+
 def gcs_dataset_info_files(dataset_dir):
   """Return paths to GCS files in the given dataset directory."""
   prefix = posixpath.join(GCS_DATASET_INFO_DIR, dataset_dir, "")
   # Filter for this dataset
-  filenames = [el for el in gcs_files(prefix_filter=prefix)
-               if el.startswith(prefix) and len(el) > len(prefix)]
+  filenames = [
+      el for el in gcs_files(prefix_filter=prefix)
+      if el.startswith(prefix) and len(el) > len(prefix)
+  ]
   return filenames
 
 
@@ -74,27 +78,32 @@ def is_dataset_on_gcs(dataset_name):
   return len(gcs_files(prefix_filter=dir_name)) > 2
 
 
-def download_gcs_dataset(
-    dataset_name, local_dataset_dir, max_simultaneous_downloads=50):
+def download_gcs_dataset(dataset_name,
+                         local_dataset_dir,
+                         max_simultaneous_downloads=50):
   """Downloads prepared GCS dataset to local dataset directory."""
   prefix = posixpath.join(GCS_DATASETS_DIR, dataset_name)
   gcs_paths_to_dl = gcs_files(prefix)
 
   # Filter out the diffs folder if present
   filter_prefix = posixpath.join(prefix, "diffs")
-  gcs_paths_to_dl = [p for p in gcs_paths_to_dl
-                     if not p.startswith(filter_prefix)]
+  gcs_paths_to_dl = [
+      p for p in gcs_paths_to_dl if not p.startswith(filter_prefix)
+  ]
 
   with utils.async_tqdm(
       total=len(gcs_paths_to_dl), desc="Dl Completed...", unit=" file") as pbar:
+
     def _copy_from_gcs(gcs_path):
-      local_path = posixpath.join(
-          local_dataset_dir, posixpath.basename(gcs_path))
+      local_path = posixpath.join(local_dataset_dir,
+                                  posixpath.basename(gcs_path))
       download_gcs_file(gcs_path, local_path)
       pbar.update(1)
+
     with concurrent.futures.ThreadPoolExecutor(
         max_workers=max_simultaneous_downloads) as executor:
       futures = [
-          executor.submit(_copy_from_gcs, path) for path in gcs_paths_to_dl]
+          executor.submit(_copy_from_gcs, path) for path in gcs_paths_to_dl
+      ]
       for future in concurrent.futures.as_completed(futures):
         future.result()
