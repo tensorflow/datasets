3111c68835d76b3a1316449ece7eaea06d04aaa9
Varan Rohila <varansinghrohila@gmail.com>
2020-03-15 01:14:52 +0530

    added fake example generator and examples

--- tensorflow_datasets/structured/adult_test.py
+++ tensorflow_datasets/structured/adult_test.py
@@ -11,12 +11,10 @@ from tensorflow_datasets.structured import adult
 class AdultTest(testing.DatasetBuilderTestCase):
   DATASET_CLASS = adult.Adult
   SPLITS = {
-      "train": 10,  # Number of fake train example
-      "test": 5,  # Number of fake test example
+      "train": 5,  # Number of fake train example
+      "test": 3,  # Number of fake test example
   }
   DL_EXTRACT_RESULT = {"train_data": "adult.data", "test_data": "adult.test"}
-
-
 
 if __name__ == "__main__":
   testing.test_main()
