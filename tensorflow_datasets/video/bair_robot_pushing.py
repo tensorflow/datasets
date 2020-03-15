e3f0d040850faefc1702207f1e19468aae08749d
Eshan Agarwal <eshanagarwal12@gmail.com>
2020-03-15 14:37:39 +0530

    Update bair_robot_pushing.py

--- tensorflow_datasets/video/bair_robot_pushing.py
+++ tensorflow_datasets/video/bair_robot_pushing.py
@@ -52,9 +52,6 @@ class BairRobotPushingSmall(tfds.core.GeneratorBasedBuilder):
 
   VERSION = tfds.core.Version(
       "2.0.0", "New split API (https://tensorflow.org/datasets/splits)")
-  SUPPORTED_VERSIONS = [
-      tfds.core.Version("1.0.0", experiments={tfds.core.Experiment.S3: False})
-  ]
 
   def _info(self):
     """Create Dataset Info."""
@@ -83,13 +80,11 @@ class BairRobotPushingSmall(tfds.core.GeneratorBasedBuilder):
     return [
         tfds.core.SplitGenerator(
             name=tfds.Split.TRAIN,
-            num_shards=10,
             gen_kwargs={
                 "filedir": os.path.join(files, "softmotion30_44k", "train"),
             }),
         tfds.core.SplitGenerator(
             name=tfds.Split.TEST,
-            num_shards=4,
             gen_kwargs={
                 "filedir": os.path.join(files, "softmotion30_44k", "test"),
             }),
