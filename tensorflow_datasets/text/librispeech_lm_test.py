a3dbdbfb09ada9402290a1ce06f1eae3865f40bc
Eshan Agarwal <eshanagarwal12@gmail.com>
2020-03-15 20:20:41 +0530

    Update librispeech_lm_test.py

--- tensorflow_datasets/text/librispeech_lm_test.py
+++ tensorflow_datasets/text/librispeech_lm_test.py
@@ -16,9 +16,6 @@
 # Lint as: python3
 """Tests for LibrispeechLm dataset builder."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
 from tensorflow_datasets import testing
 from tensorflow_datasets.text import librispeech_lm
@@ -34,4 +31,3 @@ class LibrispeechLmTest(testing.DatasetBuilderTestCase):
 
 if __name__ == "__main__":
   testing.test_main()
-
