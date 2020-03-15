81885ef42c0ccb8477021e572968665d33cdba58
Eshan Agarwal <eshanagarwal12@gmail.com>
2020-03-15 20:12:22 +0530

    Update eraser_multi_rc_test.py

--- tensorflow_datasets/text/eraser_multi_rc_test.py
+++ tensorflow_datasets/text/eraser_multi_rc_test.py
@@ -16,9 +16,6 @@
 # Lint as: python3
 """Tests for eraser_multi_rc dataset."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
 from tensorflow_datasets import testing
 from tensorflow_datasets.text import eraser_multi_rc
@@ -35,5 +32,3 @@ class EraserMultiRcTest(testing.DatasetBuilderTestCase):
 
 if __name__ == "__main__":
   testing.test_main()
-
-
