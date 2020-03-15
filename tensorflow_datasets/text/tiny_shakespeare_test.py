77c20b2039bc2d9fc0a071bbd8b4121a11e77054
Eshan Agarwal <eshanagarwal12@gmail.com>
2020-03-15 20:32:29 +0530

    Update tiny_shakespeare_test.py

--- tensorflow_datasets/text/tiny_shakespeare_test.py
+++ tensorflow_datasets/text/tiny_shakespeare_test.py
@@ -16,10 +16,6 @@
 # Lint as: python3
 """Tests for tiny Shakespeare dataset."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 from tensorflow_datasets import testing
 from tensorflow_datasets.text import tiny_shakespeare
 
@@ -41,4 +37,3 @@ class TinyShakespeareTest(testing.DatasetBuilderTestCase):
 
 if __name__ == "__main__":
   testing.test_main()
-
