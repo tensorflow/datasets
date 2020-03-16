3627c40e992cfc7d584b5a758a0d64b61bfc656d
vvkio <io@vvk.io>
2020-03-16 20:43:06 +0100

    Added documentation and small refactor

--- tensorflow_datasets/scripts/document_dataset_with_example.py
+++ tensorflow_datasets/scripts/document_dataset_with_example.py
@@ -1,23 +1,38 @@
-import os
+"""
+Script used to generate documentation .md files with example figures
 
+This script uses scripts/generate_visualization.py to generate
+an example figure of a dataset, using show_examples() it gets a figure
+that is temporary stores in examples/ and later used when generating
+the documentation with document_single_builder(builder)
+
+Typical usage example:
+
+  ds_name = 'beans'
+  generate_docs_with_visualization(ds_name)
+
+"""
+
+import os
 import tensorflow_datasets as tfds
-from tensorflow_datasets.scripts import generate_visualization
 from tensorflow_datasets.scripts import document_datasets
 
 def generate_docs_with_visualization(ds_name):
-    # DatasetBuilder
-    builder = tfds.builder(ds_name)
+  """
+  Generates example figures of a dataset
+  """
+  # DatasetBuilder
+  builder = tfds.builder(ds_name)
 
-    # Download the dataset
-    builder.download_and_prepare()
+  # Download the dataset
+  builder.download_and_prepare()
 
-    # Construct a tf.data.Dataset
-    ds = builder.as_dataset(split='train')
+  # Construct a tf.data.Dataset
+  #ds = builder.as_dataset(split='train')
 
-    dst_dir = tfds.core.get_tfds_path('examples/')
+  dst_dir = tfds.core.get_tfds_path('examples/')
 
-    with open(os.path.join(dst_dir, f'{ds_name}.md'), "w") as f:
-
-        doc_builder = document_datasets.document_single_builder(builder)
-        f.write(doc_builder)
+  with open(os.path.join(dst_dir, f'{ds_name}.md'), "w") as f:
+    doc_builder = document_datasets.document_single_builder(builder)
+    f.write(doc_builder)
 
--- tensorflow_datasets/scripts/generate_visualization.py
+++ tensorflow_datasets/scripts/generate_visualization.py
@@ -1,18 +1,33 @@
-from matplotlib import figure as fig
-from matplotlib import pyplot as plt
-import tensorflow_datasets as tfds
-from tensorflow_datasets import testing
+"""
+Script for generating dataset example figures
 
-figures_dir = ('examples/')
+Uses show_examples() to generate example figures of a dataset
+and stores it temporary in examples/
+
+Typical usage example:
+
+  ds_name = 'beans'
+  generate_visualization(ds_name)
+
+"""
+
+import tensorflow_datasets as tfds
+import matplotlib.pyplot
+
+FIG_DIR = ('examples/')
 
 def generate_visualization(ds_name):
-    try:
-        ds, ds_info = tfds.load(name=ds_name, split='train', with_info=True)
-        fig = tfds.show_examples(ds_info, ds)
-        path_dir = tfds.core.get_tfds_path(figures_dir)
-        fig.savefig(path_dir+ds_name+'.png')
+  """
+  Method used to generate examples figures of a dataset
+  """
 
-        print('Saved '+ ds_name + '.png' + 'to' + path_dir)
-    except:
-        print('The selected dataset is not supported')
+  try:
+    ds, ds_info = tfds.load(name=ds_name, split='train', with_info=True)
+    path_dir = tfds.core.get_tfds_path(FIG_DIR)
+    print(path_dir)
+    fig = tfds.show_examples(ds_info, ds, plot_scale=2)
+    fig.savefig(path_dir + ds_name + '.png')
+    print('Saved '+ ds_name + '.png' + 'to' + path_dir)
+  except RuntimeError:
+    print('The selected dataset is not supported')
 
