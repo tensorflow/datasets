3f40a163a49f573991c0c71dd11645c63cebcb51
Noumanmufc1 <noumanmufc07@gmail.com>
2020-03-22 19:27:57 +0500

    remove future imports

--- tensorflow_datasets/structured/bbbp_test.py
+++ tensorflow_datasets/structured/bbbp_test.py
@@ -14,11 +14,8 @@
 # limitations under the License.
 
 # Lint as: python3
-"""Tests for tensorflow_datasets.structured.iris."""
+"""Tests for tensorflow_datasets.structured.bbbp"""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 from tensorflow_datasets import testing
 from tensorflow_datasets.structured import bbbp
 
