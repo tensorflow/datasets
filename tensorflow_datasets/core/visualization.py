25399746613de877d63ece174a5dbc1f16dfcc7d
Harshitadd <31439716+harshitadd@users.noreply.github.com>
2020-03-22 17:54:02 +0530

    Proper formatting and instantiation type check

--- tensorflow_datasets/core/visualization.py
+++ tensorflow_datasets/core/visualization.py
@@ -20,10 +20,6 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-from random import randint
-import IPython
-from scipy.io.wavfile import write
-import numpy as np  
 
 from absl import logging
 
@@ -34,21 +30,16 @@ from tensorflow_datasets.core import lazy_imports_lib
 
 def show_examples(ds_info, ds, rows=3, cols=3, plot_scale=3., image_key=None):
   """Visualize images (and labels) from an image classification dataset.
-
   Only works with datasets that have 1 image feature and optionally 1 label
   feature (both inferred from `ds_info`). Note the dataset should be unbatched.
   Requires matplotlib to be installed.
-
   This function is for interactive use (Colab, Jupyter). It displays and return
   a plot of (rows*columns) images from a tf.data.Dataset.
-
   Usage:
-
   ```python
   ds, ds_info = tfds.load('cifar10', split='train', with_info=True)
   fig = tfds.show_examples(ds_info, ds)
   ```
-
   Args:
     ds_info: The dataset info object to which extract the label and features
       info. Available either through `tfds.load('mnist', with_info=True)` or
@@ -63,20 +54,29 @@ def show_examples(ds_info, ds, rows=3, cols=3, plot_scale=3., image_key=None):
       the labels to get overlapped.
     image_key: `string`, name of the feature that contains the image. If not
        set, the system will try to auto-detect it.
-
   Returns:
     fig: The `matplotlib.Figure` object
   """
   plt = lazy_imports_lib.lazy_imports.matplotlib.pyplot
 
   if not image_key:
-    #Infer the image and label keys
+    # Infer the image and label keys
     image_keys = [
         k for k, feature in ds_info.features.items()
         if isinstance(feature, features_lib.Image)
-        ]
-  #If image_keys has been populated by image item instances 
-  if image_keys:
+    ]
+    
+    if not image_keys:
+        audio_keys = [
+            k for k, feature in ds_info.features.items()
+            if isinstance(feature, features_lib.Audio)
+                ]
+        
+    if not audio_keys:
+      raise ValueError(
+          "Visualisation not supported for dataset `{}`. Was not able to "
+          "auto-infer image or audio dataset.".format(ds_info.name))
+
     if len(image_keys) > 1:
       raise ValueError(
           "Multiple image features detected in the dataset. Using the first one. You can "
@@ -85,53 +85,51 @@ def show_examples(ds_info, ds, rows=3, cols=3, plot_scale=3., image_key=None):
 
     image_key = image_keys[0]
 
-    label_keys = [
-        k for k, feature in ds_info.features.items()
-        if isinstance(feature, features_lib.ClassLabel)
-    ]
+  label_keys = [
+      k for k, feature in ds_info.features.items()
+      if isinstance(feature, features_lib.ClassLabel)
+  ]
 
-    label_key = label_keys[0] if len(label_keys) == 1 else None
-    if not label_key:
-      logging.info("Was not able to auto-infer label.")
+  label_key = label_keys[0] if len(label_keys) == 1 else None
+  if not label_key:
+    logging.info("Was not able to auto-infer label.")
 
-    num_examples = rows * cols
-    examples = list(dataset_utils.as_numpy(ds.take(num_examples)))
+  num_examples = rows * cols
+  examples = list(dataset_utils.as_numpy(ds.take(num_examples)))
 
-    fig = plt.figure(figsize=(plot_scale*cols, plot_scale*rows))
-    fig.subplots_adjust(hspace=1/plot_scale, wspace=1/plot_scale)
-    for i, ex in enumerate(examples):
-      if not isinstance(ex, dict):
-        raise ValueError(
-            "tfds.show_examples requires examples as `dict`, with the same "
-            "structure as `ds_info.features`. It is currently not compatible "
-            "with `as_supervised=True`. Received: {}".format(type(ex)))
-      ax = fig.add_subplot(rows, cols, i+1)
+  fig = plt.figure(figsize=(plot_scale*cols, plot_scale*rows))
+  fig.subplots_adjust(hspace=1/plot_scale, wspace=1/plot_scale)
+  for i, ex in enumerate(examples):
+    if not isinstance(ex, dict):
+      raise ValueError(
+          "tfds.show_examples requires examples as `dict`, with the same "
+          "structure as `ds_info.features`. It is currently not compatible "
+          "with `as_supervised=True`. Received: {}".format(type(ex)))
+    ax = fig.add_subplot(rows, cols, i+1)
 
-      # Plot the image
-      image = ex[image_key]
-      if len(image.shape) != 3:
-        raise ValueError(
-            "Image dimension should be 3. tfds.show_examples does not support "
-            "batched examples or video.")
-      _, _, c = image.shape
-      if c == 1:
-        image = image.reshape(image.shape[:2])
-      ax.imshow(image, cmap="gray")
-      ax.grid(False)
-      plt.xticks([], [])
-      plt.yticks([], [])
+    # Plot the image
+    image = ex[image_key]
+    if len(image.shape) != 3:
+      raise ValueError(
+          "Image dimension should be 3. tfds.show_examples does not support "
+          "batched examples or video.")
+    _, _, c = image.shape
+    if c == 1:
+      image = image.reshape(image.shape[:2])
+    ax.imshow(image, cmap="gray")
+    ax.grid(False)
+    plt.xticks([], [])
+    plt.yticks([], [])
 
-      # Plot the label
-      if label_key:
-        label = ex[label_key]
-        label_str = ds_info.features[label_key].int2str(label)
-        plt.xlabel("{} ({})".format(label_str, label))
-    plt.show()
-    return fig
-
-  # if not image item instances 
-  if not image_key:
-    audio_samples=[]
+    # Plot the label
+    if label_key:
+      label = ex[label_key]
+      label_str = ds_info.features[label_key].int2str(label)
+      plt.xlabel("{} ({})".format(label_str, label))
+  plt.show()
+  return fig
+  if audio_keys:
+    audio_samples = []
     if(ds_info.name == 'ljspeech'):
       key = 'speech'
     else:
@@ -140,27 +138,26 @@ def show_examples(ds_info, ds, rows=3, cols=3, plot_scale=3., image_key=None):
     samplerate = 16000
     for features in ds:
         audio_samples.append(features[key].numpy())
-    to_gen=[]
+    to_gen = []
     for _ in range(4):
       value = randint(0, len(audio_samples))
       to_gen.append(audio_samples[value])
-        
+
     t1 = 0
     t2 = 100 * 1000
     for audio in to_gen:
       name = '/content/audio.wav'
       newAudio = audio[t1:t2]
-      write(name,samplerate,newAudio)
+      write(name, samplerate, newAudio)
       IPython.display.display(IPython.display.Audio(name)) 
 
-    fig,a =  plt.subplots(2,2)
+    fig, a = plt.subplots(2, 2)
 
     a[0][0].plot(to_gen[0])
     a[0][1].plot(to_gen[1])
     a[1][0].plot(to_gen[2])
     a[1][1].plot(to_gen[3])
     plt.show()
-
     return fig
-
-   
+        
+        
