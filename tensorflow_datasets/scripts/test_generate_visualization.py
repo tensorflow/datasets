6ad8f06be71fb089196c9b2d8d10fd1f996ca7bb
vvkio <io@vvk.io>
2020-03-26 22:33:28 +0100

    Updated styling and minor fix

--- tensorflow_datasets/scripts/test_generate_visualization.py
+++ tensorflow_datasets/scripts/test_generate_visualization.py
@@ -1,4 +1,4 @@
-import os
+from absl import app
 import tensorflow_datasets as tfds
 from tensorflow_datasets.scripts import generate_visualization
 from tensorflow_datasets.scripts import document_datasets
@@ -6,15 +6,16 @@ from tensorflow_datasets.scripts import document_dataset_with_example
 
 DATASET_TO_TESTS = ['beans']  # Datasets you want to test the script on.
 
-def main():
-    for ds_name in DATASET_TO_TESTS:
-        # load dataset
-        tfds.load(ds_name)
+def main(_):
+  for ds_name in DATASET_TO_TESTS:
+    # load dataset
+    tfds.load(ds_name)
 
-        # Generate figure for the dataset
-        generate_visualization.generate_visualization(ds_name)
+  # Generate figure for the dataset
+  generate_visualization.generate_visualization(ds_name)
 
-        # Generate documentation which example
-        document_dataset_with_example.generate_docs_with_visualization(ds_name)
+  # Generate documentation which example
+  document_dataset_with_example.generate_docs_with_visualization(ds_name)
 
-main()
+if __name__ == "__main__":
+  app.run(main)
