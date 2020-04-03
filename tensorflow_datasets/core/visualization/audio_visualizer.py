ac73594f6fe2601599c77801eb49a5744007b9a2
Harshitadd <31439716+harshitadd@users.noreply.github.com>
2020-04-03 16:11:50 +0530

    Class docstrings and code style

--- tensorflow_datasets/core/visualization/audio_visualizer.py
+++ tensorflow_datasets/core/visualization/audio_visualizer.py
@@ -1,20 +1,19 @@
-""" Audio Visualization
-"""
+""" Audio Visualizer."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import random
 
-
-from absl import logging
-
-from tensorflow_datasets.core import dataset_utils
 from tensorflow_datasets.core import features as features_lib
 from tensorflow_datasets.core import lazy_imports_lib
 from tensorflow_datasets.core.visualization import visualizer
 
+
 class AudioGridVisualizer(visualizer.Visualizer):
+  """ Fixed grid Visualizer for audio datasets."""
   def match(self, ds_info):
+    """ See base class."""
     audio_keys = visualizer.extract_keys(ds_info.features, features_lib.Audio)
     return len(audio_keys) > 0
 
@@ -25,28 +24,26 @@ class AudioGridVisualizer(visualizer.Visualizer):
       rows=2,
       cols=2,
       plot_scale=3.,
-      image_key=None,
+      audio_key=None,
   ):
-    """Display the dataset.
+    """Display the audio dataset.
 
       Args:
         ds_info: `tfds.core.DatasetInfo` object of the dataset to visualize.
-        ds: `tf.data.Dataset`. The tf.data.Dataset object to visualize. Examples
-          should not be batched. Examples will be consumed in order until
-          (rows * cols) are read or the dataset is consumed.
-        rows: `int`, number of rows of the display grid.
-        cols: `int`, number of columns of the display grid.
-        plot_scale: `float`, controls the plot size of the images. Keep this
-          value around 3 to get a good plot. High and low values may cause
-          the labels to get overlapped.
-        image_key: `string`, name of the feature that contains the image. If not
+        ds: `tf.data.Dataset`. The tf.data.Dataset object to visualize.
+        rows: `int`, number of rows of the display grid : Default is 2.Does not
+        support user defined input as of now.
+        cols: `int`, number of columns of the display grid : Default is 2.Does
+        not support user defined input as of now.
+        plot_scale: `float`, controls the plot size of the images. Not used for
+        audio grid plots.
+        audio_key: `string`, name of the feature that contains the audio. If not
           set, the system will try to auto-detect it.
       """
-    import random
-    import IPython.display
-    if not image_key:
+    import IPython.display as ipd
+    if not audio_key:
+      #Auto inferring the audio key
       audio_keys = visualizer.extract_keys(ds_info.features, features_lib.Audio)
-      
     key = audio_keys[0]
     audio_samples = []
 
@@ -57,16 +54,13 @@ class AudioGridVisualizer(visualizer.Visualizer):
     for _ in range(4):
       value = random.randint(0, len(audio_samples))
       to_gen.append(audio_samples[value])
-    
     t1 = 0
     t2 = 100 * 1000
     for audio in to_gen:
-      newAudio = audio[t1:t2]
-      IPython.display.Audio(newAudio, rate=samplerate) 
-      
+      audio = audio[t1:t2]
+      ipd.display(ipd.Audio(audio, rate=samplerate))
     plt = lazy_imports_lib.lazy_imports.matplotlib.pyplot
     fig, a = plt.subplots(2, 2)
-
     a[0][0].plot(to_gen[0])
     a[0][1].plot(to_gen[1])
     a[1][0].plot(to_gen[2])
