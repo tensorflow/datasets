4335f487f8cfdbe47221b62825030e5c5c1915f1
Eshan Agarwal <eshanagarwal12@gmail.com>
2020-04-08 08:35:55 +0530

    Update _sharded_files_test.py

--- tensorflow_datasets/core/_sharded_files_test.py
+++ tensorflow_datasets/core/_sharded_files_test.py
@@ -20,7 +20,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from tensorflow_datasets import testing
+import tensorflow_datasets as tfds
 from tensorflow_datasets.core import _sharded_files
 
 
