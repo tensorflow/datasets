717b380925b7cafa1b7cf1fe751ac5f33c5e9099
Etienne Pot <epot@google.com>
2020-04-15 19:28:32 -0700

    Update TFDS version
    Breaking changes:
    * Legacy mode `tfds.experiment.S3` has been removed
    * New  `tfds.image_classification` section and move there some datasets from `tfds.images`.
    * `in_memory` argument removed from `as_dataset`/`tfds.load` (small datasets are auto-cached).
    * DownloadConfig do not append the dataset name anymore (manual data should be in `<manual_dir>/` instead of `<manual_dir>/<dataset_name>/`)
    * Tests now check that all `dl_manager.download` urls has registered checksums. To opt-out, add `SKIP_CHECKSUMS
     = True` to your `DatasetBuilderTestCase`.
    * `tfds.load` now always returns `tf.compat.v2.Dataset`. If you're using still using `tf.compat.v1`:
       * Use `tf.compat.v1.data.make_one_shot_iterator(ds)` rather than `ds.make_one_shot_iterator()`
       * Use `isinstance(ds, tf.compat.v2.Dataset)` instead of `isinstance(ds, tf.data.Dataset)`
    * `tfds.Split.ALL` has been removed from the API.
    
    Future breaking change:
    * The tfds.features.text encoding API is deprecated. Please use [tensorflow_text](https://www.tensorflow.org/tutorials/tensorflow_text/intro) instead.
    * `num_shards` argument of `tfds.core.SplitGenerator` is currently ignored and will be removed in the next version.
    
    Features:
    * `DownloadManager` is now pickable (can be used inside Beam pipelines)
    * `tfds.features.Audio`:
      * Support float as returned value
      * Expose sample_rate through `info.features['audio'].sample_rate`
      * Support for encoding audio features from file objects
    * Various bug fixes, better error messages, documentation improvements
    * More datasets
    
    Thank you to all our contributors for helping us make TFDS better for everyone!
    
    PiperOrigin-RevId: 306768189

--- tensorflow_datasets/version_stable.py
+++ tensorflow_datasets/version_stable.py
@@ -20,8 +20,8 @@ This file overwrites `version.py` for stable releases.
 """
 
 # We follow Semantic Versioning (https://semver.org/)
-_MAJOR_VERSION = '2'
-_MINOR_VERSION = '1'
+_MAJOR_VERSION = '3'
+_MINOR_VERSION = '0'
 _PATCH_VERSION = '0'
 
 __version__ = '.'.join([
