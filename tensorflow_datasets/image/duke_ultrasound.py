5de1830121744f4693b538bd1a7ee416f10b6690
vijayphoenix <cs17btech11040@iith.ac.in>
2020-04-22 08:27:55 +0530

    Update duke_ultrasound

--- tensorflow_datasets/image/duke_ultrasound.py
+++ tensorflow_datasets/image/duke_ultrasound.py
@@ -55,7 +55,7 @@ reduction, contrast enhancement and improvement in conspicuity of
 anatomical structures. These data were collected with support from the
 National Institute of Biomedical Imaging and Bioengineering under Grant 
 R01-EB026574 and National Institutes of Health under Grant 5T32GM007171-44.
-A usage example is avalible 
+A usage example is available
 [here](https://colab.research.google.com/drive/1R_ARqpWoiHcUQWg1Fxwyx-ZkLi0IZ5qs)."""
 
 _DATA_URL = {
