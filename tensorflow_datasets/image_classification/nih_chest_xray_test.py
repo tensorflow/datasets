328d87af09b29fe1baeaffbc5ffa14531e74e79a
jason-zl190 <i.liangzisheng@gmail.com>
2020-05-20 20:08:13 -0400

    add test and fake data to test

--- tensorflow_datasets/image_classification/nih_chest_xray_test.py
+++ tensorflow_datasets/image_classification/nih_chest_xray_test.py
@@ -9,18 +9,18 @@ from tensorflow_datasets.image_classification import nih_chest_xray
 
 
 class NihChestXrayTest(tfds.testing.DatasetBuilderTestCase):
-  # TODO(nih_chest_xray):
   DATASET_CLASS = nih_chest_xray.NihChestXray
   SPLITS = {
       "train": 3,  # Number of fake train example
       "test": 1,  # Number of fake test example
   }
 
-  # If you are calling `download/download_and_extract` with a dict, like:
-  #   dl_manager.download({'some_key': 'http://a.org/out.txt', ...})
-  # then the tests needs to provide the fake output paths relative to the
-  # fake data directory
-  # DL_EXTRACT_RESULT = {'some_key': 'output_file1.txt', ...}
+  DL_EXTRACT_RESULT = {
+    'zipfile01': 'data01', # fake data directory
+    'data_entry_2017': 'fake_data_entry_2017.csv',
+    'train_val_list': 'train_val_list.txt',
+    'test_list': 'test_list.txt',
+  }
 
 
 if __name__ == "__main__":
