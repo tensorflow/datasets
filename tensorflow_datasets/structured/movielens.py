333dc4fec316388b6440cb7baf64b755905c724e
zweichen <zweichen@google.com>
2020-06-18 14:14:22 -0400

    Revised documentation and made other style revisions

--- tensorflow_datasets/structured/movielens.py
+++ tensorflow_datasets/structured/movielens.py
@@ -48,22 +48,49 @@ keywords = {Datasets, recommendations, ratings, MovieLens}
 """
 
 _DESCRIPTION = """
-This dataset describes 5-star rating from MovieLens,
-a movie recommendation service.
-Users were selected at random for inclusion.
-There are 5 versions included: 25m, latest-small, 100k, 1m, 20m.
-In all datasets, the movies data and ratings data are joined on "movieId".
-The 25m dataset, latest-small dataset, and 1m dataset have the same schema
-and they do not include demographic data.
-The 25m dataset is the latest stable version of the MovieLens dataset.
-It is recommended for research purposes.
-The latest-small dataset is a small dataset with
-the same schema as 25m and 20m.
-It is updated over time by GroupLens and its checksum might need updating.
-The 20m dataset is one of the most used MovieLens datasets in academic papers
+This dataset contains a set of movie ratings from the MovieLens website, a movie
+recommendation service. This dataset was collected and maintained by [GroupLens]
+(https://grouplens.org/), a research group at the University of Minnesota. There
+are 5 versions included: "25m", "latest-small", "100k", "1m", "20m". In all
+datasets, the movies data and ratings data are joined on "movieId". The 25m
+dataset, latest-small dataset, and 20m dataset contain only movie data and
+rating data. The 1m dataset and 100k dataset contain demographic data in
+addition to movie and rating data.
+
+- "25m": This is the latest stable version of the MovieLens dataset. It is
+recommended for research purposes.
+- "latest-small": This is a small subset of the latest version of the MovieLens
+dataset. It is updated over time by GroupLens and its checksum might need
+updating.
+- "100k": This is the oldest version of the MovieLens datasets. It is a small
+dataset with demographic data.
+- "1m": This is the largest MovieLens dataset that contains demographic data.
+- "20m": This is one of the most used MovieLens datasets in academic papers
 along with the 1m dataset.
-The 100k dataset and 1m dataset are the oldest MovieLens datasets.
-They both contain demographic data and have the same schema.
+
+Feature Description:
+The features below are included in all versions.
+
+- "movie_id": a unique identifier of the rated movie
+- "movie_title": the title of the rated movie with the release year in
+parentheses
+- "movie_genres": a list of genres to which the rated movie belongs
+- "user_id": a unique identifier of the user who made the rating
+- "user_rating": the score of the rating on a five-star scale
+- "timestamp": the timestamp of the ratings, represented in seconds since
+midnight Coordinated Universal Time (UTC) of January 1, 1970
+
+The "100k" and "1m" version in addition includes the following demographic
+features.
+
+- "user_gender": gender of the user who made the rating; a true value
+corresponds to male
+- "user_age": age of the user who made the rating
+- "user_occupation_label": the occupation of the user who made the rating
+represented by a label
+- "user_occupation_string": the occupation of the user who made the rating in
+the original string
+- "user_zip_code": the zip code of the user who made the rating
 """
 
 _DATASET_OPTIONS = ['25m', 'latest-small', '100k', '1m', '20m']
@@ -78,90 +105,70 @@ class MovieLens(tfds.core.GeneratorBasedBuilder):
       tfds.core.BuilderConfig(
           name='25m',
           description=textwrap.dedent("""\
-              This dataset contains 25000095 ratings across 62423 movies.
-              These data were created by 162541 users between January 09, 1995
-              and November 21, 2019.
-              Ratings are in half-star increments.
-              Each user has rated at least 20 movies.
-              This dataset is the latest stable version
-              of the MovieLens dataset.
-              This dataset does not include demographic data.
-              The movies data and ratings data are joined on "movieId".
-              Genres are converted into sequence of labels.
-              "(no genres listed)" is considered a label itself.
-              This dataset was generated on November 21, 2019."""),
+              This dataset contains 25,000,095 ratings across 62,423 movies.
+              These data were created by 162,541 users between January 09, 1995
+              and November 21, 2019. Ratings are in half-star increments. Each
+              user has rated at least 20 movies. This dataset is the latest
+              stable version of the MovieLens dataset. This dataset does not
+              include demographic data. Genres are converted into sequence of
+              labels. "(no genres listed)" is considered a label itself. This
+              dataset was generated on November 21, 2019."""),
           version='0.1.0',
       ),
       tfds.core.BuilderConfig(
           name='latest-small',
           description=textwrap.dedent("""\
-              This dataset contains 100836 ratings across 9742 movies
-              These data were created by 610 users between March 29, 1996
-              and September 24, 2018.
-              Ratings are in half-star increments.
-              Each user has rated at least 20 movies.
-              This dataset is the a subset of the full latest version
-              of the MovieLens dataset.
-              This dataset is changed and updated over time and the checksum
-              might need updating.
-              This dataset does not include demographic data.
-              The movies data and ratings data are joined on "movieId".
-              Genres are converted into sequence of labels.
-              "(no genres listed)" is considered a label itself.
-              This dataset was generated on September 26, 2018."""),
+              This dataset contains 100,836 ratings across 9,742 movies. These
+              data were created by 610 users between March 29, 1996
+              and September 24, 2018. Ratings are in half-star increments. Each
+              user has rated at least 20 movies. This dataset is the a subset of
+              the full latest version of the MovieLens dataset. This dataset is
+              changed and updated over time and the checksum might need
+              updating. This dataset does not include demographic data. Genres
+              are converted into sequence of labels. "(no genres listed)" is
+              considered a label itself. This dataset was generated on September
+              26, 2018."""),
           version='0.1.0',
       ),
       tfds.core.BuilderConfig(
           name='100k',
           description=textwrap.dedent("""\
               This dataset contains 100,000 ratings (1-5) from 943 users
-              on 1682 movies.
-              Ratings are in whole-star increments.
-              Each user has rated at least 20 movies.
-              This dataset is the oldest version of the MovieLens dataset.
-              The movies data and ratings data are joined on "movie id".
-              The ratings data and users data are joined on "user id".
-              Occupation labels standardized for "user_occupation_label" to
-              ensure consistency.
-              The original occupation strings are also preserved in
+              on 1,682 movies. Ratings are in whole-star increments. Each user
+              has rated at least 20 movies. This dataset is the oldest version
+              of the MovieLens dataset. The ratings data and users data are
+              joined on "user id". Occupation labels are standardized for
+              "user_occupation_label" to ensure consistency. The original
+              occupation strings are also preserved in
               "user_occupation_string"."""),
           version='0.1.0',
       ),
       tfds.core.BuilderConfig(
           name='1m',
           description=textwrap.dedent("""\
-              This dataset contains 1,000,209 anonymous ratings of
-              approximately 3,900 movies made by 6,040 MovieLens users
-              who joined MovieLens in 2000.
-              Ratings are in whole-star increments.
-              Each user has rated at least 20 movies.
-              This dataset is the largest dataset that includes
-              demographic data.
-              The movies data and ratings data are joined on "MovieId".
-              The ratings data and users data are joined on "UserId".
-              The age values are divided into ranges and the lowest
-              age value for each range is used in the data instead of
-              the actual values.
-              Occupation labels standardized for "user_occupation_label" to
-              ensure consistency.
-              The original occupation strings are also preserved in
+              This dataset contains 1,000,209 anonymous ratings of approximately
+              3,900 movies made by 6,040 MovieLens users who joined MovieLens in
+              2000. Ratings are in whole-star increments. Each user has rated at
+              least 20 movies. This dataset is the largest dataset that includes
+              demographic data. The ratings data and users data are joined on
+              "UserId". The age values are divided into ranges and the lowest
+              age value for each range is used in the data instead of the actual
+              values. Occupation labels are standardized for
+              "user_occupation_label" to ensure consistency. The original
+              occupation strings are also preserved in
               "user_occupation_string"."""),
           version='0.1.0',
       ),
       tfds.core.BuilderConfig(
           name='20m',
           description=textwrap.dedent("""\
-              This dataset contains contains 20000263 ratings
-              across 27278 movies.
-              These data were created by 138493 users between
-              January 09, 1995 and March 31, 2015.
-              Ratings are in half-star increments.
-              Each user has rated at least 20 movies.
-              This dataset does not contain demographic data.
-              The movies data and ratings data are joined on "movieId".
-              Genres are converted into sequences of labels.
-              "(no genres listed)" is considered a label itself.
-              This dataset was generated on October 17, 2016."""),
+              This dataset contains contains 20,000,263 ratings across 27,278
+              movies. These data were created by 138,493 users between January
+              09, 1995 and March 31, 2015. Ratings are in half-star increments.
+              Each user has rated at least 20 movies. This dataset does not
+              contain demographic data. Genres are converted into sequences of
+              labels. "(no genres listed)" is considered a label itself. This
+              dataset was generated on October 17, 2016."""),
           version='0.1.0',
       ),
   ]
@@ -169,7 +176,7 @@ class MovieLens(tfds.core.GeneratorBasedBuilder):
   VERSION = tfds.core.Version('0.1.0')
 
   def _info(self) -> tfds.core.DatasetInfo:
-    """Return a DatasetInfo according to self.builder_config."""
+    """Returns DatasetInfo according to self.builder_config."""
     features_dict = {
         'movie_id': tf.string,
         'movie_title': tf.string,
@@ -242,7 +249,7 @@ class MovieLens(tfds.core.GeneratorBasedBuilder):
 def _parse_current_format(
     dir_path: str
 ) -> Iterator[Tuple[int, Dict[str, Any]]]:
-  """Parse the data in current format (20m, 25m, and latest-small)."""
+  """Parses the data in current format (20m, 25m, and latest-small)."""
   movies_file_path = os.path.join(dir_path, 'movies.csv')
   ratings_file_path = os.path.join(dir_path, 'ratings.csv')
   movie_genre_map = {}
@@ -268,7 +275,7 @@ def _parse_current_format(
 def _parse_1m_format(
     dir_path: str
 ) -> Iterator[Tuple[int, Dict[str, Any]]]:
-  """Parse the 1m data."""
+  """Parses the 1m data."""
   movies_file_path = os.path.join(dir_path, 'movies.dat')
   users_file_path = os.path.join(dir_path, 'users.dat')
   ratings_file_path = os.path.join(dir_path, 'ratings.dat')
@@ -338,7 +345,7 @@ def _parse_1m_format(
 def _parse_100k_format(
     dir_path: str
 ) -> Iterator[Tuple[int, Dict[str, Any]]]:
-  """Parse the 100k data."""
+  """Parses the 100k data."""
   movies_file_path = os.path.join(dir_path, 'u.item')
   users_file_path = os.path.join(dir_path, 'u.user')
   ratings_file_path = os.path.join(dir_path, 'u.data')
