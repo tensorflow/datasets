01663f6e9aef14b27b7334c01fe03a722af201a3
Sharan Ramjee <sharanramjee@gmail.com>
2020-06-21 06:04:04 -0400

    Fixed lint errors

--- tensorflow_datasets/image_classification/not_mnist.py
+++ tensorflow_datasets/image_classification/not_mnist.py
@@ -16,8 +16,10 @@
 # Lint as: python3
 """notMNIST dataset.
 
-notMNIST is a dataset curated by Yaroslav Bulatov by taking publicly available fonts and extracting glyphs from
-them to create a dataset similar to MNIST. The 28x28 pixel images comprise of 10 classes with letters A-J.
+notMNIST is a dataset curated by Yaroslav Bulatov by taking publicly available fonts and
+extracting glyphs from them to create a dataset similar to MNIST. The 28x28 pixel images
+comprise of 10 classes with letters A-J. 1 training example was discarded from the pool
+of 529115 training examples because its PNG header was corrupted.
 """
 
 from __future__ import absolute_import
@@ -56,84 +58,82 @@ _ASCII_OFFSET = 65
 
 
 class NotMnist(tfds.core.GeneratorBasedBuilder):
-  """notMNIST dataset."""
+    """notMNIST dataset."""
 
-  VERSION = tfds.core.Version('0.1.0')
+    VERSION = tfds.core.Version('0.1.0')
 
-  def _info(self):
-    """
-    Returns basic information about the dataset.
+    def _info(self):
+        """
+        Returns basic information about the dataset.
 
-    Returns:
-      tfds.core.DatasetInfo.
-    """
-    return tfds.core.DatasetInfo(
-        builder=self,
-        description=_DESCRIPTION,
-        features=tfds.features.FeaturesDict({
-            "image": tfds.features.Image(shape=mnist.MNIST_IMAGE_SHAPE),
-            "label": tfds.features.ClassLabel(num_classes=mnist.MNIST_NUM_CLASSES),
-        }),
-        supervised_keys=("image", "label"),
-        homepage='http://yaroslavvb.blogspot.com/2011/09/notmnist-dataset.html',
-        citation=_CITATION,
-    )
+        Returns:
+          tfds.core.DatasetInfo.
+        """
+        return tfds.core.DatasetInfo(
+            builder=self,
+            description=_DESCRIPTION,
+            features=tfds.features.FeaturesDict({
+                "image": tfds.features.Image(shape=mnist.MNIST_IMAGE_SHAPE),
+                "label": tfds.features.ClassLabel(num_classes=mnist.MNIST_NUM_CLASSES),
+            }),
+            supervised_keys=("image", "label"),
+            homepage='http://yaroslavvb.blogspot.com/2011/09/notmnist-dataset.html',
+            citation=_CITATION,
+        )
 
-  def _split_generators(self, dl_manager):
-    """Return the train, test split of notMNIST.
+    def _split_generators(self, dl_manager):
+        """Return the train, test split of notMNIST.
 
-    Args:
-      dl_manager: download manager object.
+        Args:
+          dl_manager: download manager object.
 
-    Returns:
-      train split, test split.
-    """
-    # Download the full notMNIST database
-    urls = {
-        "train_data": _TRAIN_DATA_URL,
-        "test_data": _TEST_DATA_URL,
-    }
-    files = dl_manager.download_and_extract(urls)
-    # notMNIST provides TRAIN and TEST splits, not a VALIDATION
-    # split, so we only write the TRAIN and TEST splits to disk.
-    return [
-        tfds.core.SplitGenerator(
-            name=tfds.Split.TRAIN,
-            gen_kwargs=dict(
-                dl_manager=dl_manager,
-                data_path=os.path.join(files["train_data"], _TRAIN_DATA_DIR),
-            )),
-        tfds.core.SplitGenerator(
-            name=tfds.Split.TEST,
-            gen_kwargs=dict(
-                dl_manager=dl_manager,
-                data_path=os.path.join(files["test_data"], _TEST_DATA_DIR),
-            )),
-    ]
+        Returns:
+          train split, test split.
+        """
+        # Download the full notMNIST database
+        urls = {
+            "train_data": _TRAIN_DATA_URL,
+            "test_data": _TEST_DATA_URL,
+        }
+        files = dl_manager.download_and_extract(urls)
+        # notMNIST provides TRAIN and TEST splits, not a VALIDATION
+        # split, so we only write the TRAIN and TEST splits to disk.
+        return [
+            tfds.core.SplitGenerator(
+                name=tfds.Split.TRAIN,
+                gen_kwargs=dict(
+                    data_path=os.path.join(files["train_data"], _TRAIN_DATA_DIR),
+                )),
+            tfds.core.SplitGenerator(
+                name=tfds.Split.TEST,
+                gen_kwargs=dict(
+                    data_path=os.path.join(files["test_data"], _TEST_DATA_DIR),
+                )),
+        ]
 
-  def _generate_examples(self, dl_manager, data_path):
-    """
-    Generate the notMNIST data.
+    def _generate_examples(self, data_path):
+        """
+        Generate the notMNIST data.
 
-    Args:
-        dl_manager: download manager object.
-        data_path: path to the location of extracted dataset
+        Args:
+            dl_manager: download manager object.
+            data_path: path to the location of extracted dataset
 
-    Returns:
-        record with image along with its associated label
-    """
-    for label in range(mnist.MNIST_NUM_CLASSES):
-        label_dir = os.path.join(data_path, chr(label+_ASCII_OFFSET))
-        images = list(pathlib.Path(label_dir).glob('*.png'))
-        for image in images:
-            # Discard the 1 training example with the corrupted PNG header
-            try:
-                data = tfds.core.lazy_imports.matplotlib.pyplot.imread(image)\
-                    .reshape(_IMAGE_SHAPE, _IMAGE_SHAPE, 1).astype('uint8')
-                record = {
-                    "image": data,
-                    "label": label,
-                }
-                yield image, record
-            except ValueError as value_error:
-                print(value_error)
+        Returns:
+            record with image along with its associated label
+        """
+        for label in range(mnist.MNIST_NUM_CLASSES):
+            label_dir = os.path.join(data_path, chr(label+_ASCII_OFFSET))
+            images = list(pathlib.Path(label_dir).glob('*.png'))
+            for image in images:
+                # Discard the 1 training example with the corrupted PNG header
+                try:
+                    data = tfds.core.lazy_imports.matplotlib.pyplot.imread(image)\
+                        .reshape(_IMAGE_SHAPE, _IMAGE_SHAPE, 1).astype('uint8')
+                    record = {
+                        "image": data,
+                        "label": label,
+                    }
+                    yield image, record
+                except ValueError as value_error:
+                    print(value_error)
