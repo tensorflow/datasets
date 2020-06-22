9f14d97230c291bebd4aa4637242db3d87e59dfe
vijayphoenix <cs17btech11040@iith.ac.in>
2020-06-23 04:51:21 +0530

    Refactor code and add tests

--- tensorflow_datasets/core/custom_dataset/translate_dataset.py
+++ tensorflow_datasets/core/custom_dataset/translate_dataset.py
@@ -4,8 +4,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import collections
 import os
-from typing import Dict, List, Tuple, Set
+from typing import Dict, List, Tuple
 
 import tensorflow.compat.v2 as tf
 from tensorflow_datasets.core import dataset_builder
@@ -14,6 +15,10 @@ from tensorflow_datasets.core import features as features_lib
 from tensorflow_datasets.core import splits as split_lib
 from tensorflow_datasets.core.utils import version
 
+# Dict of 'split_name'-> 'language' -> `List[text_data]`
+SplitExampleDict = Dict[str, Dict[str, List[str]]]
+
+
 class TranslateDataset(dataset_builder.DatasetBuilder):
   """Generic image classification dataset created from manual directory.
 
@@ -21,13 +26,11 @@ class TranslateDataset(dataset_builder.DatasetBuilder):
 
   ```
   path/to/manual_dir/<dataset_name>/
-    split_name/  # Ex: 'train'
-      lang1.txt
-      lang2.txt
-    split_name/  # Ex: 'test'
-      lang1.txt
-      lang2.txt
-      ...
+    lang1.train.txt
+    lang2.train.txt
+    lang1.test.txt
+    lang2.test.txt
+    ...
   ```
 
   To use it:
@@ -44,20 +47,20 @@ class TranslateDataset(dataset_builder.DatasetBuilder):
 
   def __init__(self, root_dir: str):
     root_dir = os.path.expanduser(root_dir)
-    self._languages_examples_dict, languages = _get_language_examples(root_dir)
-    self._languages = sorted(list(languages))
+    self._split_examples, self._languages = _get_split_language_examples(
+        root_dir)
     super(TranslateDataset, self).__init__()
-    
+
     # Reset `_data_dir` as it should not change to DATA_DIR/Version
     self._data_dir = root_dir
 
     # Update DatasetInfo splits
     split_dict = split_lib.SplitDict(self.name)
-    for split_name, examples in self._languages_examples_dict.items():
+    for split_name, examples in self._split_examples.items():
       split_dict.add(split_lib.SplitInfo(
           name=split_name,
           shard_lengths=[len(next(iter(examples.values())))],
-        ))
+      ))
     self.info.update_splits_if_different(split_dict)
 
   def _info(self) -> dataset_info.DatasetInfo:
@@ -65,7 +68,6 @@ class TranslateDataset(dataset_builder.DatasetBuilder):
         builder=self,
         description='Generic image classification dataset.',
         features=features_lib.Translation(self._languages),
-        # supervised_keys=(self.languages[0], self.languages[1]), # Support supervised?
     )
 
   def _download_and_prepare(self, **kwargs):
@@ -81,9 +83,10 @@ class TranslateDataset(dataset_builder.DatasetBuilder):
       split,
       shuffle_files=False,
       decoders=None,
-      read_config=None) -> tf.data.Dataset:
+      read_config=None
+  ) -> tf.data.Dataset:
     """Generate dataset for given split"""
-    del read_config # Unused (automatically created in `DatasetBuilder`)
+    del read_config  # Unused (automatically created in `DatasetBuilder`)
     if decoders:
       raise NotImplementedError(
           '`decoders` is not supported with {}'.format(type(self).__name__))
@@ -94,39 +97,39 @@ class TranslateDataset(dataset_builder.DatasetBuilder):
               split, type(self).__name__, list(self.info.splits.keys())))
 
     # Build the tf.data.Dataset object
-    lang_example_dict = self._languages_examples_dict[split]
+    lang_example_dict = self._split_examples[split]
     ds = tf.data.Dataset.from_tensor_slices(lang_example_dict)
     if shuffle_files:
       ds = ds.shuffle(len(lang_example_dict))
     return ds
 
 
-def _get_language_examples(
+def _get_split_language_examples(
     root_dir: str
-) -> Tuple[Dict[str, Dict[str, List[str]]], List[str]]:
-  split_lang_examples = dict()
+) -> Tuple[SplitExampleDict, List[str]]:
+  """Extract all split names and associated text data.
+
+  Args:
+    root_dir: The folder where the `lang.split.txt` are located
+
+  Returns:
+    split_examples: Mapping split_names -> language -> List[text_data]
+    languages: The list of languages
+  """
+  split_examples = collections.defaultdict(dict)
   languages = set()
-  for split_name in _list_folders(root_dir):
-    split_dir = os.path.join(root_dir, split_name)
-    split_lang_examples[split_name] = {
-        f[:-4]: _list_examples(os.path.join(split_dir, f))
-        for f in tf.io.gfile.listdir(split_dir)
-        if f.lower().endswith('.txt')
-    }
-    size = len(next(iter(split_lang_examples[split_name].values())))
+  files = tf.io.gfile.listdir(root_dir)
+  for file in files:
+    lang, split_name, _ = file.split('.')
+    split_examples[split_name][lang] = _list_examples(
+        os.path.join(root_dir, file))
+    languages.add(lang)
+
     # One-to-One translation
-    for lang, example in split_lang_examples[split_name].items():
-      languages.add(lang)
-      assert len(example) == size
+    size = len(next(iter(split_examples[split_name].values())))
+    assert all(len(ex) == size for ex in split_examples[split_name].values())
 
-  return split_lang_examples, languages
-
-
-def _list_folders(root_dir: str) -> List[str]:
-  return [
-      f for f in tf.io.gfile.listdir(root_dir)
-      if tf.io.gfile.isdir(os.path.join(root_dir, f))
-  ]
+  return split_examples, sorted(languages)
 
 
 def _list_examples(file: str) -> List[str]:
--- tensorflow_datasets/core/custom_dataset/translate_dataset_test.py
+++ tensorflow_datasets/core/custom_dataset/translate_dataset_test.py
@@ -2,7 +2,7 @@
 import os
 import mock
 
-# from tensorflow_datasets.core.custom_dataset import translate_dataset
+from tensorflow_datasets.core.custom_dataset import translate_dataset
 import tensorflow_datasets.public_api as tfds
 
 _EXAMPLE_DIR = os.path.join(
@@ -11,10 +11,12 @@ _EXAMPLE_DIR = os.path.join(
 original_init = tfds.TranslateDataset.__init__
 original_download_and_prepare = tfds.TranslateDataset.download_and_prepare
 
+
 def new_init(self, root_dir=None, **kwargs):
   assert root_dir is None
   del kwargs
   original_init(self, root_dir=_EXAMPLE_DIR)
+
 
 class TranslateDatasetTests(tfds.testing.DatasetBuilderTestCase):
   """Translate dataset tests."""
@@ -26,14 +28,14 @@ class TranslateDatasetTests(tfds.testing.DatasetBuilderTestCase):
   }
 
   @classmethod
-  def setUpClass(cls): # pylint:disable = invalid-name
+  def setUpClass(cls):  # pylint:disable = invalid-name
     super().setUpClass()
     super(TranslateDatasetTests, cls).setUpClass()
     cls.DATASET_CLASS.__init__ = new_init
     cls.DATASET_CLASS.download_and_prepare = mock.Mock(return_value=None)
 
   @classmethod
-  def tearDownClass(cls): # pylint:disable = invalid-name
+  def tearDownClass(cls):  # pylint:disable = invalid-name
     super().tearDownClass()
     cls.DATASET_CLASS.__init__ = original_init
     cls.DATASET_CLASS.download_and_prepare = original_download_and_prepare
@@ -42,7 +44,62 @@ class TranslateDatasetTests(tfds.testing.DatasetBuilderTestCase):
     # Custom datasets shouldn't be registered
     self.assertNotIn(tfds.TranslateDataset.name, tfds.list_builders())
 
-# TODO: Add function tests and fake directory tests
+
+class TranslateDatasetFunctionTest(tfds.testing.TestCase):
+  """Tests for TranslateDataset functions."""
+
+  def test_properties(self):
+    file_contents = {
+        'root_dir/lang1.train.txt': 'line1\nlang1',
+        'root_dir/lang2.train.txt': 'line1\nlang2',
+        'root_dir/lang3.train.txt': 'line1\nlang3',
+
+        'root_dir/lang1.val.txt': 'line1\nline2\n\nline4',
+        'root_dir/lang2.val.txt': 'line1\nline2\n\nline4',
+        'root_dir/lang3.val.txt': 'line1\nline2\n\nline4',
+
+        'root_dir/lang1.test.txt': 'line1',
+        'root_dir/lang2.test.txt': 'line1',
+    }
+
+    with tfds.testing.MockFs() as fs:
+      for file in file_contents:
+        fs.add_file(file, file_contents[file])
+
+      split_examples, langs = translate_dataset._get_split_language_examples(
+          'root_dir')
+      builder = tfds.TranslateDataset(root_dir='root_dir')
+
+      self.assertEqual(split_examples, {
+          'train': {
+              'lang1': ['line1', 'lang1'],
+              'lang2': ['line1', 'lang2'],
+              'lang3': ['line1', 'lang3'],
+          },
+          'val': {
+              'lang1': ['line1', 'line2', '', 'line4'],
+              'lang2': ['line1', 'line2', '', 'line4'],
+              'lang3': ['line1', 'line2', '', 'line4'],
+          },
+          'test': {
+              'lang1': ['line1'],
+              'lang2': ['line1'],
+          },
+      })
+
+      self.assertEqual(builder.info.splits['train'].num_examples, 2)
+      self.assertEqual(builder.info.splits['val'].num_examples, 4)
+      self.assertEqual(builder.info.splits['test'].num_examples, 1)
+
+      expected_languages = [
+          'lang1',
+          'lang2',
+          'lang3',
+      ]
+
+      self.assertEqual(expected_languages, langs)
+      self.assertEqual(builder.info.features.languages, expected_languages)
+
 
 if __name__ == '__main__':
   tfds.testing.test_main()
