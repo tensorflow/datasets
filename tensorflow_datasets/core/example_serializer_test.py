06138fbb14f0d64c36526ce386de99ef783318c2
zweichen <zweichen@google.com>
2020-06-24 16:30:31 -0400

    Removed unused import

--- tensorflow_datasets/core/example_serializer_test.py
+++ tensorflow_datasets/core/example_serializer_test.py
@@ -20,7 +20,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import textwrap
 import numpy as np
 import tensorflow.compat.v2 as tf
 
