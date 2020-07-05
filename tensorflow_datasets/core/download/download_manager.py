0665ded17a7d8e18e30cebe1e0f224470a482361
Ouwen Huang <ouwen.huang@duke.edu>
2020-07-05 15:07:15 -0400

    Nih chest xray (#2)
    * first commit of nih_chest_xray; template files generated
    * _info and _split_generators finished
    * _generate_examples function finished
    * remove the usage of tf.io.gfile.GFile
    
    * working version but has a line difference with the official version now; fix by https://github.com/tensorflow/datasets/issues/2021
    
    Co-authored-by: jason-zl190 <i.liangzisheng@gmail.com>
    Co-authored-by: jason-zl190 <i.liangzisheng.com>

--- tensorflow_datasets/core/download/download_manager.py
+++ tensorflow_datasets/core/download/download_manager.py
@@ -254,6 +254,7 @@ class DownloadManager(object):
   @property
   def downloaded_size(self):
     """Returns the total size of downloaded files."""
+    #return sum(size for size, sha256 in self._recorded_url_infos.values())
     return sum(url_info.size for url_info in self._recorded_url_infos.values())
 
   def _get_final_dl_path(self, url, sha256):
