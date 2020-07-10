819275aebe31e399c4aef773c09271318b906e68
Etienne Pot <epot@google.com>
2020-07-09 18:28:41 -0700

    Pin numba version
    PiperOrigin-RevId: 320516101

--- setup.py
+++ setup.py
@@ -129,7 +129,11 @@ DATASET_EXTRAS = {
     'librispeech': ['pydub'],  # and ffmpeg installed
     # sklearn version required to avoid conflict with librosa from
     # https://github.com/scikit-learn/scikit-learn/issues/14485
-    'nsynth': ['crepe>=0.0.11', 'librosa', 'scikit-learn==0.20.3'],
+    # TODO(epot): Remove numba==0.48 once librosa 8.0 is out
+    # See https://github.com/librosa/librosa/issues/1160
+    'nsynth': [
+        'crepe>=0.0.11', 'librosa', 'numba==0.48', 'scikit-learn==0.20.3'
+    ],
     'pet_finder': ['pandas'],
     'robonet': ['h5py'],  # and ffmpeg installed
     'svhn': ['scipy'],
