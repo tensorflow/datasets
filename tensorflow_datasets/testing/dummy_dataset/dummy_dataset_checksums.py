86252a304c57abc777cb88c63dc61dea78e69fba
vijayphoenix <cs17btech11040@iith.ac.in>
2020-07-11 09:30:57 +0530

    Update tests

--- tensorflow_datasets/testing/dummy_dataset/dummy_dataset_checksums.py
+++ tensorflow_datasets/testing/dummy_dataset/dummy_dataset_checksums.py
@@ -1,4 +1,38 @@
-from tensorflow_datasets.image_classification import cats_vs_dogs
+import os
 
-class DummyDatasetChecksums(cats_vs_dogs.CatsVsDogs):
-  pass
+import tensorflow.compat.v2 as tf
+import tensorflow_datasets.public_api as tfds
+
+
+class DummyDatasetChecksums(tfds.core.GeneratorBasedBuilder):
+
+  VERSION = tfds.core.Version('1.0.0')
+
+  def _info(self):
+    return tfds.core.DatasetInfo(
+        builder=self,
+        features=tfds.features.FeaturesDict({"x": tf.int64}),
+    )
+
+  def _split_generators(self, dl_manager):
+    url = "http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz"
+    path = dl_manager.download(url)
+
+    return [
+        tfds.core.SplitGenerator(
+            name=tfds.Split.TRAIN,
+            gen_kwargs={
+                "path": os.path.join(path, "train.txt")
+            },
+        ),
+    ]
+
+  def _generate_examples(self, path):
+    with tf.io.gfile.GFile(path) as f:
+      value = f.read()
+    range_ = int(value)
+    for i in range(range_):
+      x = i
+      if self.builder_config:
+        x += self.builder_config.increment
+      yield i, {"x": x}
--- tensorflow_datasets/testing/dummy_dataset/dummy_dataset_checksums_test.py
+++ tensorflow_datasets/testing/dummy_dataset/dummy_dataset_checksums_test.py
@@ -1,18 +1,16 @@
 import os
+
 from tensorflow_datasets import testing
 from dummy_dataset_checksums import DummyDatasetChecksums
-from tensorflow_datasets.image_classification import cats_vs_dogs
 
-
-cats_vs_dogs._NUM_CORRUPT_IMAGES = 0  # pylint: disable=protected-access
 
 class DummyDatasetChecksumTest(testing.DatasetBuilderTestCase):
   DATASET_CLASS = DummyDatasetChecksums
-  EXAMPLE_DIR = os.path.join(DummyDatasetChecksums.code_dir, "fake_example")
+  EXAMPLE_DIR = os.path.join(DummyDatasetChecksums.code_dir, "fake_examples")
   SPLITS = {
-      'train': 4
+      'train': 20,
   }
-  DL_EXTRACT_RESULT = 'cats_vs_dogs.zip'
+
 
 if __name__ == "__main__":
   testing.test_main()
