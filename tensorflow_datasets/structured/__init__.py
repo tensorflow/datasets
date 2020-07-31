4a49f398f4832d1435e5b79240b53e0cdb78f0f0
Jie Ren <jjren@google.com>
2020-07-31 16:15:04 -0700

    Add genomics_ood dataset to TFDS
    PiperOrigin-RevId: 324304441

--- tensorflow_datasets/structured/__init__.py
+++ tensorflow_datasets/structured/__init__.py
@@ -17,6 +17,7 @@
 
 from tensorflow_datasets.structured.amazon_us_reviews import AmazonUSReviews
 from tensorflow_datasets.structured.forest_fires import ForestFires
+from tensorflow_datasets.structured.genomics_ood import GenomicsOod
 from tensorflow_datasets.structured.german_credit_numeric import GermanCreditNumeric
 from tensorflow_datasets.structured.higgs import Higgs
 from tensorflow_datasets.structured.iris import Iris
