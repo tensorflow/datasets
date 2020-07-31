173aed7ee9a08882d0bd58fc86670fbb347c3dc6
TensorFlow Datasets Team <no-reply@google.com>
2020-07-31 11:12:37 -0700

    Internal change
    PiperOrigin-RevId: 324244596

--- tensorflow_datasets/summarization/big_patent.py
+++ tensorflow_datasets/summarization/big_patent.py
@@ -83,9 +83,11 @@ class BigPatentConfig(tfds.core.BuilderConfig):
         # 1.0.0 lower cased tokenized words.
         # 2.0.0 cased raw strings.
         # 2.1.0 cased raw strings (fixed).
-        version=tfds.core.Version("2.1.0", "Fix update to cased raw strings."),
-        supported_versions=[tfds.core.Version("1.0.0"),
-                            tfds.core.Version("2.0.0")],
+        version=tfds.core.Version("2.1.1", "Fix update to cased raw strings."),
+        supported_versions=[
+            tfds.core.Version("1.0.0", "lower cased tokenized words"),
+            tfds.core.Version("2.0.0", "Update to use cased raw strings")
+        ],
         **kwargs)
     self.cpc_codes = cpc_codes
 
@@ -184,13 +186,14 @@ _ENGLISH_WORDS = None
 
 
 def _get_english_words():
+  """Load dictionary of common english words from NLTK."""
   global _ENGLISH_WORDS
   if not _ENGLISH_WORDS:
     nltk = tfds.core.lazy_imports.nltk
     resource_path = importlib_resources.files(nltk)
-    data_path = resource_path / "nltk_data/corpora/words/en"
-    _ENGLISH_WORDS = frozenset(
-        nltk.data.load(data_path, format="raw").decode("utf-8").split("\n"))
+    data_path = str(resource_path / "nltk_data/corpora/words/en")
+    word_list = nltk.data.load(data_path, format="raw").decode("utf-8")
+    _ENGLISH_WORDS = frozenset(word_list.split("\n"))
   return _ENGLISH_WORDS
 
 
