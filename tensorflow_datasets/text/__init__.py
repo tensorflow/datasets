8ee947a57aefe5cf019b17af1f249726c25923b3
TensorFlow Datasets Team <no-reply@google.com>
2020-08-12 04:56:24 -0700

    Add PAWSX Wiki dataset
    PiperOrigin-RevId: 326210406

--- tensorflow_datasets/text/__init__.py
+++ tensorflow_datasets/text/__init__.py
@@ -42,6 +42,7 @@ from tensorflow_datasets.text.multi_nli import MultiNLI
 from tensorflow_datasets.text.multi_nli_mismatch import MultiNLIMismatch
 from tensorflow_datasets.text.openbookqa import Openbookqa
 from tensorflow_datasets.text.paws_wiki import PawsWiki
+from tensorflow_datasets.text.paws_x_wiki import PawsXWiki
 from tensorflow_datasets.text.pg19 import Pg19
 from tensorflow_datasets.text.qa4mre import Qa4mre
 from tensorflow_datasets.text.reddit_disentanglement import RedditDisentanglement
