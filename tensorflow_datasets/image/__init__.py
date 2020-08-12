ffbb8decc6f6709cb5c7f961c870b2b10e41c339
TensorFlow Datasets Team <no-reply@google.com>
2020-08-12 06:16:19 -0700

    Add BCCD dataset
    PiperOrigin-RevId: 326218347

--- tensorflow_datasets/image/__init__.py
+++ tensorflow_datasets/image/__init__.py
@@ -18,6 +18,7 @@
 from tensorflow_datasets.image.abstract_reasoning import AbstractReasoning
 from tensorflow_datasets.image.aflw2k3d import Aflw2k3d
 from tensorflow_datasets.image.arc import ARC
+from tensorflow_datasets.image.bccd import BCCD
 from tensorflow_datasets.image.binarized_mnist import BinarizedMNIST
 from tensorflow_datasets.image.celeba import CelebA
 from tensorflow_datasets.image.celebahq import CelebAHq
