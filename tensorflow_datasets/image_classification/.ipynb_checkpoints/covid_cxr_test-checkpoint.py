c7bc5c5eba301b4e2ff57b382b840ad81314ed7f
VishaalVenkatesh <venkateshvishaal@gmail.com>
2020-09-12 14:13:04 -0400

    changed test data

--- tensorflow_datasets/image_classification/.ipynb_checkpoints/covid_cxr_test-checkpoint.py
+++ tensorflow_datasets/image_classification/.ipynb_checkpoints/covid_cxr_test-checkpoint.py
@@ -3,26 +3,28 @@
 import tensorflow_datasets.public_api as tfds
 from tensorflow_datasets.image_classification import covid_cxr
 
+covid_cxr._IMAGE_SHAPE = (None, None, 3)
 
 class CovidCxrTest(tfds.testing.DatasetBuilderTestCase):
     
   # TODO(covid_cxr):
 
     DATASET_CLASS = covid_cxr.CovidCxr
-    _CONFIG = covid_cxr.CovidCxrConfig
+    BUILDER_CONFIG_NAMES_TO_TEST = ["224"]
+    #_CONFIG = DATASET_CLASS.CovidCxrConfig
     SPLITS = {
       "train": 3,  # Number of fake train example
       "test": 3,  # Number of fake test example
   }
+    
 
   # If you are calling `download/download_and_extract` with a dict, like:
   #   dl_manager.download({'some_key': 'http://a.org/out.txt', ...})
   # then the tests needs to provide the fake output paths relative to the
   # fake data directory
   # DL_EXTRACT_RESULT = {'some_key': 'output_file1.txt', ...}
-    
-    DL_EXTRACT_RESULT = [_CONFIG]
 
+    DL_EXTRACT_RESULT = ['train.zip', 'test.zip']
 
 if __name__ == "__main__":
   tfds.testing.test_main()
