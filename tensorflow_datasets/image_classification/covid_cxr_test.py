654a8f351caa787af51be4095ee7f27b5f5070df
VishaalVenkatesh <venkateshvishaal@gmail.com>
2020-09-12 17:46:15 -0400

    modded test data

--- tensorflow_datasets/image_classification/covid_cxr_test.py
+++ tensorflow_datasets/image_classification/covid_cxr_test.py
@@ -2,30 +2,54 @@
 
 import tensorflow_datasets.public_api as tfds
 from tensorflow_datasets.image_classification import covid_cxr
-
-covid_cxr._IMAGE_SHAPE = (None, None, 3)
+import tensorflow_datasets.testing as testing
 
 class CovidCxrTest(tfds.testing.DatasetBuilderTestCase):
     
   # TODO(covid_cxr):
-
     DATASET_CLASS = covid_cxr.CovidCxr
-    BUILDER_CONFIG_NAMES_TO_TEST = ["224"]
-    #_CONFIG = DATASET_CLASS.CovidCxrConfig
-    SPLITS = {
-      "train": 3,  # Number of fake train example
-      "test": 3,  # Number of fake test example
-  }
-    
+    supervised_keys = ('image', 'label')
+    class CovidCxrOriginalTest(testing.DatasetBuilderTestCase):
+          DATASET_CLASS = covid_cxr.CovidCxr
+          BUILDER_CONFIG_NAMES_TO_TEST = ["original"]
+          SPLITS = {
+              "train": 3,
+              "test": 3,
+          }
+           
+            
+            DL_EXTRACT_RESULT = {
+                          "train": "original/train",  
+                          "test": "original/test"
+                                  }
 
-  # If you are calling `download/download_and_extract` with a dict, like:
-  #   dl_manager.download({'some_key': 'http://a.org/out.txt', ...})
-  # then the tests needs to provide the fake output paths relative to the
-  # fake data directory
-  # DL_EXTRACT_RESULT = {'some_key': 'output_file1.txt', ...}
 
-    DL_EXTRACT_RESULT = ['train.zip', 'test.zip']
+    class CovidCxr224Test(testing.DatasetBuilderTestCase):
+          DATASET_CLASS = covid_cxr.CovidCxr
+          BUILDER_CONFIG_NAMES_TO_TEST = ["224"]
+          SPLITS = {
+              "train": 3,
+              "test": 3,
+          }
+            
+            DL_EXTRACT_RESULT = {
+                          "train": "224/train", 
+                          "test": "224/test"
+                                  }
+
+
+    class CovidCxr480Test(testing.DatasetBuilderTestCase):
+          DATASET_CLASS = covid_cxr.CovidCxr
+          BUILDER_CONFIG_NAMES_TO_TEST = ["480"]
+          SPLITS = {
+              "train": 3,
+              "test": 3,}
+            
+          DL_EXTRACT_RESULT = {
+                        "train": "480/train",  
+                        "test": "480/test"
+          }
 
 if __name__ == "__main__":
-  tfds.testing.test_main()
+      tfds.testing.test_main()
 
