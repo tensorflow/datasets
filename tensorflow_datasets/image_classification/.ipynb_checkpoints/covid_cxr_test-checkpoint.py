49b203910ac3526c365431568b32578b9a3073c5
VishaalVenkatesh <venkateshvishaal@gmail.com>
2020-09-12 20:36:57 -0400

    better code

--- tensorflow_datasets/image_classification/.ipynb_checkpoints/covid_cxr_test-checkpoint.py
+++ tensorflow_datasets/image_classification/.ipynb_checkpoints/covid_cxr_test-checkpoint.py
@@ -9,46 +9,40 @@ class CovidCxrTest(tfds.testing.DatasetBuilderTestCase):
   # TODO(covid_cxr):
     DATASET_CLASS = covid_cxr.CovidCxr
     supervised_keys = ('image', 'label')
-    class CovidCxrOriginalTest(testing.DatasetBuilderTestCase):
-          DATASET_CLASS = covid_cxr.CovidCxr
-          BUILDER_CONFIG_NAMES_TO_TEST = ["original"]
-          SPLITS = {
-              "train": 3,
-              "test": 3,
-          }
-           
-            
-            DL_EXTRACT_RESULT = {
-                          "train": "original/train",  
-                          "test": "original/test"
-                                  }
+    class CovidCxrOriginalTest(testing.DatasetBuilderTestCase): 
+                  covid_cxr._IMAGE_SHAPE = (None, None, 3)
+                  DATASET_CLASS = covid_cxr.CovidCxr
+                  BUILDER_CONFIG_NAMES_TO_TEST = ["original"]
+                  SPLITS = {"train": 3, "test": 3,}
+                  DL_EXTRACT_RESULT = {
+                              "image": "original",  
+                              "label": "original"
+                                      }
 
 
     class CovidCxr224Test(testing.DatasetBuilderTestCase):
-          DATASET_CLASS = covid_cxr.CovidCxr
-          BUILDER_CONFIG_NAMES_TO_TEST = ["224"]
-          SPLITS = {
-              "train": 3,
-              "test": 3,
-          }
-            
-            DL_EXTRACT_RESULT = {
-                          "train": "224/train", 
-                          "test": "224/test"
-                                  }
+                  covid_cxr._IMAGE_SHAPE = (224, 224, 3)
+                  DATASET_CLASS = covid_cxr.CovidCxr
+                  BUILDER_CONFIG_NAMES_TO_TEST = ["224"]
+                  SPLITS = { "train": 3,"test": 3,}
+                  DL_EXTRACT_RESULT = {
+                                  "image": "224", 
+                                  "label": "224"
+                                          }
 
 
     class CovidCxr480Test(testing.DatasetBuilderTestCase):
-          DATASET_CLASS = covid_cxr.CovidCxr
-          BUILDER_CONFIG_NAMES_TO_TEST = ["480"]
-          SPLITS = {
-              "train": 3,
-              "test": 3,}
-            
-          DL_EXTRACT_RESULT = {
-                        "train": "480/train",  
-                        "test": "480/test"
-          }
+                  covid_cxr._IMAGE_SHAPE = (480, 480, 3)
+                  DATASET_CLASS = covid_cxr.CovidCxr
+                  BUILDER_CONFIG_NAMES_TO_TEST = ["480"]
+                  SPLITS = {
+                      "train": 3,
+                      "test": 3,}
+
+                  DL_EXTRACT_RESULT = {
+                                "image": "480",  
+                                "label": "480"
+                  }
 
 if __name__ == "__main__":
       tfds.testing.test_main()
