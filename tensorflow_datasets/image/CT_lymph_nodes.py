8e2333aa1cf6d5331c529cf450cbd2c9ee8c3619
Jingjing Shi <js884@duke.edu>
2020-09-20 22:02:53 -0400

    Update CT_lymph_nodes.py

--- tensorflow_datasets/image/CT_lymph_nodes.py
+++ tensorflow_datasets/image/CT_lymph_nodes.py
@@ -1,7 +1,7 @@
 """CT_Lymph_Nodes dataset."""
 
-import tensorflow_datasets as tfds
-import tensorflow.compat.v2 as tf
+import tensorflow as tf
+import tensorflow_datasets.public_api as tfds
 import os
 import io
 import pydicom
