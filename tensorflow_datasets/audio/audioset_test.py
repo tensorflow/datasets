7b1d869e8a28fc48dfe5a085700bc7400166f086
William Huang <william.wh.huang@gmail.com>
2020-10-01 16:06:15 +0000

    modify test

--- tensorflow_datasets/audio/audioset_test.py
+++ tensorflow_datasets/audio/audioset_test.py
@@ -3,22 +3,10 @@
 import tensorflow_datasets as tf
 import tensorflow_datasets.public_api as tfds
 from tensorflow_datasets.audio import audioset
+import tensorflow_datasets.testing as tfds_test
 
-class AudiosetTest(tfds.testing.DatasetBuilderTestCase):
-  # TODO(audioset):
-  DATASET_CLASS = audioset.Audioset
-  SPLITS = {
-      "train": 3,   # Number of fake train example
-                    # Number of fake test example
-  }
 
-  # If you are calling `download/download_and_extract` with a dict, like:
-  #   dl_manager.download({'some_key': 'http://a.org/out.txt', ...})
-  # then the tests needs to provide the fake output paths relative to the
-  # fake data directory
-  # DL_EXTRACT_RESULT = {'some_key': 'output_file1.txt', ...}
-
-class AudioSetTest(testing.DatasetBuilderTestCase):
+class AudioSetTest(tfds_test.DatasetBuilderTestCase):
   DATASET_CLASS = audioset.AudioSet
   SPLITS = {
       'train': 4,
@@ -28,11 +16,11 @@ class AudioSetTest(testing.DatasetBuilderTestCase):
       'summary_table':
           'id2label.json',
       'all_files': [
-          '--PJHxphWEs.mp3', '-275_wTLm-4.mp3',
-          '08u-jdwjM74.mp3', 'X-5UcZiOIhQ.mp3',
+          'train/--PJHxphWEs.mp3', 'train/-275_wTLm-4.mp3',
+          'train/08u-jdwjM74.mp3', 'train/X-5UcZiOIhQ.mp3',
       ]
   }
 
 if __name__ == "__main__":
-  tfds.testing.test_main()
+  tfds_test.test_main()
 
