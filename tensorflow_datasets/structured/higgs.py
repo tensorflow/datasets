04355db4d7edebff6a7ba0a1e8b786f91303ddf7
NikhilBartwal <nikhilbartwal1234@gmail.com>
2020-10-02 23:46:07 +0530

    Add release notes for kiti dataset

--- tensorflow_datasets/structured/higgs.py
+++ tensorflow_datasets/structured/higgs.py
@@ -39,16 +39,16 @@ _CITATION = """\
 """
 
 _DESCRIPTION = """\
-The data has been produced using Monte Carlo simulations. 
-The first 21 features (columns 2-22) are kinematic properties 
-measured by the particle detectors in the accelerator. 
-The last seven features are functions of the first 21 features; 
-these are high-level features derived by physicists to help 
-discriminate between the two classes. There is an interest 
-in using deep learning methods to obviate the need for physicists 
-to manually develop such features. Benchmark results using 
-Bayesian Decision Trees from a standard physics package and 
-5-layer neural networks are presented in the original paper. 
+The data has been produced using Monte Carlo simulations.
+The first 21 features (columns 2-22) are kinematic properties
+measured by the particle detectors in the accelerator.
+The last seven features are functions of the first 21 features;
+these are high-level features derived by physicists to help
+discriminate between the two classes. There is an interest
+in using deep learning methods to obviate the need for physicists
+to manually develop such features. Benchmark results using
+Bayesian Decision Trees from a standard physics package and
+5-layer neural networks are presented in the original paper.
 """
 
 _URL = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00280/HIGGS.csv.gz'
