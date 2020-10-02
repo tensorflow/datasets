b84d1d9b275c640940016b59e9b482b70c406194
NikhilBartwal <nikhilbartwal1234@gmail.com>
2020-10-03 00:20:23 +0530

    Add release notes for lm1b dataset

--- tensorflow_datasets/text/lm1b.py
+++ tensorflow_datasets/text/lm1b.py
@@ -72,9 +72,9 @@ class Lm1bConfig(tfds.core.BuilderConfig):
       **kwargs: keyword arguments forwarded to super.
     """
     super(Lm1bConfig, self).__init__(
-        version=tfds.core.Version(
-            "1.0.0",
-            "New split API (https://tensorflow.org/datasets/splits)"),
+        version=tfds.core.Version("1.0.0"),
+        release_notes=
+        "New split API (https://tensorflow.org/datasets/splits)",
         **kwargs)
     self.text_encoder_config = (
         text_encoder_config or tfds.deprecated.text.TextEncoderConfig())
