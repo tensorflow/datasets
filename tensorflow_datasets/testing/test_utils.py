e51bf4a9720c6be9ce042c66ccb74e841d4b2b4c
Etienne Pot <epot@google.com>
2020-10-03 01:55:37 -0700

    Additional mock gfile API fixes
    PiperOrigin-RevId: 335173931

--- tensorflow_datasets/testing/test_utils.py
+++ tensorflow_datasets/testing/test_utils.py
@@ -200,6 +200,7 @@ def mock_tf(symbol_name: str, *args: Any, **kwargs: Any) -> Iterator[None]:
       module = tf_lib
       for submodule in tf_submodules:
         module = getattr(module, submodule)
+      getattr(module, symbol_name)  # Trigger the lazy-loading of the TF API.
       # Patch the module/object
       stack.enter_context(
           mock.patch.object(module, symbol_name, *args, **kwargs)
