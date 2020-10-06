5dd79ad752cf87126eb70d8d267ce13526007829
Etienne Pot <epot@google.com>
2020-10-06 00:48:45 -0700

    Improve debug message for audio file encoding (Fix #2513)
    PiperOrigin-RevId: 335587214

--- tensorflow_datasets/core/features/audio_feature.py
+++ tensorflow_datasets/core/features/audio_feature.py
@@ -74,7 +74,10 @@ class Audio(feature.Tensor):
       filename = audio_or_path_or_fobj
       file_format = self._file_format or filename.split('.')[-1]
       with tf.io.gfile.GFile(filename, 'rb') as audio_f:
-        return self._encode_file(audio_f, file_format)
+        try:
+          return self._encode_file(audio_f, file_format)
+        except Exception as e:  # pylint: disable=broad-except
+          utils.reraise(e, prefix=f'Error for {filename}: ')
     else:
       return self._encode_file(audio_or_path_or_fobj, self._file_format)
 
