22e8539172442b1134e4071df23ccfea3652fd7e
Etienne Pot <epot@google.com>
2020-10-09 09:23:49 -0700

    Update TF to 4.0.1
    Fix `tfds.load` when generation code isn't present and improve GCS compatibility.
    
    Thanks @carlthome for reporting and fixing the issue.
    
    PiperOrigin-RevId: 336306487

--- tensorflow_datasets/version.py
+++ tensorflow_datasets/version.py
@@ -25,7 +25,7 @@ This file is replaced by `version_stable.py` for stable releases
 # We follow Semantic Versioning (https://semver.org/)
 _MAJOR_VERSION = '4'
 _MINOR_VERSION = '0'
-_PATCH_VERSION = '0'
+_PATCH_VERSION = '1'
 
 __version__ = '.'.join([
     _MAJOR_VERSION,
--- tensorflow_datasets/version_stable.py
+++ tensorflow_datasets/version_stable.py
@@ -21,7 +21,7 @@ This file overwrites `version.py` for stable releases.
 # We follow Semantic Versioning (https://semver.org/)
 _MAJOR_VERSION = '4'
 _MINOR_VERSION = '0'
-_PATCH_VERSION = '0'
+_PATCH_VERSION = '1'
 
 __version__ = '.'.join([
     _MAJOR_VERSION,
