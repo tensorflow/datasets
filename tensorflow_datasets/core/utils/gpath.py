8f3bab74c96b7fcea31c0c5d2d687e5b58e0db61
Charles <charlespatel07@gmail.com>
2020-10-16 18:51:23 -0700

    Added GcsPath class

--- tensorflow_datasets/core/utils/gpath.py
+++ tensorflow_datasets/core/utils/gpath.py
@@ -1,55 +1,65 @@
-"""GPath wrapper around the gfile API"""
+"""GcsPath wrapper around the gfile API"""
 
 import os
 import pathlib
 import typing
-from typing import Union, Iterator, Optional, Any, AnyStr
+from typing import Any, AnyStr, Iterator, Optional, Union
+
 import tensorflow as tf
+
 from tensorflow_datasets.core.utils import type_utils
 
-PathArg = Union[str, pathlib.Path, 'GPath']
+PathArg = Union[str, pathlib.Path, 'GcsPath']
 
 
-class GPath(pathlib.PurePosixPath, type_utils.ReadWritePath):
+class GcsPath(pathlib.PurePosixPath, type_utils.ReadWritePath):
+  """Pathlib like api of tf.io.gfile"""
 
-  def __new__(cls, *parts: PathArg):
-    if not parts:
-      return pathlib.Path()
-    full_path = '/'.join(parts)
+  def __new__(cls, *parts: type_utils.PathLike):
+    full_path = '/'.join(os.fspath(p) for p in parts)
     if full_path.startswith(('gs://')):
       return super().__new__(cls, full_path.replace('gs://', '/gs/', 1))
     if full_path.startswith(('/gs/')):
       return super().__new__(cls, *parts)
     else:
-      return pathlib.Path(*parts)
+      raise ValueError('Invalid path')
 
-  def __fpath__(self) -> str:
-    return str(self)
 
-  def __str__(self) -> str:
+  def _get_path_str(self) -> str:
     value = super().__str__()
     if value.startswith('/gs/'):
       return value.replace('/gs/', 'gs://', 1)
-    return value
+    return value  # TODO: Could cache the value
+
+
+  def __fpath__(self) -> str:
+    return self._get_path_str()
+
+
+  def __str__(self) -> str:
+    return self._get_path_str()
+
 
   def exists(self) -> bool:
     return tf.io.gfile.exists(str(self))
 
-  def iterdir(self) -> Iterator['GPath']:
-    for f in tf.io.gfile.listdir(self):
-      yield GPath(f)
+
+  def iterdir(self) -> Iterator['GcsPath']:
+    for f in tf.io.gfile.listdir(str(self)):
+      yield GcsPath(f)
+
 
   def is_dir(self) -> bool:
     """Returns True if self is a directory."""
-    return tf.io.gfile.isdir(self)
+    return tf.io.gfile.isdir(str(self))
 
-  def glob(self, pattern: str) -> Iterator['GPath']:
+
+  def glob(self, pattern: str) -> Iterator['GcsPath']:
     """Yielding all matching files (of any kind)."""
-    # Might be able to implement using `iterdir` (recursivelly for `rglob`).
     path = os.path.join(str(self), pattern)
-
     for f in tf.io.gfile.glob(str(path)):
-      yield GPath(f)
+      yield GcsPath(f)
+
 
   def mkdir(
       self,
@@ -60,9 +70,6 @@ class GPath(pathlib.PurePosixPath, type_utils.ReadWritePath):
     """Create a new directory at this given path."""
     if self.exists() and not exist_ok:
       raise FileExistsError(f'{str(self)} already exists.')
-
-    if not self.is_dir():
-      raise NotADirectoryError(f'{str(self)} is not a directory.')
 
     if parents:
       tf.io.gfile.makedirs(str(self))
@@ -78,17 +85,20 @@ class GPath(pathlib.PurePosixPath, type_utils.ReadWritePath):
       **kwargs: Any,
   ) -> typing.IO[AnyStr]:
     """Opens the file."""
-    return tf.io.gfile.GFile(self, mode)
+    return tf.io.gfile.GFile(str(self), mode, **kwargs)
+
 
   def read_bytes(self) -> bytes:
     """Reads contents of self as bytes."""
     with tf.io.gfile.GFile(str(self), 'rb') as f:
       return f.read()
 
+
   def read_text(self, encoding: Optional[str] = None) -> str:
     """Reads contents of self as str."""
     with tf.io.gfile.GFile(str(self), 'r') as f:
       return f.read()
+
 
   def write_bytes(self, data: bytes) -> None:
     """Writes content as bytes."""
@@ -105,13 +115,11 @@ class GPath(pathlib.PurePosixPath, type_utils.ReadWritePath):
       return f.write(data)
 
 
+  def rename(self, target) -> None:
+    """Rename file or directory to the given target """
+    tf.io.gfile.rename(str(self), target)
 
-if __name__ == '__main__':
 
-  PATH = "gs://tfds-data/datasets/mnist/3.0.0/"
-
-  gpath = GPath(PATH)
-  print(gpath.exists())
-
-  f = gpath.glob("*json")
-  print(list(f))
+  def replace(self, target) -> None:
+    """Replace file or directory to the given target """
+    tf.io.gfile.rename(str(self), target, overwrite=True)
--- tensorflow_datasets/core/utils/gpath_test.py
+++ tensorflow_datasets/core/utils/gpath_test.py
@@ -1,23 +1,112 @@
 """Tests for tensorflow_datasets.core.utils.gpath."""
 
+import os
+import pathlib
+
 import pytest
 import tensorflow as tf
+from pathlib import Path
 from tensorflow_datasets import testing
-from tensorflow_datasets.core.utils import gpath
+from tensorflow_datasets.core.utils.gpath import GcsPath
 
 
-class GPathTest(testing.TestCase):
+@pytest.fixture
+def mocked_gfile_path(tmp_path: pathlib.Path):
+  def _norm_path(path: str):
+    return path.replace('gs://', os.fspath(tmp_path) + '/')
+
+  with testing.mock_tf(
+      'tf.io.gfile',
+      exists=lambda p: os.path.exists(_norm_path(p)),
+      isdir=lambda p: os.path.isdir(_norm_path(p)),
+      listdir=lambda p: os.listdir(_norm_path(p)),
+      GFile=lambda p, *args, **kwargs: open(_norm_path(p), *args, **kwargs),
+      rename=lambda p1, p2: os.rename(_norm_path(p1), _norm_path(p2)),
+      replace=lambda p1, p2, **kwargs: os.replace(_norm_path(p1), _norm_path(p2), **kwargs),
+      mkdir=lambda p: os.mkdir(_norm_path(p)),
+      makedirs=lambda p: os.makedirs(_norm_path(p)),
+      glob=lambda p: Path(_norm_path(p)).parent.glob(Path(_norm_path(p)).stem)  # TODO: temporary func
+  ):
+    yield tmp_path
 
 
-  def test_path_exists(self):
-    PATH = "gs://tfds-data/datasets/mnist/3.0.0/"
-    g_path = gpath.GPath(PATH)
-    self.assertTrue(g_path.exists())
+def test_gfs(mocked_gfile_path: pathlib.Path):
+  g_path = GcsPath('gs://bucket/dir')
+  assert not g_path.exists()
 
-  def test_path_not_exists(self):
-    PATH = "gs://tfds-data/foo/bar"
-    g_path = gpath.GPath(PATH)
-    self.assertFalse(g_path.exists())
+  # mkdir()
+  g_path.mkdir(parents=True)
 
-if __name__ == '__main__':
-  testing.test_main()
+  # exists()
+  assert g_path.exists()
+  assert mocked_gfile_path.joinpath('bucket/dir').exists()
+
+  # is_dir()
+  assert not mocked_gfile_path.joinpath('read_text_file.txt').is_dir()
+  assert g_path.is_dir()
+
+
+def test_functions(mocked_gfile_path: pathlib.Path):
+
+  files = ['foo.py', 'bar.py', 'foo_bar.py', 'dataset.json',
+           'dataset_info.json', 'readme.md']
+  dataset_path = GcsPath('gs://bucket/dataset')
+
+  dataset_path.mkdir(parents=True)
+  assert dataset_path.exists()
+
+  # open()
+  for file in files:
+    with dataset_path.joinpath(file).open('w') as f:
+      f.write(' ')
+
+  # iterdir()
+  assert len(list(mocked_gfile_path.joinpath('bucket/dataset').iterdir())) == 6
+
+
+def test_read_write(mocked_gfile_path: pathlib.Path):
+  # read_text()
+  with tf.io.gfile.GFile('gs://text_file.txt', 'w') as f:
+    f.write('abcd')
+  assert mocked_gfile_path.joinpath('text_file.txt').read_text() == 'abcd'
+
+  # read_bytes()
+  with tf.io.gfile.GFile('gs://bytes_file.txt', 'wb') as f:
+    f.write(b'abcd')
+  assert mocked_gfile_path.joinpath('bytes_file.txt').read_bytes() == b'abcd'
+
+  write_file_path = GcsPath('gs://text_file1.txt')
+  write_bytes_path = GcsPath('gs://bytes_file1.txt')
+
+  # write_text() write_bytes()
+  write_file_path.write_text('abcd')
+  write_bytes_path.write_bytes(b'foobar')
+
+  # read_text() read_bytes()
+  assert mocked_gfile_path.joinpath('text_file1.txt').read_text() == 'abcd'
+  assert mocked_gfile_path.joinpath('bytes_file1.txt').read_bytes() == b'foobar'
+
+
+def test_mkdir(mocked_gfile_path: pathlib.Path):
+  g_path = GcsPath('gs://bucket')
+  assert not g_path.exists()
+
+  g_path.mkdir()
+  assert g_path.exists()
+  assert mocked_gfile_path.joinpath('bucket').exists()
+
+
+def test_rename_replace(mocked_gfile_path: pathlib.Path):
+  src_path = GcsPath('gs://foo.py')
+  src_path.write_text(' ')
+
+  assert mocked_gfile_path.joinpath('foo.py').exists()
+
+  # Rename()
+  # target_path = GcsPath('gs://foo.py')
+  src_path.rename('gs://bar.py')
+  # target = GcsPath(mocked_gfile_path.joinpath())
+  # GcsPath(mocked_gfile_path.joinpath('foo.py')).rename('bar.py')
+
+  assert not mocked_gfile_path.joinpath('foo.py').exists()
+  assert mocked_gfile_path.joinpath('bar.py').exists()
