38a1b5d73c4e6e3700d77fbe31c9cc469f3f5918
Adam Hillier <7688302+AdamHillier@users.noreply.github.com>
2020-11-19 15:40:09 +0000

    Review suggestions.

--- tensorflow_datasets/object_detection/voc.py
+++ tensorflow_datasets/object_detection/voc.py
@@ -40,7 +40,7 @@ _SBD_CITATION = """\
 
 _VOC_DESCRIPTION = """\
 This dataset contains the data from the PASCAL Visual Object Classes Challenge,
-corresponding to the Classification and Detection competitions.
+corresponding to the Classification, Detection, and Segmentation competitions.
 
 In the Classification competition, the goal is to predict the set of labels
 contained in the image.
@@ -49,33 +49,24 @@ In the Detection competition, the goal is to predict the bounding box and label
 of each individual object.
 
 In the Segmentation competition, the goal is to predict a pixel-wise
-segmentation mask giving the class of the object visible at each pixel, or
-"background" otherwise.
+segmentation mask giving the class or instance id of the object visible at each
+pixel. Label 0 is reserved and is used to mark the background of images.
+Likewise, label 255 is reserved and is used to label regions of the image which
+should be ignored (the boundaries between classes/instances).
 
 WARNING: As per the official dataset, the test set of VOC2012 does not contain
 annotations.
 """
 
 _VOC_DETECTION_CONFIG_DESCRIPTION = """\
-This dataset contains the data from the PASCAL Visual Object Classes Challenge
-{year}, a.k.a. VOC{year}, corresponding to the Classification and Detection
-competitions.
-
-A total of {num_images} images are included in this dataset, where each image
-contains a set of objects, out of 20 different classes, making a total of
-{num_objects} annotated objects.
+This config contains the data from the VOC{year} Classification and Detection
+competitions. A total of {num_images} images are included, with {num_objects}
+annotated objects.
 """
 
 _VOC_SEGMENTATION_CONFIG_DESCRIPTION_COMMON = """\
-This dataset contains the data from the PASCAL Visual Object Classes Challenge,
-{year}, a.k.a. VOC{year}, corresponding to the Segmentation competition.
-
-A total of {num_images} images are included in this dataset, where each image
-has corresponding class and instance masks which label each pixel with a class
-between 1 and 20, or an instance id. Label 0 is reserved and is used to mark the
-background of images. Likewise, label 255 is reserved and is used to label
-regions of the image which should be ignored (the boundaries between
-classes/instances).
+This config contains the data from the VOC{year} Segmentation competition. A
+total of {num_images} images are included.
 """
 _VOC_SEGMENTATION_CONFIG_DESCRIPTION_2007 = _VOC_SEGMENTATION_CONFIG_DESCRIPTION_COMMON.format(
         year=2007, num_images=632)
@@ -85,6 +76,7 @@ _VOC_SEGMENTATION_CONFIG_DESCRIPTION_2012 = _VOC_SEGMENTATION_CONFIG_DESCRIPTION
 The splits "train" and "validation" are the original training and validation
 splits from VOC2012. The VOC2012 test split is not included as it does not have
 publicly available annotations.
+
 Also included are splits "sbd_train" and "sbd_validation", containing data from
 the Semantic Boundaries Dataset. SBD is a superset of VOC2012; "sbd_train" and
 "sbd_validation" contain all the images of the training and validation splits of
@@ -132,42 +124,6 @@ _VOC_POSES = (
 )
 
 
-class IDGenerator(object):
-  """A helper class to generate example IDs."""
-
-  def __init__(self, filename, image_set_relative_path):
-    self.filename = filename
-    self.image_set_relative_path = image_set_relative_path
-
-  def generate(self, paths):
-    image_set_path = os.path.join(paths[self.filename], self.image_set_relative_path)
-    with tf.io.gfile.GFile(image_set_path, "r") as f:
-      for line in f:
-        yield line.strip()
-
-class IncludeExcludeIDGenerator(object):
-  """
-  An ID generator which yields every ID from `include_generators` which would
-  not be yielded by any generator of `exclude_generators`. This is necessary
-  for the splits 'sbd_train' and 'sbd_validation' of 'VOC/2012-segmentation'.
-  """
-
-  def __init__(self, include_generators, exclude_generators):
-    self.include_generators = include_generators
-    self.exclude_generators = exclude_generators
-
-  def generate(self, paths):
-    exclude_ids = set(
-        example_id
-        for generator in self.exclude_generators
-        for example_id in generator.generate(paths)
-    )
-    for generator in self.include_generators:
-      for example_id in generator.generate(paths):
-        if example_id not in exclude_ids:
-          yield example_id
-
-
 def _get_detection_objects(annotation_filepath):
   """Function to get all the detection objects from an annotation XML file."""
   with tf.io.gfile.GFile(annotation_filepath, "r") as f:
@@ -203,7 +159,7 @@ def _get_detection_objects(annotation_filepath):
     # pytype: enable=attribute-error
 
 
-class VocPath(object):
+class VocPath:
   def __init__(self, filename, relative_path):
     self.filename = filename
     self.relative_path = relative_path
@@ -214,30 +170,17 @@ class VocPath(object):
     ).format(example_id)
 
 
-class VOCExampleBuilder(object):
-  """A base class to build VOC examples."""
-
-  def __init__(self, year=None, filename=None, image_path=None):
-    if image_path is None:
-      image_path = VocPath(filename,
-          "VOCdevkit/VOC{}/JPEGImages/{{}}.jpg".format(year))
-    self.image_path = image_path
-
-  def build(self, paths, example_id):
-    return {
-        "image": self.image_path.resolve(paths, example_id),
-        "image/filename": example_id + ".jpg",
-    }
-
-
-class DetectionExampleBuilder(VOCExampleBuilder):
+class DetectionExampleBuilder:
   """A helper class to build VOC object detection examples."""
 
   def __init__(self, year=None, filename=None, image_path=None, annotation_path=None):
-    super(DetectionExampleBuilder, self).__init__(year, filename, image_path)
+    if image_path is None:
+      image_path = VocPath(filename,
+          "VOCdevkit/VOC{}/JPEGImages/{{}}.jpg".format(year))
     if annotation_path is None:
       annotation_path = VocPath(filename,
           "VOCdevkit/VOC{}/Annotations/{{}}.xml".format(year))
+    self.image_path = image_path
     self.annotation_path = annotation_path
 
   def build(self, paths, example_id):
@@ -254,7 +197,8 @@ class DetectionExampleBuilder(VOCExampleBuilder):
       labels = []
       labels_no_difficult = []
     return {
-        **super(DetectionExampleBuilder, self).build(paths, example_id),
+        "image": self.image_path.resolve(paths, example_id),
+        "image/filename": example_id + ".jpg",
         "objects": objects,
         "labels": labels,
         "labels_no_difficult": labels_no_difficult,
@@ -265,7 +209,7 @@ class SegmentationExampleBuilder(DetectionExampleBuilder):
 
   def __init__(self, year=None, filename=None, image_path=None, annotation_path=None,
       class_mask_path=None, instance_mask_path=None):
-    super(SegmentationExampleBuilder, self).__init__(year, filename, image_path, annotation_path)
+    super().__init__(year, filename, image_path, annotation_path)
     if class_mask_path is None:
       class_mask_path = VocPath(filename,
           "VOCdevkit/VOC{}/SegmentationClass/{{}}.png".format(year))
@@ -278,7 +222,6 @@ class SegmentationExampleBuilder(DetectionExampleBuilder):
   @staticmethod
   def _load_png_segmentation_mask(path):
     """Load a '.png' segmentation mask, ignoring any colour map."""
-
     with tf.io.gfile.GFile(path, "rb") as fp:
       image = tfds.core.lazy_imports.PIL_Image.open(fp)
     return np.expand_dims(np.array(image, dtype=np.uint8), axis=-1)
@@ -286,7 +229,6 @@ class SegmentationExampleBuilder(DetectionExampleBuilder):
   @staticmethod
   def _load_mat_segmentation_mask(path, key):
     """Load a '.mat' segmentation mask of the kind used in the SBD dataset."""
-
     record = tfds.core.lazy_imports.scipy.io.loadmat(path, struct_as_record=True)
     return np.expand_dims(record[key]["Segmentation"][0][0], axis=-1)
 
@@ -304,49 +246,70 @@ class SegmentationExampleBuilder(DetectionExampleBuilder):
         else self._load_png_segmentation_mask(instance_mask_filepath)
     )
     return {
-        **super(SegmentationExampleBuilder, self).build(paths, example_id),
+        **super().build(paths, example_id),
         "segmentation/class": class_mask,
         "segmentation/instance": instance_mask,
     }
 
 
-class SplitConfig(object):
-  def __init__(self, name, id_generator, example_builder):
+class IDGenerator:
+  """A helper class to generate example IDs."""
+
+  def __init__(self, filename, image_set_relative_path):
+    self.filename = filename
+    self.image_set_relative_path = image_set_relative_path
+
+  def generate(self, paths):
+    image_set_path = os.path.join(paths[self.filename], self.image_set_relative_path)
+    with tf.io.gfile.GFile(image_set_path, "r") as f:
+      for line in f:
+        yield line.strip()
+
+
+class SplitConfig:
+  def __init__(self, name, id_generator, example_builder, exclude_id_generators=[]):
     self.name = name
     self.id_generator = id_generator
     self.example_builder = example_builder
+    self.exclude_id_generators = exclude_id_generators
 
 
 class VocConfig(tfds.core.BuilderConfig):
   """BuilderConfig for Voc."""
 
-  def __init__(self, homepage, citation, filenames=None, splits=[], **kwargs):
-    self.homepage = homepage
-    self.citation = citation
-    self.filenames = filenames
+  def __init__(self, year, has_segmentation, splits, **kwargs):
+    self.has_segmentation = has_segmentation
+    self.homepage = _VOC_URL.format(year=year)
+    self.citation = _VOC_CITATION.format(year=year)
+    if year == 2007:
+      self.filenames = {
+          "trainval": os.path.join(_VOC_DATA_URL, "VOCtrainval_06-Nov-2007.tar"),
+          "test": os.path.join(_VOC_DATA_URL, "VOCtest_06-Nov-2007.tar"),
+      }
+    else:
+      self.filenames = {
+          "trainval": os.path.join(_VOC_DATA_URL, "VOCtrainval_11-May-2012.tar"),
+          "test": os.path.join(_VOC_DATA_URL, "VOC2012test.tar"),
+      }
+    if year == 2012 and has_segmentation:
+      self.citation += _SBD_CITATION
+      self.filenames["sbd"] = os.path.join(_SBD_DATA_URL, "semantic_contours/benchmark.tgz")
     self.splits = splits
-    super(VocConfig, self).__init__(
+    super().__init__(
         # Version history:
         # 5.0.0: Added Segmentation BuildConfigs.
         # 4.0.0: Added BuildConfig and 2012 version support, deprecate Voc2007.
         # 3.0.0: S3 with new hashing function (different shuffle).
         # 2.0.0: S3 (new shuffling, sharding and slicing mechanism).
+        name=str(year) + ("-segmentation" if has_segmentation else ""),
         version=tfds.core.Version("5.0.0"),
         **kwargs)
 
   @property
   def features(self):
-    return tfds.features.FeaturesDict({
+    features = {
         "image": tfds.features.Image(shape=(None, None, 3), dtype=tf.uint8),
         "image/filename": tfds.features.Text(),
-    })
-
-
-class VocDetectionConfig(VocConfig):
-  @property
-  def features(self):
-    return tfds.features.FeaturesDict({
-        **super(VocDetectionConfig, self).features,
         "objects": tfds.features.Sequence({
             "label": tfds.features.ClassLabel(names=_VOC_LABELS),
             "bbox": tfds.features.BBoxFeature(),
@@ -358,33 +321,24 @@ class VocDetectionConfig(VocConfig):
             tfds.features.ClassLabel(names=_VOC_LABELS)),
         "labels_no_difficult": tfds.features.Sequence(
             tfds.features.ClassLabel(names=_VOC_LABELS)),
-    })
-
-
-class VocSegmentationConfig(VocDetectionConfig):
-  @property
-  def features(self):
-    return tfds.features.FeaturesDict({
-        **super(VocSegmentationConfig, self).features,
-        "segmentation/class": tfds.features.Image(shape=(None, None, 1), dtype=tf.uint8),
-        "segmentation/instance": tfds.features.Image(shape=(None, None, 1), dtype=tf.uint8),
-    })
+    }
+    if self.has_segmentation:
+      features.update({
+          "segmentation/class": tfds.features.Image(shape=(None, None, 1), dtype=tf.uint8),
+          "segmentation/instance": tfds.features.Image(shape=(None, None, 1), dtype=tf.uint8),
+      })
+    return tfds.features.FeaturesDict(features)
 
 
 class Voc(tfds.core.GeneratorBasedBuilder):
   """Pascal VOC 2007 or 2012."""
 
   BUILDER_CONFIGS = [
-      VocDetectionConfig(
-          name="2007",
+      VocConfig(
+          year=2007,
+          has_segmentation=False,
           description=_VOC_DETECTION_CONFIG_DESCRIPTION.format(
               year=2007, num_images=9963, num_objects=24640),
-          homepage=_VOC_URL.format(year="2007"),
-          citation=_VOC_CITATION.format(year="2007"),
-          filenames={
-              "trainval": os.path.join(_VOC_DATA_URL, "VOCtrainval_06-Nov-2007.tar"),
-              "test": os.path.join(_VOC_DATA_URL, "VOCtest_06-Nov-2007.tar"),
-          },
           splits=[
               SplitConfig(
                   name=tfds.Split.TRAIN,
@@ -400,16 +354,11 @@ class Voc(tfds.core.GeneratorBasedBuilder):
                   example_builder=DetectionExampleBuilder(year=2007, filename="test")),
           ],
       ),
-      VocDetectionConfig(
-          name="2012",
+      VocConfig(
+          year=2012,
+          has_segmentation=False,
           description=_VOC_DETECTION_CONFIG_DESCRIPTION.format(
               year=2012, num_images=11540, num_objects=27450),
-          homepage=_VOC_URL.format(year="2012"),
-          citation=_VOC_CITATION.format(year="2012"),
-          filenames={
-              "trainval": os.path.join(_VOC_DATA_URL, "VOCtrainval_11-May-2012.tar"),
-              "test": os.path.join(_VOC_DATA_URL, "VOC2012test.tar"),
-          },
           splits=[
               SplitConfig(
                   name=tfds.Split.TRAIN,
@@ -427,15 +376,10 @@ class Voc(tfds.core.GeneratorBasedBuilder):
                   example_builder=DetectionExampleBuilder(year=2012, filename="test", annotation_path=False)),
           ],
       ),
-      VocSegmentationConfig(
-          name="2007-segmentation",
+      VocConfig(
+          year=2007,
+          has_segmentation=True,
           description=_VOC_SEGMENTATION_CONFIG_DESCRIPTION_2007,
-          homepage=_VOC_URL.format(year=2007),
-          citation=_VOC_CITATION.format(year=2007),
-          filenames={
-              "trainval": os.path.join(_VOC_DATA_URL, "VOCtrainval_06-Nov-2007.tar"),
-              "test": os.path.join(_VOC_DATA_URL, "VOCtest_06-Nov-2007.tar"),
-          },
           splits=[
               SplitConfig(
                   name=tfds.Split.TRAIN,
@@ -451,16 +395,10 @@ class Voc(tfds.core.GeneratorBasedBuilder):
                   example_builder=SegmentationExampleBuilder(year=2007, filename="test")),
           ],
       ),
-      VocSegmentationConfig(
-          name="2012-segmentation",
+      VocConfig(
+          year=2012,
+          has_segmentation=True,
           description=_VOC_SEGMENTATION_CONFIG_DESCRIPTION_2012,
-          homepage=_VOC_URL.format(year="2012"),
-          citation=_VOC_CITATION.format(year="2012") + _SBD_CITATION,
-          filenames={
-              "trainval": os.path.join(_VOC_DATA_URL, "VOCtrainval_11-May-2012.tar"),
-              "test": os.path.join(_VOC_DATA_URL, "VOC2012test.tar"),
-              "sbd": os.path.join(_SBD_DATA_URL, "semantic_contours/benchmark.tgz"),
-          },
           splits=[
               SplitConfig(
                   name=tfds.Split.TRAIN,
@@ -474,37 +412,25 @@ class Voc(tfds.core.GeneratorBasedBuilder):
               ),
               SplitConfig(
                   name="sbd_train",
-                  id_generator=IncludeExcludeIDGenerator(
-                      # The images of 'sbd_train' are all those contained in the
-                      # SBD train split, excluding any included in the VOC2012
-                      # train or validation split.
-                      include_generators=[IDGenerator("sbd", "benchmark_RELEASE/dataset/train.txt")],
-                      exclude_generators=[
-                          IDGenerator("trainval", "VOCdevkit/VOC2012/ImageSets/Segmentation/train.txt"),
-                          IDGenerator("trainval", "VOCdevkit/VOC2012/ImageSets/Segmentation/val.txt"),
-                      ],
-                  ),
+                  id_generator=IDGenerator("sbd", "benchmark_RELEASE/dataset/train.txt"),
+                  exclude_id_generators=[
+                      IDGenerator("trainval", "VOCdevkit/VOC2012/ImageSets/Segmentation/train.txt"),
+                      IDGenerator("trainval", "VOCdevkit/VOC2012/ImageSets/Segmentation/val.txt"),
+                  ],
                   example_builder=SegmentationExampleBuilder(
-                      year=2012,
-                      filename="trainval",
+                      year=2012, filename="trainval",
                       class_mask_path=VocPath("sbd", "benchmark_RELEASE/dataset/cls/{}.mat"),
                       instance_mask_path=VocPath("sbd", "benchmark_RELEASE/dataset/inst/{}.mat")),
               ),
               SplitConfig(
                   name="sbd_validation",
-                  id_generator=IncludeExcludeIDGenerator(
-                      # The images of 'sbd_validation' are all those contained
-                      # in the SBD validation split, excluding any included in
-                      # the VOC2012 train or validation split.
-                      include_generators=[IDGenerator("sbd", "benchmark_RELEASE/dataset/val.txt")],
-                      exclude_generators=[
-                          IDGenerator("trainval", "VOCdevkit/VOC2012/ImageSets/Segmentation/train.txt"),
-                          IDGenerator("trainval", "VOCdevkit/VOC2012/ImageSets/Segmentation/val.txt"),
-                      ],
-                  ),
+                  id_generator=IDGenerator("sbd", "benchmark_RELEASE/dataset/val.txt"),
+                  exclude_id_generators=[
+                      IDGenerator("trainval", "VOCdevkit/VOC2012/ImageSets/Segmentation/train.txt"),
+                      IDGenerator("trainval", "VOCdevkit/VOC2012/ImageSets/Segmentation/val.txt"),
+                  ],
                   example_builder=SegmentationExampleBuilder(
-                      year=2012,
-                      filename="trainval",
+                      year=2012, filename="trainval",
                       class_mask_path=VocPath("sbd", "benchmark_RELEASE/dataset/cls/{}.mat"),
                       instance_mask_path=VocPath("sbd", "benchmark_RELEASE/dataset/inst/{}.mat")),
               ),
@@ -515,7 +441,7 @@ class Voc(tfds.core.GeneratorBasedBuilder):
   def _info(self):
     return tfds.core.DatasetInfo(
         builder=self,
-        description=self.builder_config.description,
+        description=_VOC_DESCRIPTION,
         features=self.builder_config.features,
         homepage=self.builder_config.homepage,
         citation=self.builder_config.citation,
@@ -529,6 +455,7 @@ class Voc(tfds.core.GeneratorBasedBuilder):
             name=split.name,
             gen_kwargs={
                 "id_generator": split.id_generator,
+                "exclude_id_generators": split.exclude_id_generators,
                 "example_builder": split.example_builder,
                 "paths": paths,
             },
@@ -536,8 +463,14 @@ class Voc(tfds.core.GeneratorBasedBuilder):
         for split in self.builder_config.splits
     ]
 
-  def _generate_examples(self, paths, id_generator, example_builder):
+  def _generate_examples(self, paths, id_generator, exclude_id_generators, example_builder):
     """Yields examples."""
 
+    exclude_ids = set(
+        example_id
+        for generator in exclude_id_generators
+        for example_id in generator.generate(paths)
+    )
     for example_id in id_generator.generate(paths):
-      yield example_id, example_builder.build(paths, example_id)
+      if example_id not in exclude_ids:
+        yield example_id, example_builder.build(paths, example_id)
