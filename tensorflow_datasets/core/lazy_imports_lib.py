96c6512d769794b89cdbc367d2d9ef9b4ff01af5
Harsh Soni <devilincarcerated020@yahoo.com>
2020-11-28 00:38:45 +0530

    Show Bbox image in df

--- tensorflow_datasets/core/lazy_imports_lib.py
+++ tensorflow_datasets/core/lazy_imports_lib.py
@@ -117,6 +117,14 @@ class LazyImporter(object):
 
   @utils.classproperty
   @classmethod
+  def PIL_ImageDraw(cls):  # pylint: disable=invalid-name
+    # TiffImagePlugin need to be activated explicitly on some systems
+    # https://github.com/python-pillow/Pillow/blob/5.4.x/src/PIL/Image.py#L407
+    _try_import("PIL.TiffImagePlugin")
+    return _try_import("PIL.ImageDraw")
+
+  @utils.classproperty
+  @classmethod
   def pretty_midi(cls):
     return _try_import("pretty_midi")
 
