ca17b5712ac25598310a5a498ccb60feef111fd2
Etienne Pot <epot@google.com>
2021-01-12 08:19:57 -0800

    Fix extraction error when archive contains bad symlink
    PiperOrigin-RevId: 351374376

--- tensorflow_datasets/core/download/extractor.py
+++ tensorflow_datasets/core/download/extractor.py
@@ -160,7 +160,21 @@ def iter_tar(arch_f, stream=False):
         # Links cannot be dereferenced in stream mode.
         logging.warning('Skipping link during extraction: %s', member.name)
         continue
-      extract_file = tar.extractfile(member)
+
+      try:
+        extract_file = tar.extractfile(member)
+      except KeyError:
+        if not (member.islnk() or member.issym()):
+          raise  # Forward exception non-link files which couldn't be extracted
+        # The link could not be extracted, which likely indicates a corrupted
+        # archive.
+        logging.warning(
+            'Skipping extraction of invalid link: %s -> %s',
+            member.name,
+            member.linkname,
+        )
+        continue
+
       if extract_file:  # File with data (not directory):
         path = _normpath(member.path)  # pytype: disable=attribute-error
         if not path:
