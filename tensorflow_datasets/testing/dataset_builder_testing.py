c7fa3da980498ee7c1cbf2fd744f94ac3c16d321
Harsh Soni <devilincarcerated020@yahoo.com>
2021-01-25 02:23:04 +0530

    Support custom BuilderConfig in testing

--- tensorflow_datasets/testing/dataset_builder_testing.py
+++ tensorflow_datasets/testing/dataset_builder_testing.py
@@ -300,8 +300,13 @@ class DatasetBuilderTestCase(
   @test_utils.run_in_graph_and_eager_modes()
   def test_download_and_prepare_as_dataset(self):
     # If configs specified, ensure they are all valid
+    test_config_names = []  # extract names of all builder configs to test
+
     if self.BUILDER_CONFIG_NAMES_TO_TEST:
       for config in self.BUILDER_CONFIG_NAMES_TO_TEST:  # pylint: disable=not-an-iterable
+        if isinstance(config, dataset_builder.BuilderConfig):
+          config = config.name
+        test_config_names.append(config)
         assert config in self.builder.builder_configs, (
             "Config %s specified in test does not exist. Available:\n%s" % (
                 config, list(self.builder.builder_configs)))
@@ -311,8 +316,8 @@ class DatasetBuilderTestCase(
     if configs:
       for config in configs:
         # Skip the configs that are not in the list.
-        if (self.BUILDER_CONFIG_NAMES_TO_TEST is not None and
-            (config.name not in self.BUILDER_CONFIG_NAMES_TO_TEST)):  # pylint: disable=unsupported-membership-test
+        if (len(test_config_names) > 0 and
+            (config.name not in test_config_names)):  # pylint: disable=unsupported-membership-test
           print("Skipping config %s" % config.name)
           continue
         with self._subTest(config.name):
