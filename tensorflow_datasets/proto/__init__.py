4eca24ce59e9bffd0b8eb56ed75a16ccf9dfed89
Jonathan Shen <jonathanasdf@google.com>
2021-02-03 01:56:23 -0800

    Fix waymo open dataset proto import.
    PiperOrigin-RevId: 355350302

--- tensorflow_datasets/proto/__init__.py
+++ tensorflow_datasets/proto/__init__.py
@@ -17,7 +17,7 @@
 # pylint: disable=g-import-not-at-top,g-importing-member, import-outside-toplevel
 
 try:  # pylint: disable=g-statement-before-imports
-  from waymo_open_dataset import waymo_dataset_pb2
+  from waymo_open_dataset import dataset_pb2 as waymo_dataset_pb2
 except ImportError:
   # If original waymo proto is not found, fallback to the pre-generated proto
   from tensorflow_datasets.proto import waymo_dataset_generated_pb2 as waymo_dataset_pb2  # pylint: disable=line-too-long
