f1441e168299539d1451a0b5de424d882327b645
NikhilBartwal <nikhilbartwal1234@gmail.com>
2021-03-19 01:49:01 +0530

    Namespace flag completed

--- tensorflow_datasets/scripts/cli/list.py
+++ tensorflow_datasets/scripts/cli/list.py
@@ -3,8 +3,9 @@
 
 import argparse
 
+from tensorflow_datasets.core import community
 from tensorflow_datasets.core import load
-
+from tensorflow_datasets.core import visibility
 
 def register_subparser(parsers: argparse._SubParsersAction) -> None:  # pylint: disable=protected-access
   """Add subparser for `list` command."""
@@ -13,7 +14,7 @@ def register_subparser(parsers: argparse._SubParsersAction) -> None:  # pylint: 
   new_parser.add_argument(
     '--namespace',
     type=str,
-    nargs='+',
+    nargs=1,
     help='Namespace flag for displaying datasets from given namespace'
   )
 
@@ -32,9 +33,9 @@ def _list_datasets(args: argparse.Namespace) -> None:
   if not args.namespace and not args.search_query:
     list_datasets()
   elif args.namespace:
-    list_namespace_datasets(namespace: args.namespace)
+    list_namespace_datasets(namespace=args.namespace)
   elif args.search_query:
-    search_datasets(search_query: args.search_query)
+    search_datasets(search_query=args.search_query)
 
 def list_datasets():
   datasets = load.list_builders(with_community_datasets=True)
@@ -42,4 +43,19 @@ def list_datasets():
     print(dataset)
 
 def list_namespace_datasets(namespace: str) -> None:
-  
+  datasets = list()
+  dataset_found = False
+  #Fetching all community datasets
+  if visibility.DatasetType.COMMUNITY_PUBLIC.is_available():
+    datasets = community.community_register.list_builders()
+
+  print(f"Datasets associated with `{namespace[0]}` namespace:")
+  #Getting all datasets for given namespace
+  for dataset in datasets:
+    if dataset.split(':')[0] == namespace[0]:
+      print(dataset)
+      if not dataset_found:
+        dataset_found = True
+
+  if not dataset_found:
+    print("No datasets match given namespace.")
