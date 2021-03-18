6aecbb54ce9f95691e9c8acf4ea78d28a4d2da14
NikhilBartwal <nikhilbartwal1234@gmail.com>
2021-03-19 01:16:17 +0530

    Add namespace keyword argument

--- tensorflow_datasets/scripts/cli/list.py
+++ tensorflow_datasets/scripts/cli/list.py
@@ -9,11 +9,20 @@ from tensorflow_datasets.core import load
 def register_subparser(parsers: argparse._SubParsersAction) -> None:  # pylint: disable=protected-access
   """Add subparser for `list` command."""
   new_parser = parsers.add_parser('list', help='Lists all datasets')
+
+  new_parser.add_argument(
+    '--namespace',
+    type=str,
+    nargs='*',
+    dest='namespace_keyword',
+    help='Namespace flag for displaying datasets from given namespace'
+  )
+
   new_parser.set_defaults(subparser_fn=_list_datasets)
 
 
 def _list_datasets(args: argparse.Namespace) -> None:
-  """Creates the dataset directory. Executed by `tfds new <name>`."""
+  """Lists the datasets/namespaces. Executed by `tfds list <flags> <name>`."""
   list_datasets()
 
 
