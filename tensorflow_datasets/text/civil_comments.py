4dee7777b5e776bda9428a68110113d666627e93
Ian Kivlichan <kivlichan@google.com>
2021-03-30 07:33:42 -0700

    Explicitly include 'id' field in civil_comments TFDS examples
    PiperOrigin-RevId: 365807019

--- tensorflow_datasets/text/civil_comments.py
+++ tensorflow_datasets/text/civil_comments.py
@@ -134,7 +134,7 @@ class CivilComments(tfds.core.GeneratorBasedBuilder):
   def _info(self):
     features = {'text': tfds.features.Text()}
     labels = [
-        'toxicity', 'severe_toxicity', 'obscene', 'threat', 'insult',
+        'id', 'toxicity', 'severe_toxicity', 'obscene', 'threat', 'insult',
         'identity_attack', 'sexual_explicit'
     ]
     if self.builder_config.include_identity_labels:
@@ -194,6 +194,7 @@ class CivilComments(tfds.core.GeneratorBasedBuilder):
 
   def _parse_row_as_example(self, row, toxicity_label, other_labels):
     example = {}
+    example['id'] = row['id']
     example['text'] = row['comment_text']
     example['toxicity'] = float(row[toxicity_label])
 
