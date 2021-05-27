c268120957166f8b5f10355ad7f0deee374bac4c
Sabela Ramos <sabela@google.com>
2021-05-27 02:35:26 -0700

    Move _TASK and _EPISODE_LENGTH outside of the class in rlu_dmlab_seekavoid_arena01
    PiperOrigin-RevId: 376126743

--- tensorflow_datasets/rl_unplugged/rlu_dmlab_seekavoid_arena01/rlu_dmlab_seekavoid_arena01.py
+++ tensorflow_datasets/rl_unplugged/rlu_dmlab_seekavoid_arena01/rlu_dmlab_seekavoid_arena01.py
@@ -19,6 +19,14 @@ import tensorflow_datasets.public_api as tfds
 from tensorflow_datasets.rl_unplugged import dmlab_dataset
 
 
+_TASK = 'seekavoid_arena_01'
+_EPISODE_LENGTH = 301
+_CONFIG_NAMES = ['training_0', 'training_1', 'training_2',
+                 'snapshot_0_eps_0.0', 'snapshot_1_eps_0.0',
+                 'snapshot_0_eps_0.01', 'snapshot_1_eps_0.01',
+                 'snapshot_0_eps_0.25', 'snapshot_1_eps_0.25']
+
+
 class RluDmlabSeekavoidArena01(dmlab_dataset.DMLabDatasetBuilder):
   """DatasetBuilder for rlu_dmlab_seekavoid_arena01 dataset."""
 
@@ -27,38 +35,11 @@ class RluDmlabSeekavoidArena01(dmlab_dataset.DMLabDatasetBuilder):
       '1.0.0': 'Initial release.',
       '1.0.1': 'Complete list of builder configs.',
   }
-  _TASK = 'seekavoid_arena_01'
-  _EPISODE_LENGTH = 301
 
   # pytype: disable=wrong-keyword-args
   BUILDER_CONFIGS = [
       dmlab_dataset.BuilderConfig(
-          name='training_0', task=_TASK, episode_length=_EPISODE_LENGTH),
-      dmlab_dataset.BuilderConfig(
-          name='training_1', task=_TASK, episode_length=_EPISODE_LENGTH),
-      dmlab_dataset.BuilderConfig(
-          name='training_2', task=_TASK, episode_length=_EPISODE_LENGTH),
-      dmlab_dataset.BuilderConfig(
-          name='snapshot_0_eps_0.0', task=_TASK,
-          episode_length=_EPISODE_LENGTH),
-      dmlab_dataset.BuilderConfig(
-          name='snapshot_1_eps_0.0', task=_TASK,
-          episode_length=_EPISODE_LENGTH),
-      dmlab_dataset.BuilderConfig(
-          name='snapshot_0_eps_0.01',
-          task=_TASK,
-          episode_length=_EPISODE_LENGTH),
-      dmlab_dataset.BuilderConfig(
-          name='snapshot_1_eps_0.01',
-          task=_TASK,
-          episode_length=_EPISODE_LENGTH),
-      dmlab_dataset.BuilderConfig(
-          name='snapshot_0_eps_0.25',
-          task=_TASK,
-          episode_length=_EPISODE_LENGTH),
-      dmlab_dataset.BuilderConfig(
-          name='snapshot_1_eps_0.25',
-          task=_TASK,
-          episode_length=_EPISODE_LENGTH),
+          name=name, task=_TASK, episode_length=_EPISODE_LENGTH)
+      for name in _CONFIG_NAMES
   ]
   # pytype: enable=wrong-keyword-args
