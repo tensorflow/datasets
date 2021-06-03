f707f598448313cf0ea79be4d36b00a12ed1b107
whatwilliam <william.wh.huang@gmail.com>
2021-06-03 19:03:17 +0000

    testing

--- tensorflow_datasets/image/ribfrac_test.py
+++ tensorflow_datasets/image/ribfrac_test.py
@@ -18,10 +18,12 @@ class RibfracTest(tfds.testing.DatasetBuilderTestCase):
   #   dl_manager.download({'some_key': 'http://a.org/out.txt', ...})
   # then the tests needs to provide the fake output paths relative to the
   # fake data directory
-  DL_EXTRACT_RESULT = {
-    'image': 'ribfrac-val-images/RibFrac421-image.nii.gz',
-    'mask': 'ribfrac-val-labels/RibFrac421-label.nii.gz',
-    'csv': 'ribfrac-val-info.csv'
+  DL_EXTRACT_RESULT = { 
+    'valid': {
+      'valid_images_1': 'ribfrac-val-images/RibFrac421-image.nii.gz',
+      'valid_masks_1': 'ribfrac-val-labels/RibFrac421-label.nii.gz',
+      'csv_1': 'ribfrac-val-info.csv'
+    }
   }
 
 
