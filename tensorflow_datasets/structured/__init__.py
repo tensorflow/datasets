9d1f9ab09359482aa4605f0f75719f0e75a660cc
Alfonso Castaño <alcastano@google.com>
2021-06-24 01:00:38 -0700

    Add Covid-19 dataset.
    PiperOrigin-RevId: 381199376

--- tensorflow_datasets/structured/__init__.py
+++ tensorflow_datasets/structured/__init__.py
@@ -17,6 +17,7 @@
 
 from tensorflow_datasets.structured.amazon_us_reviews import AmazonUSReviews
 from tensorflow_datasets.structured.cherry_blossoms import CherryBlossoms
+from tensorflow_datasets.structured.covid19 import Covid19
 from tensorflow_datasets.structured.dart import Dart
 from tensorflow_datasets.structured.e2e_cleaned import E2eCleaned
 from tensorflow_datasets.structured.efron_morris_75 import EfronMorris75
