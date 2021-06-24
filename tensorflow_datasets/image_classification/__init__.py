ecab4d3e0636b57e0be98fb66141c7b2d5bc52e3
Pierre Ruyssen <pierrot@google.com>
2021-06-24 06:13:23 -0700

    remove vgg_face2 dataset, no longer supported by their authors, and cannot be downloaded.
    PiperOrigin-RevId: 381239157

--- tensorflow_datasets/image_classification/__init__.py
+++ tensorflow_datasets/image_classification/__init__.py
@@ -82,5 +82,4 @@ from tensorflow_datasets.image_classification.stl10 import Stl10
 from tensorflow_datasets.image_classification.sun import Sun397
 from tensorflow_datasets.image_classification.svhn import SvhnCropped
 from tensorflow_datasets.image_classification.uc_merced import UcMerced
-from tensorflow_datasets.image_classification.vgg_face2 import VggFace2
 from tensorflow_datasets.image_classification.visual_domain_decathlon import VisualDomainDecathlon
