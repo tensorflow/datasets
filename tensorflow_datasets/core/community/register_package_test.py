5e5814ba4ad433194381838a0b48c067d8f8a674
Alfonso Castaño <alcastano@google.com>
2021-07-01 05:28:35 -0700

    fix test
    PiperOrigin-RevId: 382503588

--- tensorflow_datasets/core/community/register_package_test.py
+++ tensorflow_datasets/core/community/register_package_test.py
@@ -95,7 +95,7 @@ def test_builder_cls(dummy_register):  # pylint: disable=redefined-outer-name
   builder_cls = dummy_register.builder_cls(ds_name)
   assert builder_cls.name == 'dummy_dataset'
 
-  clshash = '1de59094bbe913e9a95aa0cff6f46bc06d813bd5c288eac34950b473e4ef199c'
+  clshash = '31c83eccbfe3dcd7dab023e3bcfadd61aee9f395f47a98a2bbca9d0b85a73cd6'
   assert installed_path / f'{clshash}/dummy_dataset.py' == builder_cls.code_path
   assert 'kaggle' in builder_cls.code_path.parts
   assert issubclass(builder_cls, dataset_builder.DatasetBuilder)
