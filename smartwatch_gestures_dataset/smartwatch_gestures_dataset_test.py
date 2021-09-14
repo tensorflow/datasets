23b49bf1631ed166aa6af39d2c36e05de9d979eb
Ian Zurutuza <Ian Zurutuza>
2021-09-14 10:36:31 -0500

    tf license

--- smartwatch_gestures_dataset/smartwatch_gestures_dataset_test.py
+++ smartwatch_gestures_dataset/smartwatch_gestures_dataset_test.py
@@ -1,3 +1,17 @@
+# coding=utf-8
+# Copyright 2021 The TensorFlow Datasets Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """smartwatch_gestures_dataset dataset."""
 
 import tensorflow_datasets as tfds
@@ -10,14 +24,7 @@ class SmartwatchGesturesDatasetTest(tfds.testing.DatasetBuilderTestCase):
   DATASET_CLASS = smartwatch_gestures_dataset.SmartwatchGesturesDataset
   SPLITS = {
       'train': 3,  # Number of fake train example
-      'test': 1,  # Number of fake test example
   }
-
-  # If you are calling `download/download_and_extract` with a dict, like:
-  #   dl_manager.download({'some_key': 'http://a.org/out.txt', ...})
-  # then the tests needs to provide the fake output paths relative to the
-  # fake data directory
-  # DL_EXTRACT_RESULT = {'some_key': 'output_file1.txt', ...}
 
 
 if __name__ == '__main__':
