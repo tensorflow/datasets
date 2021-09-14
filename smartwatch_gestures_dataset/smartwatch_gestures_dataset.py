6fc9251aa6519a30a9bba61d94a047ab13804029
Ian Zurutuza <Ian Zurutuza>
2021-09-14 10:32:27 -0500

    removed duplicate function doc-string

--- smartwatch_gestures_dataset/smartwatch_gestures_dataset.py
+++ smartwatch_gestures_dataset/smartwatch_gestures_dataset.py
@@ -1,3 +1,17 @@
+# coding=utf-8
+# Copyright 2021 The TensorFlow Datasets Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """smartwatch_gestures_dataset dataset."""
 
 import tensorflow as tf
@@ -63,7 +77,6 @@ class SmartwatchGesturesDataset(tfds.core.GeneratorBasedBuilder):
     )
 
   def _split_generators(self, dl_manager: tfds.download.DownloadManager):
-    """Returns SplitGenerators."""
     """Returns SplitGenerators."""
     # (tev_smartwatch_gestures_dataset): Downloads the data
     path = dl_manager.download_and_extract('https://drive.google.com/uc?export=download&id=1nEs-JlAQv6xpuSIqahTKK68TgK37GirP')
