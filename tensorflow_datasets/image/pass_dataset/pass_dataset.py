bad4440efe97519c220ade5053b8765c38c1c814
yukimasano <yuki.m.asano@gmail.com>
2021-09-27 08:42:51 +0200

    use tar files, add metadata, fix reference to YFCC, added checksums

--- tensorflow_datasets/image/pass_dataset/pass_dataset.py
+++ tensorflow_datasets/image/pass_dataset/pass_dataset.py
@@ -2,6 +2,9 @@
 
 import tensorflow_datasets as tfds
 import urllib.request
+import tensorflow.compat.v2 as tf
+import glob
+pd = tfds.core.lazy_imports.pandas
 
 _DESCRIPTION = """
 PASS is a large-scale image dataset that does not include any humans,
@@ -9,6 +12,9 @@ human parts, or other personally identifiable information.
 It that can be used for high-quality self-supervised pretraining while significantly reducing privacy concerns.
 
 PASS contains 1.440.191 images without any labels sourced from YFCC-100M.
+
+All images in this dataset are licenced under the CC-BY licence, as is the dataset itself.
+For YFCC-100M see  http://www.multimediacommons.org/.
 """
 
 _CITATION = """
@@ -20,41 +26,61 @@ year = "2021"
 }
 """
 
+_URLS = {
+    'train_images': [tfds.download.Resource(  # pylint:disable=g-complex-comprehension
+        url='https://zenodo.org/record/5528345/files/PASS.%s.tar' % i_,
+        extract_method=tfds.download.ExtractMethod.TAR)
+        for i_ in '0123456789'],
+    'meta_data':tfds.download.Resource(
+        url='https://zenodo.org/record/5528345/files/pass_metadata.csv')
+}
 
 class PASS(tfds.core.GeneratorBasedBuilder):
-  """DatasetBuilder for pass dataset."""
+    """DatasetBuilder for pass dataset."""
 
-  VERSION = tfds.core.Version('1.0.0')
-  RELEASE_NOTES = {
-      '1.0.0': 'Initial release.',
-  }
-
-  def _info(self) -> tfds.core.DatasetInfo:
-    """Returns the dataset metadata."""
-    return tfds.core.DatasetInfo(
-        builder=self,
-        description=_DESCRIPTION,
-        features=tfds.features.FeaturesDict({
-            'image': tfds.features.Image(shape=(None, None, 3)),
-        }),
-        supervised_keys=None,
-        homepage='https://www.robots.ox.ac.uk/~vgg/research/pass/',
-        citation=_CITATION,
-    )
-
-  def _split_generators(self, dl_manager: tfds.download.DownloadManager):
-    """Returns SplitGenerators."""
-    urls =  urllib.request.urlopen("https://www.robots.ox.ac.uk/~vgg/research/pass/pass_urls.txt")
-    urls = [li.decode("utf-8").split('\n')[0] for li in urls]
-    path = dl_manager.download(urls)
-
-    return {
-        'train': self._generate_examples(path),
+    VERSION = tfds.core.Version('1.0.0')
+    RELEASE_NOTES = {
+        '1.0.0': 'Initial release.',
     }
 
-  def _generate_examples(self, path):
-    """Yields examples."""
-    for idx,f in enumerate(path):
-      yield idx, {
-          'image': f,
-      }
+    def _info(self):
+        """Returns the dataset metadata."""
+        return tfds.core.DatasetInfo(
+            builder=self,
+            description=_DESCRIPTION,
+            features=tfds.features.FeaturesDict({
+                'image': tfds.features.Image(shape=(None, None, 3)),
+                'image/creator_uname': tf.string,
+            }),
+            supervised_keys=None,
+            homepage='https://www.robots.ox.ac.uk/~vgg/research/pass/',
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager: tfds.download.DownloadManager):
+        """Returns SplitGenerators."""
+        paths = dl_manager.download_and_extract(_URLS)
+        with tf.io.gfile.GFile(paths['meta_data']) as f:
+            meta = pd.read_csv(f)
+        meta = {m[1]['hash']:m[1]['unickname'] for m in meta.iterrows()}
+        return [tfds.core.SplitGenerator(
+            name=tfds.Split.TRAIN,
+            gen_kwargs=dict(
+                parts=paths['train_images'],
+                meta=meta),
+        )]
+
+
+    def _generate_examples(self, parts, meta):
+        """Yields examples."""
+        _idx = 0
+        for part in parts:
+            images = glob.glob(str(part)+'/PASS_dataset/*/*.jpg')
+            for image in images:
+                _idx += 1
+                image_path = image
+                record = {
+                    "image": image_path,
+                    "image/creator_uname": meta[image.split('/')[-1].split('.')[0]]
+                }
+                yield _idx, record
