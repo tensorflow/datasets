3a1aa57d596a3a140e4f51cdd7ce0fcf5210549e
yukimasano <yuki.m.asano@gmail.com>
2021-10-15 12:03:19 +0200

    update to pass v2.0.0

--- tensorflow_datasets/image/pass_dataset/pass_dataset.py
+++ tensorflow_datasets/image/pass_dataset/pass_dataset.py
@@ -15,16 +15,16 @@
 
 """PASS dataset."""
 
-import tensorflow.compat.v2 as tf
 import tensorflow_datasets.public_api as tfds
-
+import tensorflow.compat.v2 as tf
+pd = tfds.core.lazy_imports.pandas
 
 _DESCRIPTION = """
 PASS is a large-scale image dataset that does not include any humans,
 human parts, or other personally identifiable information.
 It that can be used for high-quality self-supervised pretraining while significantly reducing privacy concerns.
 
-PASS contains 1.440.191 images without any labels sourced from YFCC-100M.
+PASS contains 1,439,719 images without any labels sourced from YFCC-100M.
 
 All images in this dataset are licenced under the CC-BY licence, as is the dataset itself.
 For YFCC-100M see  http://www.multimediacommons.org/.
@@ -40,60 +40,69 @@ year = "2021"
 """
 
 _URLS = {
-    'train_images': [
-        tfds.download.Resource(  # pylint:disable=g-complex-comprehension
-            url=f'https://zenodo.org/record/5528345/files/PASS.{i}.tar',
-            extract_method=tfds.download.ExtractMethod.TAR) for i in range(10)
-    ],
-    'meta_data':
-        tfds.download.Resource(
-            url='https://zenodo.org/record/5528345/files/pass_metadata.csv')
+    'train_images': [tfds.download.Resource(  # pylint:disable=g-complex-comprehension
+        url='https://zenodo.org/record/5570664/files/PASS.%s.tar' % i_,
+        extract_method=tfds.download.ExtractMethod.TAR)
+        for i_ in '0123456789'],
+    'meta_data':tfds.download.Resource(
+        url='https://zenodo.org/record/5570664/files/pass_metadata.csv')
 }
 
-
 class PASS(tfds.core.GeneratorBasedBuilder):
-  """DatasetBuilder for pass dataset."""
+    """DatasetBuilder for pass dataset."""
 
-  VERSION = tfds.core.Version('1.0.0')
-  RELEASE_NOTES = {
-      '1.0.0': 'Initial release.',
-  }
-
-  def _info(self):
-    """Returns the dataset metadata."""
-    return tfds.core.DatasetInfo(
-        builder=self,
-        description=_DESCRIPTION,
-        features=tfds.features.FeaturesDict({
-            'image': tfds.features.Image(shape=(None, None, 3)),
-            'image/creator_uname': tf.string,
-        }),
-        supervised_keys=None,
-        homepage='https://www.robots.ox.ac.uk/~vgg/research/pass/',
-        citation=_CITATION,
-    )
-
-  def _split_generators(self, dl_manager: tfds.download.DownloadManager):
-    """Returns SplitGenerators."""
-    pd = tfds.core.lazy_imports.pandas
-
-    paths = dl_manager.download(_URLS)
-    with tf.io.gfile.GFile(paths['meta_data']) as f:
-      meta = pd.read_csv(f)
-    meta = {m[1]['hash']: m[1]['unickname'] for m in meta.iterrows()}
-    return {
-        'train':
-            self._generate_examples(dl_manager, paths['train_images'], meta)
+    VERSION = tfds.core.Version('2.0.0')
+    RELEASE_NOTES = {
+        '2.0.0': 'v2: Removed 472 images from v1 as they contained humans. Also added metadata: datetaken and GPS. ',
     }
 
-  def _generate_examples(self, dl_manager, parts, meta):
-    """Yields examples."""
-    i = 0
-    for part in parts:
-      for fname, fobj in dl_manager.iter_archive(part):
-        i += 1
-        record = {
-            'image': fobj,
-            'image/creator_uname': meta[fname.split('/')[-1].split('.')[0]]
-        }
-        yield i, record
+    def _info(self):
+        """Returns the dataset metadata."""
+        return tfds.core.DatasetInfo(
+            builder=self,
+            description=_DESCRIPTION,
+            features=tfds.features.FeaturesDict({
+                'image': tfds.features.Image(shape=(None, None, 3)),      # the image
+                'image/creator_uname': tf.string,                         # the photographer/creator
+                'image/hash':tf.string,                                   # the hash, as computed from YFCC-100M
+                'image/gps_lon':tf.float,                                 # Longitude of image if existent, otw. NaN
+                'image/gps_lat':tf.float,                                 # Latitude of image if existent, otw. NaN
+                'image/date_taken': pd._libs.tslibs.timestamps.Timestamp, # datetime of image
+            }),
+            supervised_keys=None,
+            homepage='https://www.robots.ox.ac.uk/~vgg/research/pass/',
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager: tfds.download.DownloadManager):
+        """Returns SplitGenerators."""
+        paths = dl_manager.download(_URLS)
+        with tf.io.gfile.GFile(paths['meta_data']) as f:
+            meta = pd.read_csv(f)
+        meta = meta.set_index('hash')
+        return [tfds.core.SplitGenerator(
+            name=tfds.Split.TRAIN,
+            gen_kwargs=dict(
+                parts=paths['train_images'],
+                meta=meta,
+                dl_manager=dl_manager),
+        )]
+
+
+    def _generate_examples(self, dl_manager, parts, meta):
+        """Yields examples."""
+        i = 0
+        for part in parts:
+            for fname, fobj in dl_manager.iter_archive(part):
+                i += 1
+                img_hash = fname.split('/')[-1].split('.')[0]
+                meta = meta.loc[img_hash]
+                record = {
+                    "image": fobj,
+                    "image/creator_uname": meta['unickname'],
+                    "image/hash": img_hash,
+                    "image/gps_lon": meta['longitude'],
+                    "image/gps_lat": meta['latitude'],
+                    "image/date_taken": pd.to_datetime(meta['datetime']),
+                }
+                yield i, record
--- tensorflow_datasets/image/pass_dataset/pass_dataset_test.py
+++ tensorflow_datasets/image/pass_dataset/pass_dataset_test.py
@@ -1,22 +1,7 @@
-# coding=utf-8
-# Copyright 2021 The TensorFlow Datasets Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 """pass dataset."""
 
-from tensorflow_datasets.image.pass_dataset import pass_dataset
-import tensorflow_datasets.public_api as tfds
+import tensorflow_datasets as tfds
+import pass_dataset
 
 
 class PASSTest(tfds.testing.DatasetBuilderTestCase):
@@ -27,7 +12,7 @@ class PASSTest(tfds.testing.DatasetBuilderTestCase):
   }
 
   DL_EXTRACT_RESULT = {
-      'train_images': ['pass_dataset/dummy.0.tar', 'pass_dataset/dummy.1.tar'],
+      'train_images': ['pass_dataset/dummy.0.tar','pass_dataset/dummy.1.tar'],
       'meta_data': 'dummy_meta.csv'
   }
 
