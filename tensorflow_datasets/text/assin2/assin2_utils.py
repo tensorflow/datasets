8af15fe493242a737110cead9fc6288902dbcabf
marcospiau <marcospiau123@gmail.com>
2021-12-23 22:44:07 -0300

    Files ready to PR

--- tensorflow_datasets/text/assin2/assin2_utils.py
+++ tensorflow_datasets/text/assin2/assin2_utils.py
@@ -1,3 +1,19 @@
+# coding=utf-8
+# Copyright 2021 The TensorFlow Datasets Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Util functions for assin2 dataset"""
+
 from xml.etree import cElementTree as ET
 from typing import List
 from dataclasses import dataclass
@@ -40,9 +56,7 @@ def parse_xml_string(xml_str: str) -> List[Pair]:
     # assert valid fields
     if entailment not in VALID_ENTAILMENT:
       raise ValueError(
-          'entailment should be in {}, got {}'.format(
-              VALID_ENTAILMENT, entailment
-          )
+          'entailment should be in {VALID_ENTAILMENT}, got {entailment}'
       )
     pairs.append(
         Pair(
