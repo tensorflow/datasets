c97fcb9580015173f6fe5e14a83ca101c7fabd9a
marcospiau <marcospiau123@gmail.com>
2021-12-23 22:44:07 -0300

    Files ready to PR

--- tensorflow_datasets/text/assin2/assin2.py
+++ tensorflow_datasets/text/assin2/assin2.py
@@ -1,3 +1,17 @@
+# coding=utf-8
+# Copyright 2021 The TensorFlow Datasets Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """assin2 dataset."""
 
 import tensorflow_datasets.public_api as tfds
@@ -6,11 +20,13 @@ from tensorflow_datasets.text.assin2.assin2_utils import parse_xml_string
 
 _HOMEPAGE = 'https://sites.google.com/view/assin2/english'
 
+# pylint: disable=line-too-long
 _DESCRIPTION = f"""\
 # Contextualization
 ASSIN 2 is the second edition of the Avaliação de Similaridade Semântica e
 Inferência Textual (Evaluating Semantic Similarity and Textual Entailment),
-and was a workshop collocated with [STIL 2019](http://www.google.com/url?q=http%3A%2F%2Fcomissoes.sbc.org.br%2Fce-pln%2Fstil2019%2F&sa=D&sntz=1&usg=AFQjCNHN8DosAsJ-gd48TfkXFX5YD6xM7g). It follows the [first edition of ASSIN](http://www.google.com/url?q=http%3A%2F%2Fpropor2016.di.fc.ul.pt%2F%3Fpage_id%3D381&sa=D&sntz=1&usg=AFQjCNHV7ySeNzH4k6MWKBLqO9yUkqiUqw), proposing a new shared task with new data.
+and was a workshop collocated with [STIL 2019](http://www.google.com/url?q=http%3A%2F%2Fcomissoes.sbc.org.br%2Fce-pln%2Fstil2019%2F&sa=D&sntz=1&usg=AFQjCNHN8DosAsJ-gd48TfkXFX5YD6xM7g). It follows the [first edition of ASSIN](http://www.google.com/url?q=http%3A%2F%2Fpropor2016.di.fc.ul.pt%2F%3Fpage_id%3D381&sa=D&sntz=1&usg=AFQjCNHV7ySeNzH4k6MWKBLqO9yUkqiUqw),
+proposing a new shared task with new data.
 
 The workshop evaluated systems that assess two types of relations between
 two sentences: Semantic Textual Similarity and Textual Entailment.
@@ -23,7 +39,8 @@ classifying whether the first sentence entails the second.
 The corpus used in ASSIN 2 is composed of rather simple sentences. Following
 the procedures of SemEval 2014 Task 1, we tried to remove from the corpus named
 entities and indirect speech, and tried to have all verbs in the present tense.
-The [annotation instructions](https://drive.google.com/open?id=1aUPhywEHD0r_pxPiTqZwS0fRj-1Xda2w) given to annotators are available (in Portuguese).
+The [annotation instructions](https://drive.google.com/open?id=1aUPhywEHD0r_pxPiTqZwS0fRj-1Xda2w)
+given to annotators are available (in Portuguese).
 
 The training and validation data are composed, respectively, of 6,500 and 500
 sentence pairs in Brazilian Portuguese, annotated for entailment and
@@ -43,6 +60,7 @@ the last edition.
 PS.: Description is extracted from [official homepage]({_HOMEPAGE}).
 """
 
+# pylint: disable=line-too-longm anomalous-backslash-in-string
 _CITATION = """
 @inproceedings{DBLP:conf/propor/RealFO20,
   author    = {Livy Real and
@@ -70,12 +88,12 @@ _CITATION = """
 }
 """
 _DOWNLOAD_URLS = {
-  'train':
-  'https://drive.google.com/u/0/uc?id=1Q9j1a83CuKzsHCGaNulSkNxBm7Dkn7Ln&export=download',
-  'validation':
-  'https://drive.google.com/u/0/uc?id=1kb7xq6Mb3eaqe9cOAo70BaG9ypwkIqEU&export=download',
-  'test':
-  'https://drive.google.com/u/0/uc?id=1J3FpQaHxpM-FDfBUyooh-sZF-B-bM_lU&export=download',
+    'train':
+        'https://drive.google.com/u/0/uc?id=1Q9j1a83CuKzsHCGaNulSkNxBm7Dkn7Ln&export=download',
+    'validation':
+        'https://drive.google.com/u/0/uc?id=1kb7xq6Mb3eaqe9cOAo70BaG9ypwkIqEU&export=download',
+    'test':
+        'https://drive.google.com/u/0/uc?id=1J3FpQaHxpM-FDfBUyooh-sZF-B-bM_lU&export=download',
 }
 
 
@@ -84,36 +102,43 @@ class Assin2(tfds.core.GeneratorBasedBuilder):
 
   VERSION = tfds.core.Version('1.0.0')
   RELEASE_NOTES = {
-    '1.0.0': 'Initial release.',
+      '1.0.0': 'Initial release.',
   }
 
   def _info(self) -> tfds.core.DatasetInfo:
     """Returns the dataset metadata."""
     return tfds.core.DatasetInfo(
-      builder=self,
-      description=_DESCRIPTION,
-      features=tfds.features.FeaturesDict({
-        'text':tfds.features.Text(),
-        'hypothesis': tfds.features.Text(),
-        'id': tf.int32,
-        'entailment': tfds.features.ClassLabel(names=['None', 'Entailment']),
-        'similarity': tf.float32
-      }),
-      # If there's a common (input, target) tuple from the
-      # features, specify them here. They'll be used if
-      # `as_supervised=True` in `builder.as_dataset`.
-      supervised_keys=None,  # Set to `None` to disable
-      homepage=_HOMEPAGE,
-      citation=_CITATION,
+        builder=self,
+        description=_DESCRIPTION,
+        features=tfds.features.FeaturesDict(
+            {
+                'text':
+                    tfds.features.Text(),
+                'hypothesis':
+                    tfds.features.Text(),
+                'id':
+                    tf.int32,
+                'entailment':
+                    tfds.features.ClassLabel(names=['None', 'Entailment']),
+                'similarity':
+                    tf.float32
+            }
+        ),
+        # If there's a common (input, target) tuple from the
+        # features, specify them here. They'll be used if
+        # `as_supervised=True` in `builder.as_dataset`.
+        supervised_keys=None,  # Set to `None` to disable
+        homepage=_HOMEPAGE,
+        citation=_CITATION,
     )
 
   def _split_generators(self, dl_manager: tfds.download.DownloadManager):
     """Returns SplitGenerators."""
     path = dl_manager.download_and_extract(_DOWNLOAD_URLS)
     return {
-      'train': self._generate_examples(path['train']),
-      'validation': self._generate_examples(path['validation']),
-      'test': self._generate_examples(path['test'])
+        'train': self._generate_examples(path['train']),
+        'validation': self._generate_examples(path['validation']),
+        'test': self._generate_examples(path['test'])
     }
 
   def _generate_examples(self, path):
@@ -123,9 +148,9 @@ class Assin2(tfds.core.GeneratorBasedBuilder):
     # pairs = read_xml(path)
     for pair in pairs:
       yield pair.id, {
-        'text': pair.text,
-        'hypothesis': pair.hypothesis,
-        'id': pair.id,
-        'entailment': pair.entailment,
-        'similarity': pair.similarity
+          'text': pair.text,
+          'hypothesis': pair.hypothesis,
+          'id': pair.id,
+          'entailment': pair.entailment,
+          'similarity': pair.similarity
       }
--- tensorflow_datasets/text/assin2/assin2_test.py
+++ tensorflow_datasets/text/assin2/assin2_test.py
@@ -1,8 +1,21 @@
+# coding=utf-8
+# Copyright 2021 The TensorFlow Datasets Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """assin2 dataset."""
 
 import tensorflow_datasets as tfds
 from . import assin2
-# import assin2
 
 
 class Assin2Test(tfds.testing.DatasetBuilderTestCase):
@@ -14,16 +27,11 @@ class Assin2Test(tfds.testing.DatasetBuilderTestCase):
       'validation': 5,
       'test': 5,
   }
-
-  # If you are calling `download/download_and_extract` with a dict, like:
-  #   dl_manager.download({'some_key': 'http://a.org/out.txt', ...})
-  # then the tests needs to provide the fake output paths relative to the
-  # fake data directory
   DL_EXTRACT_RESULT = {
-    'train': 'assin2-train-only.xml',
-    'validation': 'assin2-dev.xml',
-    'test': 'assin2-test.xml'
-    }
+      'train': 'assin2-train-only.xml',
+      'validation': 'assin2-dev.xml',
+      'test': 'assin2-test.xml'
+  }
 
 
 if __name__ == '__main__':
