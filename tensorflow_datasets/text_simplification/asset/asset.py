b84959f7ef6eb1fba6ba77ded391356ed1c8e824
TensorFlow Datasets Team <no-reply@google.com>
2022-01-03 08:15:27 -0800

    Fix small typo in ASSET dataset documentation.
    PiperOrigin-RevId: 419444773

--- tensorflow_datasets/text_simplification/asset/asset.py
+++ tensorflow_datasets/text_simplification/asset/asset.py
@@ -40,14 +40,14 @@ for Computational Linguistics",
 }
 """
 
-_DESCRIPTION = """\
-ASSET is a dataset for evaluating Sentence Simplification systems with \
-multiple rewriting transformations, as described in "ASSET: A Dataset for \
-Tuning and Evaluation of Sentence Simplification Models with Multiple \
-Rewriting Transformations." The corpus is composed of 2000 validation \
-and 359 test original sentences that were each simplified 10 times by \
-different annotators. The corpus also contains human judgments of meaning \
-preservation, fluency and simplicity for the outputs of several automatic \
+_DESCRIPTION = """
+ASSET is a dataset for evaluating Sentence Simplification systems with
+multiple rewriting transformations, as described in "ASSET: A Dataset for
+Tuning and Evaluation of Sentence Simplification Models with Multiple
+Rewriting Transformations." The corpus is composed of 2000 validation
+and 359 test original sentences that were each simplified 10 times by
+different annotators. The corpus also contains human judgments of meaning
+preservation, fluency and simplicity for the outputs of several automatic
 text simplification systems.
 """
 
