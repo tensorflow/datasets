a4eabfae188ec2f1003b2d2a81d8cef82a1e9e62
Peter J. Liu <peterjliu@google.com>
2022-01-14 11:29:27 -0800

    Add scrolls dataset.
    PiperOrigin-RevId: 421873856

--- tensorflow_datasets/text/__init__.py
+++ tensorflow_datasets/text/__init__.py
@@ -65,6 +65,7 @@ from tensorflow_datasets.text.scan import Scan
 from tensorflow_datasets.text.scan import ScanConfig
 from tensorflow_datasets.text.schema_guided_dialogue import SchemaGuidedDialogue
 from tensorflow_datasets.text.scicite import Scicite
+from tensorflow_datasets.text.scrolls import Scrolls
 from tensorflow_datasets.text.sentiment140 import Sentiment140
 from tensorflow_datasets.text.snli import Snli
 from tensorflow_datasets.text.squad_question_generation import SquadQuestionGeneration
