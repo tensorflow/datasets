244d635185d7a460a58d6b60963035aa44842290
Tom van der Weide <weide@google.com>
2022-03-03 05:06:17 -0800

    Enrich the community catalog for Huggingface datasets
    It uses the dataset_info.json in the Huggingface Github repo to extract dataset properties.
    
    PiperOrigin-RevId: 432167523

--- tensorflow_datasets/scripts/documentation/build_community_catalog_test.py
+++ tensorflow_datasets/scripts/documentation/build_community_catalog_test.py
@@ -14,8 +14,10 @@
 # limitations under the License.
 
 """Tests for build_community."""
+from unittest import mock
 
 from etils import epath
+
 import tensorflow_datasets as tfds  # pylint: disable=unused-import
 from tensorflow_datasets.core import utils
 from tensorflow_datasets.scripts.documentation import build_community_catalog
@@ -44,11 +46,15 @@ def _create_templates(
       namespace_toc_template=namespace_toc_template)
 
 
+def _create_options() -> build_community_catalog._Options:
+  return build_community_catalog._Options(catalog_dir='/tmp')
+
+
 def test_dataset_documentation_properties():
   dataset = _create_dataset_package('community1', 'ds1')
   templates = _create_templates()
   dataset_doc = build_community_catalog.DatasetDocumentation(
-      dataset=dataset, templates=templates)
+      dataset=dataset, templates=templates, options=_create_options())
   assert dataset_doc.tfds_id == 'community1:ds1'
   assert dataset_doc.name == 'ds1'
 
@@ -57,7 +63,7 @@ def test_dataset_documentation_links():
   dataset = _create_dataset_package('community1', 'ds1')
   templates = _create_templates()
   dataset_doc = build_community_catalog.DatasetDocumentation(
-      dataset=dataset, templates=templates)
+      dataset=dataset, templates=templates, options=_create_options())
   expected_code_url = '[Code](github://huggingface/datasets/tree/master/datasets/ds1)'
   assert dataset_doc.code_url() == expected_code_url
   assert dataset_doc.extra_links() == [expected_code_url]
@@ -70,7 +76,7 @@ def test_dataset_documentation_to_details_markdown():
   templates = _create_templates(
       dataset_details_template='{tfds_id}\n{references_bulleted_list}')
   dataset_doc = build_community_catalog.DatasetDocumentation(
-      dataset=dataset, templates=templates)
+      dataset=dataset, templates=templates, options=_create_options())
   assert dataset_doc.to_details_markdown() == (
       'community1:ds1\n*   '
       '[Code](github://huggingface/datasets/tree/master/datasets/ds1)')
@@ -80,7 +86,7 @@ def test_dataset_documentation_to_toc_markdown():
   dataset = _create_dataset_package('community1', 'ds1')
   templates = _create_templates()
   dataset_doc = build_community_catalog.DatasetDocumentation(
-      dataset=dataset, templates=templates)
+      dataset=dataset, templates=templates, options=_create_options())
   assert dataset_doc.to_toc_markdown(
   ) == 'ds1 ([Code](github://huggingface/datasets/tree/master/datasets/ds1))'
 
@@ -89,7 +95,7 @@ def test_huggingface_dataset_documentation():
   dataset = _create_dataset_package('community1', 'ds1')
   templates = _create_templates()
   dataset_doc = build_community_catalog.HuggingfaceDatasetDocumentation(
-      dataset=dataset, templates=templates)
+      dataset=dataset, templates=templates, options=_create_options())
   assert dataset_doc.extra_links() == [
       '[Code](https://github.com/huggingface/datasets/blob/master/datasets/ds1)',
       '[Huggingface](https://huggingface.co/datasets/ds1)',
@@ -101,38 +107,54 @@ def test_huggingface_formatter_overview_page():
   datasets = []
   templates = _create_templates()
   formatter = build_community_catalog.HuggingfaceFormatter(
-      namespace=namespace1, datasets=datasets, templates=templates)
+      namespace=namespace1,
+      datasets=datasets,
+      templates=templates,
+      options=_create_options())
   assert formatter.overview_page == 'community1.md'
 
 
-def test_huggingface_formatter_to_details_markdown():
-  namespace1 = 'community1'
+@mock.patch('tensorflow_datasets.core.github_api.github_path.get_content')
+def test_huggingface_formatter_to_namespace_overview(get_content_mock):
+  get_content_mock.return_value = '{}'
+  namespace1 = 'huggingface'
   datasets = [
       _create_dataset_package(namespace1, 'ds1'),
       _create_dataset_package(namespace1, 'ds2'),
   ]
   templates = _create_templates(dataset_details_template='{tfds_id}')
+  get_content_mock.return_value = '{}'
   formatter = build_community_catalog.HuggingfaceFormatter(
-      namespace=namespace1, datasets=datasets, templates=templates)
-  assert formatter.to_details_markdown() == """\
+      namespace=namespace1,
+      datasets=datasets,
+      templates=templates,
+      options=_create_options())
+  assert formatter.to_namespace_overview() == """\
 # Huggingface datasets
 
-Huggingface has forked TFDS and provide a lot of text datasets. See
+Huggingface has forked TFDS and provides a lot of text datasets. See
 [here](https://huggingface.co/docs/datasets/) for more documentation.
+Next you can find the list of all the datasets that can be used with TFDS.
 
 
-community1:ds1
-community1:ds2
+*  [ds1](huggingface/ds1.md)
+*  [ds2](huggingface/ds2.md)
 """
 
 
-def test_huggingface_formatter_to_toc_markdown():
+@mock.patch('tensorflow_datasets.core.github_api.github_path.get_content')
+def test_huggingface_formatter_to_toc_markdown(get_content_mock):
+  get_content_mock.return_value = '{}'
   namespace1 = 'community1'
   datasets = [
       _create_dataset_package(namespace1, 'ds1'),
       _create_dataset_package(namespace1, 'ds2'),
   ]
   templates = _create_templates(namespace_toc_template='{name}')
+
   formatter = build_community_catalog.HuggingfaceFormatter(
-      namespace=namespace1, datasets=datasets, templates=templates)
+      namespace=namespace1,
+      datasets=datasets,
+      templates=templates,
+      options=_create_options())
   assert formatter.to_toc_markdown() == 'Community1'
