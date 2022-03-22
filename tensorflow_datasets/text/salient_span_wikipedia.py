eb1cd3e03d588c2f441941b4f4bd70b99deec6f5
Yilei Yang <yileiyang@google.com>
2022-03-21 22:29:31 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 436387787

--- tensorflow_datasets/text/salient_span_wikipedia.py
+++ tensorflow_datasets/text/salient_span_wikipedia.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# lint as: python3
 r"""Wikipedia sentences with labeled salient spans."""
 import numpy as np
 import tensorflow as tf
