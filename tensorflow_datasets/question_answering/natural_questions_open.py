13a3eda2357db5c752cd8e8f5c4cf096bb666098
Yilei Yang <yileiyang@google.com>
2022-03-29 17:41:53 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 438170532

--- tensorflow_datasets/question_answering/natural_questions_open.py
+++ tensorflow_datasets/question_answering/natural_questions_open.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """Natural Questions Open: A Benchmark for Open Domain Question Answering."""
 
 import json
