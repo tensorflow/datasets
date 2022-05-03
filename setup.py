fa3e3218398b3ecf331b645dbc94efdd3c90d19f
The TensorFlow Datasets Authors <no-reply@google.com>
2022-05-02 17:28:55 -0700

    Add BEIR retrieval dataset to TFDS.
    PiperOrigin-RevId: 446070793

--- setup.py
+++ setup.py
@@ -122,6 +122,7 @@ DATASET_FILES = [
 DATASET_EXTRAS = {
     # In alphabetical order
     'aflw2k3d': ['scipy'],
+    'beir': ['apache_beam'],
     'ble_wind_field': ['gcsfs', 'zarr'],
     'c4': ['apache_beam', 'gcld3', 'langdetect', 'nltk', 'tldextract'],
     'cats_vs_dogs': ['matplotlib'],
