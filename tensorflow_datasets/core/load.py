04656ddba45ea3c5738996fc7f7acf13ef9402ba
Etienne Pot <epot@google.com>
2022-05-27 05:55:24 -0700

    More complete urls rename
    PiperOrigin-RevId: 451375451

--- tensorflow_datasets/core/load.py
+++ tensorflow_datasets/core/load.py
@@ -288,7 +288,7 @@ def load(
     decoders: Nested dict of `Decoder` objects which allow to customize the
       decoding. The structure should match the feature structure, but only
       customized feature keys need to be present. See [the
-      guide](https://github.com/tensorflow/datasets/tree/master/docs/decode.md)
+      guide](https://github.com/tensorflow/datasets/blob/master/docs/decode.md)
       for more info.
     read_config: `tfds.ReadConfig`, Additional options to configure the input
       pipeline (e.g. seed, num parallel reads,...).
