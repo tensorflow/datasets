1b6c8dae7d2371c72a112ebce2195a36d70f2b5d
The TensorFlow Datasets Authors <no-reply@google.com>
2022-05-27 07:58:09 -0700

    Add FLEURS arxiv link to XTREME-S TFDS config.
    PiperOrigin-RevId: 451396136

--- tensorflow_datasets/audio/xtreme_s/xtreme_s.py
+++ tensorflow_datasets/audio/xtreme_s/xtreme_s.py
@@ -189,11 +189,19 @@ _DESCRIPTIONS = {
 }
 
 _CITATIONS = {
-    "fleurs": "",
+    "fleurs":
+        """\
+@article{fleurs2022arxiv,
+  title = {FLEURS: Few-shot Learning Evaluation of Universal Representations of Speech},
+  author = {Conneau, Alexis and Ma, Min and Khanuja, Simran and Zhang, Yu and Axelrod, Vera and Dalmia, Siddharth and Riesa, Jason and Rivera, Clara and Bapna, Ankur},
+  journal={arXiv preprint arXiv:2205.12446},
+  url = {https://arxiv.org/abs/2205.12446},
+  year = {2022},
+}""",
 }
 
 _HOMEPAGE_URLS = {
-    "fleurs": "",
+    "fleurs": "https://arxiv.org/abs/2205.12446",
 }
 
 _DATA_URLS = {
