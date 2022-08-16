dbff8df66a5288ac2f572a21dd10b09a8027e634
Guillermo Ortiz-Jimenez <gortizjimenez@google.com>
2022-08-16 07:24:12 -0700

    Fix correspondence between annotations and images in CIFAR100-N.
    PiperOrigin-RevId: 467919423

--- tensorflow_datasets/image_classification/cifar100_n/cifar100_n.py
+++ tensorflow_datasets/image_classification/cifar100_n/cifar100_n.py
@@ -49,8 +49,8 @@ class Cifar100N(tfds.core.GeneratorBasedBuilder):
   """DatasetBuilder for CIFAR-10N dataset."""
 
   MANUAL_DOWNLOAD_INSTRUCTIONS = """
-  Download 'side_info_cifar100N.csv' and 'CIFAR-100_human_ordered.npy' from 
-  https://github.com/UCSC-REAL/cifar-10-100n.
+  Download 'side_info_cifar100N.csv', 'CIFAR-100_human_ordered.npy' and
+  'image_order_c100.npy' from https://github.com/UCSC-REAL/cifar-10-100n.
   
   Then convert 'CIFAR-100_human_ordered.npy' into a CSV file 
   'CIFAR-100_human_annotations.csv'. This can be done with the following code:
@@ -69,13 +69,14 @@ class Cifar100N(tfds.core.GeneratorBasedBuilder):
   df = pd.DataFrame(human_annotations[()])
 
   with tf.io.gfile.GFile(human_labels_csv_path, "w") as f:
-    df.to_csv(f)
+    df.to_csv(f, index=False)
   ```
   """
 
-  VERSION = tfds.core.Version('1.0.0')
+  VERSION = tfds.core.Version('1.0.1')
   RELEASE_NOTES = {
       '1.0.0': 'Initial release.',
+      '1.0.1': 'Fixed correspondence between annotations and images.',
   }
 
   def _info(self) -> tfds.core.DatasetInfo:
@@ -108,7 +109,8 @@ class Cifar100N(tfds.core.GeneratorBasedBuilder):
         label_files=['coarse_label_names.txt', 'fine_label_names.txt'],
         label_keys=['coarse_label', 'label'],
         human_label_path='CIFAR-100_human_annotations.csv',
-        side_info_path='side_info_cifar100N.csv')
+        side_info_path='side_info_cifar100N.csv',
+        annotations_order_path='image_order_c100.npy')
 
   def _split_generators(self, dl_manager):
     """Returns SplitGenerators."""
@@ -118,8 +120,12 @@ class Cifar100N(tfds.core.GeneratorBasedBuilder):
     cifar_path = os.path.join(cifar_path, cifar_info.prefix)
 
     dl_paths = {
-        'human_labels': dl_manager.manual_dir / cifar_info.human_label_path,
-        'side_info': dl_manager.manual_dir / cifar_info.side_info_path
+        'human_labels':
+            dl_manager.manual_dir / cifar_info.human_label_path,
+        'side_info':
+            dl_manager.manual_dir / cifar_info.side_info_path,
+        'annotations_order':
+            dl_manager.manual_dir / cifar_info.annotations_order_path,
     }
 
     # Load the label names
@@ -170,6 +176,8 @@ class Cifar100N(tfds.core.GeneratorBasedBuilder):
       worker_id = side_info['worker_id']
       worker_time = side_info['worker_time']
 
+      annotations_order = _load_annotations_order(dl_paths['annotations_order'])
+
     for path in filepaths:
       for labels, np_image in _load_data(path, len(label_keys)):
         record = dict(zip(label_keys, labels))
@@ -180,11 +188,15 @@ class Cifar100N(tfds.core.GeneratorBasedBuilder):
         record['image'] = np_image
 
         if 'train' in split_prefix:
-          record['noise_label'] = noise_label[index]
+          # Note: The human labels are provided according to default shuffling
+          # of 'cifar100'. We need to invert the shuffling to assign the right
+          # human label to each example.
+          annotations_index = np.where(annotations_order == index)[0][0]
 
+          record['noise_label'] = noise_label[annotations_index]
           # Worker metadata is shared every 5 samples
-          record['worker_id'] = worker_id[index % 5]
-          record['worker_time'] = worker_time[index % 5]
+          record['worker_id'] = worker_id[index // 5]
+          record['worker_time'] = worker_time[index // 5]
         else:
           # There is no annotator metadata for test split
           record['noise_label'] = -1
@@ -198,7 +210,8 @@ class Cifar100N(tfds.core.GeneratorBasedBuilder):
 class CifarInfo(
     collections.namedtuple('_CifarInfo', [
         'name', 'url', 'prefix', 'train_files', 'test_files', 'label_files',
-        'label_keys', 'human_label_path', 'side_info_path'
+        'label_keys', 'human_label_path', 'side_info_path',
+        'annotations_order_path'
     ])):
   """Contains the information necessary to generate a CIFAR dataset.
 
@@ -211,8 +224,9 @@ class CifarInfo(
     test_files (list<str>): name of test files within `prefix`.
     label_files (list<str>): names of the label files in the data.
     label_keys (list<str>): names of the label keys in the data.
-    human_label_path (str): Pat to human annotations
-    side_info_path (str): Path to metadata about annotations
+    human_label_path (str): path to human annotations.
+    side_info_path (str): path to metadata about annotations.
+    annotations_order_path (str): path to annotation-image order correspondence.
   """
 
 
@@ -235,6 +249,13 @@ def _load_data(path, labels_number=1):
     yield labels, img
 
 
+def _load_annotations_order(path):
+  """Loads index mapping between the annotation files and the CIFAR100 binaries."""
+  with tf.io.gfile.GFile(path, 'rb') as f:
+    annotations_order = np.load(f)
+  return annotations_order
+
+
 def _load_side_info(path):
   """Loads information from side_info_cifar100N.csv."""
   side_info_key_map = {
@@ -254,5 +275,5 @@ def _load_human_labels(path):
   with tf.io.gfile.GFile(path, 'r') as f:
     human_labels_array = np.genfromtxt(f, delimiter=',', skip_header=1)
   human_labels = {}
-  human_labels['noise_label'] = human_labels_array[:, 2]
+  human_labels['noise_label'] = human_labels_array[:, 1]
   return human_labels
