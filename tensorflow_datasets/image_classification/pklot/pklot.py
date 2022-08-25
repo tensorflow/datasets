d0ddfe9664c4f2b3f9e911750b9de46d33d96be0
Iyad Ahmed <iyadahmed430@gmail.com>
2022-08-25 09:06:55 +0200

    PKLot: Fix duplicated key error

--- tensorflow_datasets/image_classification/pklot/pklot.py
+++ tensorflow_datasets/image_classification/pklot/pklot.py
@@ -66,24 +66,20 @@ class Pklot(tfds.core.GeneratorBasedBuilder):
     """Returns SplitGenerators."""
     path = dl_manager.download_and_extract(
         "http://www.inf.ufpr.br/vri/databases/PKLot.tar.gz"
-    )
+    ) / "PKLot" / "PKLotSegmented"
     return {
         "train":
             chain(
-                self._generate_examples(
-                    path / "PKLot" / "PKLotSegmented" / "UFPR04"
-                ),
-                self._generate_examples(
-                    path / "PKLot" / "PKLotSegmented" / "UFPR05"
-                ),
+                self._generate_examples(path, "UFPR04"),
+                self._generate_examples(path, "UFPR05"),
             ),
         "test":
-            self._generate_examples(path / "PKLot" / "PKLotSegmented" / "PUC"),
+            self._generate_examples(path, "PUC"),
     }
 
-  def _generate_examples(self, path: Path):
+  def _generate_examples(self, path: Path, subdir: str):
     """Yields examples."""
-    for weather_dir_entry in os.scandir(path):
+    for weather_dir_entry in os.scandir(path / subdir):
       if not weather_dir_entry.is_dir():
         continue
 
@@ -91,14 +87,14 @@ class Pklot(tfds.core.GeneratorBasedBuilder):
         if not date_dir_entry.is_dir():
           continue
 
-        prefix = weather_dir_entry.name + "_" + date_dir_entry.name + "_"
+        key_prefix = subdir + "_" + weather_dir_entry.name + "_" + date_dir_entry.name + "_"
 
         for label_dir_entry in os.scandir(date_dir_entry):
           if not label_dir_entry.is_dir():
             continue
 
           for jpg_file_entry in os.scandir(label_dir_entry):
-            yield prefix + jpg_file_entry.name, {
+            yield key_prefix + label_dir_entry.name + "_" + jpg_file_entry.name, {
                 "image": jpg_file_entry.path,
                 "label": _LABEL_DIR_NAME_TO_LABEL_MAP[label_dir_entry.name]
             }
