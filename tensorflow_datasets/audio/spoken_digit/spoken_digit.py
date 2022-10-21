577e7500559864a137d76edc5e1bba6ca85920bf
Pierre Ruyssen <pierrot@google.com>
2022-10-21 14:40:59 -0700

    Move spoken_digit to 'datasets/' directory, using ConfigBasedBuilder.
    PiperOrigin-RevId: 482891964

--- tensorflow_datasets/audio/spoken_digit/spoken_digit.py
+++ tensorflow_datasets/audio/spoken_digit/spoken_digit.py
@@ -13,87 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Spoken Digit Dataset."""
+"""Dataset definition for spoken_digit.
 
-import os
-
-from tensorflow_datasets.core.utils.lazy_imports_utils import tensorflow as tf
-import tensorflow_datasets.public_api as tfds
-
-_CITATION = """
-@ONLINE {Free Spoken Digit Dataset,
-    author = "Zohar Jackson",
-    title  = "Spoken_Digit",
-    year   = "2016",
-    url    = "https://github.com/Jakobovski/free-spoken-digit-dataset"
-}
-"""
-_DESCRIPTION = """
-A free audio dataset of spoken digits. Think MNIST for audio.
-
-A simple audio/speech dataset consisting of recordings of spoken digits in wav files at 8kHz.
-The recordings are trimmed so that they have near minimal silence at the beginnings and ends.
-
-5 speakers
-2,500 recordings (50 of each digit per speaker)
-English pronunciations
-
-Files are named in the following format: {digitLabel}_{speakerName}_{index}.wav
+DEPRECATED!
+If you want to use the SpokenDigit dataset builder class, use:
+tfds.builder_cls('spoken_digit')
 """
 
-_DOWNLOAD_URL = "https://github.com/Jakobovski/free-spoken-digit-dataset/archive/v1.0.9.tar.gz"
-_HOMEPAGE_URL = "https://github.com/Jakobovski/free-spoken-digit-dataset"
+from tensorflow_datasets.core import lazy_builder_import
 
-
-class SpokenDigit(tfds.core.GeneratorBasedBuilder):
-  """Spoken Digit Dataset."""
-
-  VERSION = tfds.core.Version("1.0.9")
-
-  def _info(self):
-    return tfds.core.DatasetInfo(
-        builder=self,
-        description=_DESCRIPTION,
-        features=tfds.features.FeaturesDict({
-            "audio": tfds.features.Audio(file_format="wav", sample_rate=8000),
-            "label": tfds.features.ClassLabel(num_classes=10),
-            "audio/filename": tfds.features.Text()
-        }),
-        supervised_keys=("audio", "label"),
-        homepage=_HOMEPAGE_URL,
-        citation=_CITATION,
-    )
-
-  def _split_generators(self, dl_manager):
-    """Returns Split Generators."""
-    dl_path = dl_manager.download_and_extract(_DOWNLOAD_URL)
-    extracted_dir_path = os.path.join(dl_path,
-                                      "free-spoken-digit-dataset-1.0.9")
-    path = os.path.join(extracted_dir_path, "recordings")
-    # There is no predefined train/val/test split for this dataset.
-    return [
-        tfds.core.SplitGenerator(
-            name=tfds.Split.TRAIN, gen_kwargs={"path": path})
-    ]
-
-  def _generate_examples(self, path):
-    """Yields examples.
-
-    Args:
-       path: Path of the directory that contains audio files
-
-    Yields:
-       Next examples
-    """
-    for root, _, file_name in tf.io.gfile.walk(path):
-      for fname in file_name:
-        if fname.endswith(".wav"):  # Select only .wav files
-          # Example of audio file name: 7_jackson_32.wav
-          label = fname.split(".")[0].split("_")[0]
-          key = fname.split(".")[0]
-          example = {
-              "audio": os.path.join(root, fname),
-              "label": label,
-              "audio/filename": fname,
-          }
-          yield key, example
+SpokenDigit = lazy_builder_import.LazyBuilderImport('spoken_digit')
