c3c27742325e1824e940ff840837213cddcef271
Pierre Ruyssen <pierrot@google.com>
2022-10-27 07:32:28 -0700

    Move answer_equivalence to 'datasets/' directory, using ConfigBasedBuilder.
    PiperOrigin-RevId: 484247574

--- tensorflow_datasets/question_answering/answer_equivalence/answer_equivalence.py
+++ tensorflow_datasets/question_answering/answer_equivalence/answer_equivalence.py
@@ -13,108 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""answer_equivalence dataset."""
+"""Dataset definition for answer_equivalence.
 
-from __future__ import annotations
-
-import json
-import os
-
-from etils import epath
-from tensorflow_datasets.core.utils.lazy_imports_utils import tensorflow as tf
-import tensorflow_datasets.public_api as tfds
-
-_DESCRIPTION = """
-The Answer Equivalence Dataset contains human ratings on model predictions from
-several models on the SQuAD dataset. The ratings establish whether the predicted
-answer is 'equivalent' to the gold answer (taking into account both question and
-context).
-
-More specifically, by 'equivalent' we mean that the predicted answer contains at
-least the same information as the gold answer and does not add superfluous
-information. The dataset contains annotations for:
-  * predictions from BiDAF on SQuAD dev
-  * predictions from XLNet on SQuAD dev
-  * predictions from Luke on SQuAD dev
-  * predictions from Albert on SQuAD training, dev and test examples
+DEPRECATED!
+If you want to use the AnswerEquivalence dataset builder class, use:
+tfds.builder_cls('answer_equivalence')
 """
 
-_CITATION = """
-@article{bulian-etal-2022-tomayto,
-      title={Tomayto, Tomahto. Beyond Token-level Answer Equivalence for Question Answering Evaluation},
-      author={Jannis Bulian and Christian Buck and Wojciech Gajewski and Benjamin Boerschinger and Tal Schuster},
-      year={2022},
-      eprint={2202.07654},
-      archivePrefix={arXiv},
-      primaryClass={cs.CL}
-}
-"""
+from tensorflow_datasets.core import lazy_builder_import
 
-
-class AnswerEquivalence(tfds.core.GeneratorBasedBuilder):
-  """DatasetBuilder for answer_equivalence dataset."""
-
-  VERSION = tfds.core.Version('1.0.0')
-  RELEASE_NOTES = {
-      '1.0.0': 'Initial release.',
-  }
-
-  def _info(self) -> tfds.core.DatasetInfo:
-    """Returns the dataset metadata."""
-    return tfds.core.DatasetInfo(
-        builder=self,
-        description=_DESCRIPTION,
-        features=tfds.features.FeaturesDict({
-            'qid': tfds.features.Text(),
-            'gold_index': tf.int32,
-            'context': tfds.features.Text(),
-            'question': tfds.features.Text(),
-            'reference': tfds.features.Text(),
-            'candidate': tfds.features.Text(),
-            'score': tf.float32,
-            'question_1': tfds.features.ClassLabel(names=['no', 'yes', 'null']),
-            'question_2': tfds.features.ClassLabel(names=['no', 'yes', 'null']),
-            'question_3': tfds.features.ClassLabel(names=['no', 'yes', 'null']),
-            'question_4': tfds.features.ClassLabel(names=['no', 'yes', 'null']),
-        }),
-        supervised_keys=None,
-        homepage='https://github.com/google-research-datasets/answer-equivalence-dataset',
-        citation=_CITATION,
-    )
-
-  def _split_generators(self, dl_manager: tfds.download.DownloadManager):
-    """Returns SplitGenerators."""
-    homepage = 'https://raw.githubusercontent.com/google-research-datasets/answer-equivalence-dataset/main/v1/'
-    archive = {
-        'train': os.path.join(homepage, 'train.jsonl'),
-        'ae_dev': os.path.join(homepage, 'ae_dev.jsonl'),
-        'ae_test': os.path.join(homepage, 'ae_test.jsonl'),
-        'dev_xlnet': os.path.join(homepage, 'dev_by_system/dev_xlnet.jsonl'),
-        'dev_luke': os.path.join(homepage, 'dev_by_system/dev_luke.jsonl'),
-        'dev_bidaf': os.path.join(homepage, 'dev_by_system/dev_bidaf.jsonl')
-    }
-
-    paths = dl_manager.download_and_extract(archive)
-
-    return {
-        split: self._generate_examples(path) for split, path in paths.items()
-    }
-
-  def _generate_examples(self, filepath):
-    """Yields examples."""
-    with epath.Path(filepath).open() as fin:
-      for i, line in enumerate(fin):
-        data = json.loads(line)
-        sample_id = data['qid'] + f'_{i}'
-        sample = {
-            'qid': data['qid'],
-            'gold_index': data['gold_index'],
-            'context': data['context'],
-            'question': data['question'],
-            'reference': data['reference'],
-            'candidate': data['candidate'],
-            'score': data['score'],
-        }
-        for j in range(4):
-          sample[f'question_{j+1}'] = data['raw_rating'][j]
-        yield sample_id, sample
+AnswerEquivalence = lazy_builder_import.LazyBuilderImport('answer_equivalence')
