b6384a2a65775cd2bc98085343eab4aee977bb49
Pierre Ruyssen <pierrot@google.com>
2022-10-27 03:58:27 -0700

    Move aeslc to 'datasets/' directory, using ConfigBasedBuilder.
    PiperOrigin-RevId: 484211797

--- tensorflow_datasets/summarization/aeslc.py
+++ tensorflow_datasets/summarization/aeslc.py
@@ -13,102 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Annotated Enron Subject Line Corpus Dataset."""
+"""Dataset definition for aeslc.
 
-import os
-
-from etils import epath
-from tensorflow_datasets.core.utils.lazy_imports_utils import tensorflow as tf
-import tensorflow_datasets.public_api as tfds
-
-_CITATION = """
-@misc{zhang2019email,
-    title={This Email Could Save Your Life: Introducing the Task of Email Subject Line Generation},
-    author={Rui Zhang and Joel Tetreault},
-    year={2019},
-    eprint={1906.03497},
-    archivePrefix={arXiv},
-    primaryClass={cs.CL}
-}
+DEPRECATED!
+If you want to use the Aeslc dataset builder class, use:
+tfds.builder_cls('aeslc')
 """
 
-_DESCRIPTION = """
-A collection of email messages of employees in the Enron Corporation.
+from tensorflow_datasets.core import lazy_builder_import
 
-There are two features:
-  - email_body: email body text.
-  - subject_line: email subject text.
-"""
-
-_URL = "https://github.com/ryanzhumich/AESLC/archive/master.zip"
-
-_DOCUMENT = "email_body"
-_SUMMARY = "subject_line"
-
-
-class Aeslc(tfds.core.GeneratorBasedBuilder):
-  """Annotated Enron Subject Line Corpus Dataset."""
-
-  VERSION = tfds.core.Version("1.0.0")
-
-  def _info(self):
-    return tfds.core.DatasetInfo(
-        builder=self,
-        description=_DESCRIPTION,
-        features=tfds.features.FeaturesDict({
-            _DOCUMENT: tfds.features.Text(),
-            _SUMMARY: tfds.features.Text()
-        }),
-        supervised_keys=(_DOCUMENT, _SUMMARY),
-        homepage="https://github.com/ryanzhumich/AESLC",
-        citation=_CITATION,
-    )
-
-  def _split_generators(self, dl_manager):
-    """Returns SplitGenerators."""
-    dl_path = dl_manager.download_and_extract(_URL)
-    input_path = os.path.join(dl_path, "AESLC-master", "enron_subject_line")
-    return [
-        tfds.core.SplitGenerator(
-            name=tfds.Split.TRAIN,
-            gen_kwargs={
-                "pattern": os.path.join(input_path, "train", "*.subject")
-            },
-        ),
-        tfds.core.SplitGenerator(
-            name=tfds.Split.VALIDATION,
-            gen_kwargs={
-                "pattern": os.path.join(input_path, "dev", "*.subject")
-            },
-        ),
-        tfds.core.SplitGenerator(
-            name=tfds.Split.TEST,
-            gen_kwargs={
-                "pattern": os.path.join(input_path, "test", "*.subject")
-            },
-        ),
-    ]
-
-  def _generate_examples(self, pattern=None):
-    """Yields examples."""
-    for filename in tf.io.gfile.glob(pattern):
-      email_body, subject_line = _parse_email_file(filename)
-      key = os.path.basename(filename).rstrip(".subject")
-      yield key, {_DOCUMENT: email_body, _SUMMARY: subject_line}
-
-
-def _parse_email_file(filename):
-  """Parse email file text for email body and subject."""
-  with epath.Path(filename).open() as f:
-    email_body = ""
-    for line in f:
-      if line == "\n":
-        break
-      email_body += line
-    line = next(f)
-    subject = ""
-    for line in f:
-      if line == "\n":
-        break
-      subject += line
-  return email_body, subject
+Aeslc = lazy_builder_import.LazyBuilderImport('aeslc')
