37e8568a77d222405cb76c046602155c033fb47e
Pierre Ruyssen <pierrot@google.com>
2022-10-28 14:36:01 -0700

    Move d4rl_adroit_hammer to 'datasets/' directory, using ConfigBasedBuilder.
    PiperOrigin-RevId: 484622684

--- tensorflow_datasets/d4rl/d4rl_adroit_door/d4rl_adroit_door.py
+++ tensorflow_datasets/d4rl/d4rl_adroit_door/d4rl_adroit_door.py
@@ -13,25 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""d4rl_adroit_door dataset."""
-from typing import Any
+"""Dataset definition for d4rl_adroit_door.
 
-from tensorflow_datasets.d4rl import dataset_builder
-import tensorflow_datasets.public_api as tfds
+DEPRECATED!
+If you want to use the D4rlAdroitDoor dataset builder class, use:
+tfds.builder_cls('d4rl_adroit_door')
+"""
 
+from tensorflow_datasets.core import lazy_builder_import
 
-class D4rlAdroitDoor(dataset_builder.D4RLDatasetBuilder):
-  """DatasetBuilder for d4rl_adroit_door dataset."""
-
-  VERSION = tfds.core.Version('1.1.0')
-  RELEASE_NOTES = {
-      '1.0.0': 'Initial release.',
-      '1.1.0': 'Added is_last.',
-  }
-
-  BUILDER_CONFIGS = dataset_builder.ADROIT_BUILDER_CONFIGS
-
-  def __init__(self, **kwargs: Any):
-    config = dataset_builder.DatasetConfig(
-        name='door', obs_len=39, action_len=28, qpos_len=30, qvel_len=30)
-    super().__init__(ds_config=config, **kwargs)
+D4rlAdroitDoor = lazy_builder_import.LazyBuilderImport('d4rl_adroit_door')
---
+++ tensorflow_datasets/datasets/d4rl_adroit_hammer/README.md
@@ -0,0 +1,5 @@
+D4RL is an open-source benchmark for offline reinforcement learning. It provides
+standardized environments and datasets for training and benchmarking algorithms.
+
+The datasets follow the [RLDS format](https://github.com/google-research/rlds)
+to represent steps and episodes.
