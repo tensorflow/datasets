440f722acf541beaa67dcaa3d87ef6e9225b89d6
Tom van der Weide <weide@google.com>
2022-11-04 07:24:29 -0700

    Add support for mapping a complete sequence and mapping each element in a sequence
    PiperOrigin-RevId: 486134792

--- tensorflow_datasets/core/transform.py
+++ tensorflow_datasets/core/transform.py
@@ -20,8 +20,9 @@ Note that this is an experimental new feature, so the API may change.
 
 from __future__ import annotations
 
+import dataclasses
 import functools
-from typing import Any, Callable, List, Optional
+from typing import Any, Callable, List, Optional, Union
 
 import numpy as np
 
@@ -34,7 +35,20 @@ ExampleTransformFn = Callable[[Example],
                               Example]  # TODO(weide) return Iterator[Example]?
 
 
+@dataclasses.dataclass()
+class FeatureName:
+  feature: str
+  parts: List[str] = dataclasses.field(default_factory=list)
+
+  def __post_init__(self):
+    if not self.feature:
+      raise ValueError("Feature name must not be empty!")
+    if not self.parts:
+      self.parts = self.feature.split("/")
+
+
 def remove_feature(feature_name: str) -> ExampleTransformFn:
+  """Removes a feature from examples."""
 
   def apply_on_example(example: Example) -> Example:
     del example[feature_name]
@@ -46,8 +60,43 @@ def remove_feature(feature_name: str) -> ExampleTransformFn:
 def _transform_example(
     example: Example,
     fn: Callable[[Any], Any],
+    input_feature: FeatureName,
+    output_feature: FeatureName,
+) -> Example:
+  """Transforms the specified input entry with `fn`.
+
+  Note that the given example data is transformed in place.
+
+  Arguments:
+    example: the example data that needs to be transformed.
+    fn: the function used to transform data.
+    input_feature: what feature in the example should be transformed.
+    output_feature: where the transformed data should be stored in the example.
+
+  Returns:
+    the transformed example.
+  """
+  # Find the data to give to `fn` inside the possibly nested example.
+  input_data = example
+  for part in input_feature.parts:
+    input_data = input_data[part]
+
+  # Find where the transformed data needs to be stored.
+  destination = example
+  for part in output_feature.parts[:-1]:
+    destination = destination[part]
+
+  # Store the transformed data.
+  destination[output_feature.parts[-1]] = fn(input_data)
+  return example
+
+
+def _transform_all_elements(
+    example: Example,
+    fn: Callable[[Any], Any],
     in_parts: List[str],
     out_parts: List[str],
+    levels: int,
 ) -> Example:
   """Transforms the specified input entry with `fn`.
 
@@ -63,40 +112,102 @@ def _transform_example(
       that this needs to be at the same level as `in_parts`. For example, if
       `in_parts=["a", "b", "c"]`, then the output feature needs to be inside
       `["a", "b']`, e.g. `out_parts=["a", "b", "new_feature"]`.
+    levels: how many levels deep each element should be mapped. If 0, then `fn`
+      will be applied on the whole feature and not on each element in it.
 
   Returns:
     the transformed example.
   """
   if not in_parts:
     raise ValueError("Should be called with non-empty `in_parts`!")
-  in_key = in_parts[0]
+  if not out_parts:
+    raise ValueError("Should be called with non-empty `out_parts`!")
   out_key = out_parts[0]
-  if len(in_parts) > 1:
+  input_data = example[in_parts[0]]
+  if levels == 0:
+    example[out_key] = fn(input_data)
+  elif len(in_parts) > 1:
     in_rest = in_parts[1:]
     out_rest = out_parts[1:]
-    if (isinstance(example[in_key], np.ndarray) or
-        isinstance(example[in_key], list)):
-      example[out_key] = [
-          _transform_example(
-              example=ex, fn=fn, in_parts=in_rest, out_parts=out_rest)
-          for ex in example[in_key]
-      ]
+    if isinstance(input_data, np.ndarray):
+      input_data = input_data.tolist()
+    if isinstance(input_data, list):
+      example[out_key] = []
+      for ex in input_data:
+        example[out_key].append(
+            _transform_all_elements(
+                example=ex,
+                fn=fn,
+                in_parts=in_rest,
+                out_parts=out_rest,
+                levels=levels - 1))
     else:
-      example[out_key] = _transform_example(
-          example=example[in_key], fn=fn, in_parts=in_rest, out_parts=out_rest)
+      example[out_key] = _transform_all_elements(
+          example=input_data,
+          fn=fn,
+          in_parts=in_rest,
+          out_parts=out_rest,
+          levels=levels - 1)
   elif len(in_parts) == 1:
-    input_data = example[in_key]
-    if isinstance(input_data, List):
+    if isinstance(input_data, np.ndarray):
+      example[out_key] = np.vectorize(fn)(input_data)
+    elif isinstance(input_data, List):
       example[out_key] = [fn(x) for x in input_data]
     else:
-      example[out_key] = fn(example[in_key])
+      example[out_key] = fn(input_data)
   return example
+
+
+def map_each_fn(
+    fn: Callable[[Any], Any],
+    input_feature: Union[str, FeatureName],
+    output_feature: Union[None, str, FeatureName] = None,
+    levels: Optional[int] = None,
+) -> ExampleTransformFn:
+  """Applies `fn` to each element in the feature.
+
+  If `input_feature` is a sequence, then `fn` will be applied to each element in
+  the sequence. Similarly, if `input_feature` is a sequence nested in another
+  sequence, then `fn` will be applied to each element. For example, if
+  `input_feature='a/b'` and both `a` and `b` are sequences, then `fn` is applied
+  to each element in `a` and then to each element in `b`.
+
+  Arguments:
+    fn: the function to apply.
+    input_feature: the name of the feature on which `fn` should be applied. If a
+      nested feature needs to be transformed, then specify it using slashes. For
+      example, to transform feature `b` inside `a`, use `a/b`.
+    output_feature: the name of the feature where the result should be stored.
+      If `None`, then `input_feature` will be overridden.
+    levels: how many levels deep each element should be mapped. For example, if
+      feature `a` is a sequence with an image `img` in it, and you invoke
+      `map_each_fn` with `input_feature="a/img"` and `levels=1`, then the `fn`
+      will be applied on each image and not on each pixel.
+
+  Returns:
+    a function that applies `fn` to `input_feature` field of examples and stores
+    the result in `output_feature`.
+  """
+  if isinstance(input_feature, str):
+    input_feature = FeatureName(feature=input_feature)
+  if output_feature is None:
+    output_feature = input_feature
+  elif isinstance(output_feature, str):
+    output_feature = FeatureName(feature=output_feature)
+  levels = levels or len(input_feature.parts)
+
+  return functools.partial(
+      _transform_all_elements,
+      fn=fn,
+      in_parts=input_feature.parts,
+      out_parts=output_feature.parts,
+      levels=levels)
 
 
 def apply_fn(
     fn: Callable[[Any], Any],
-    input_feature: str,
-    output_feature: Optional[str] = None,
+    input_feature: Union[str, FeatureName],
+    output_feature: Union[None, str, FeatureName] = None,
 ) -> ExampleTransformFn:
   """Returns a function that applies the given `fn` on the `input_feature`.
 
@@ -112,17 +223,30 @@ def apply_fn(
     a function that applies `fn` to `input_feature` field of examples and stores
     the result in `output_feature`.
   """
-  input_parts = input_feature.split("/")
-  if output_feature is not None:
-    output_parts = output_feature.split("/")
-  else:
-    output_parts = input_parts
-  if (len(input_parts) != len(output_parts) or
-      input_parts[:-1] != output_parts[:-1]):
-    raise ValueError("The out-feature must have the same ancestor as the "
-                     f"in-feature! Got in={input_parts}, out={output_parts}.")
+  if isinstance(input_feature, str):
+    input_feature = FeatureName(feature=input_feature)
+  if output_feature is None:
+    output_feature = input_feature
+  elif isinstance(output_feature, str):
+    output_feature = FeatureName(feature=output_feature)
+
+  if not input_feature.parts:
+    raise ValueError(f"No input feature was specified! Got {input_feature}")
+  if not output_feature.parts:
+    raise ValueError(f"No output feature was specified! Got {output_feature}")
+
+  if (len(output_feature.parts) > 1 and
+      (len(input_feature.parts) != len(input_feature.parts) or
+       input_feature.parts[:-1] != input_feature.parts[:-1])):
+    raise ValueError(
+        "If the out-feature is nested, then it must have the same ancestor as "
+        f"the in-feature! Got in={input_feature}, out={output_feature}.")
+
   return functools.partial(
-      _transform_example, fn=fn, in_parts=input_parts, out_parts=output_parts)
+      _transform_example,
+      fn=fn,
+      input_feature=input_feature,
+      output_feature=output_feature)
 
 
 def apply_transformations(
--- tensorflow_datasets/core/transform_test.py
+++ tensorflow_datasets/core/transform_test.py
@@ -14,55 +14,141 @@
 # limitations under the License.
 
 """Tests for transform."""
-import functools
+from typing import List
 
+import numpy as np
 import pytest
 
 from tensorflow_datasets.core import transform
 
 
-def add_number(number: int, increment: int) -> int:
-  return number + increment
+def add_two(number: int) -> int:
+  return number + 2
 
 
-def test_apply_fn_simple():
-  add_two = functools.partial(add_number, increment=2)
-  apply_fn = transform.apply_fn(
-      fn=add_two, input_feature="a", output_feature="c")
-  example = {"a": 1, "d": "left alone"}
-  expected = {"a": 1, "c": 3, "d": "left alone"}
-  assert apply_fn(example) == expected
+def compute_sum(numbers: List[int]) -> int:
+  return sum(numbers)
 
 
-def test_apply_fn_nested_dict():
-  add_two = functools.partial(add_number, increment=2)
-  apply_fn = transform.apply_fn(
-      fn=add_two, input_feature="a/b", output_feature="a/c")
-  example = {"a": {"b": 1}, "d": "something"}
-  expected = {"a": {"b": 1, "c": 3}, "d": "something"}
-  assert apply_fn(example) == expected
+class TestApplyFn:
+
+  def test_simple(self):
+    apply_fn = transform.apply_fn(
+        fn=add_two, input_feature="a", output_feature="c")
+    example = {"a": 1, "d": "left alone"}
+    expected = {"a": 1, "c": 3, "d": "left alone"}
+    assert apply_fn(example) == expected
+
+  def test_nested_dict(self):
+    apply_fn = transform.apply_fn(
+        fn=add_two, input_feature="a/b", output_feature="a/c")
+    example = {"a": {"b": 1}, "d": "something"}
+    expected = {"a": {"b": 1, "c": 3}, "d": "something"}
+    assert apply_fn(example) == expected
+
+  def test_nested_dict_different_output(self):
+    apply_fn = transform.apply_fn(
+        fn=add_two, input_feature="a/b", output_feature="c")
+    example = {"a": {"b": 1}, "d": "something"}
+    expected = {"a": {"b": 1}, "c": 3, "d": "something"}
+    assert apply_fn(example) == expected
+
+  def test_sequence_sum(self):
+    apply_fn = transform.apply_fn(
+        fn=compute_sum, input_feature="a", output_feature="b")
+    example = {"a": [1, 2, 3]}
+    expected = {"a": [1, 2, 3], "b": 6}
+    assert apply_fn(example) == expected
+
+  def test_nested_sequence_sum(self):
+    apply_fn = transform.apply_fn(
+        fn=compute_sum, input_feature="a/b", output_feature="c")
+    example = {"a": {"b": [1, 2, 3]}}
+    expected = {"a": {"b": [1, 2, 3]}, "c": 6}
+    assert apply_fn(example) == expected
+
+  def test_non_existing_feature(self):
+    with pytest.raises(KeyError):
+      apply_fn = transform.apply_fn(
+          fn=add_two, input_feature="a", output_feature="b")
+      example = {"x": 1}
+      apply_fn(example)
 
 
-def test_apply_fn_sequence():
-  add_two = functools.partial(add_number, increment=2)
-  apply_fn = transform.apply_fn(
-      fn=add_two, input_feature="a", output_feature="b")
-  example = {"a": [1, 2, 3]}
-  expected = {"a": [1, 2, 3], "b": [3, 4, 5]}
-  assert apply_fn(example) == expected
+class TestMapEachFn:
+
+  def test_sequence(self):
+    apply_fn = transform.map_each_fn(
+        fn=add_two, input_feature="a", output_feature="b")
+    example = {"a": [1, 2, 3]}
+    expected = {"a": [1, 2, 3], "b": [3, 4, 5]}
+    assert apply_fn(example) == expected
+
+  def test_sequence_numpy(self):
+    apply_fn = transform.map_each_fn(
+        fn=add_two, input_feature="a", output_feature="b")
+    example = {"a": np.array([1, 2, 3])}
+    expected = {"a": np.array([1, 2, 3]), "b": np.array([3, 4, 5])}
+    actual = apply_fn(example)
+    assert np.array_equal(actual["a"], expected["a"])
+    assert np.array_equal(actual["b"], expected["b"])
+
+  def test_sequence_numpy_multidim(self):
+    apply_fn = transform.map_each_fn(
+        fn=add_two, input_feature="a", output_feature="b")
+    md_array = np.array([[1, 2, 3], [4, 5, 6]], np.int32)
+    example = {"a": md_array}
+    expected = {"a": md_array, "b": np.array([[3, 4, 5], [6, 7, 8]], np.int32)}
+    actual = apply_fn(example)
+    assert np.array_equal(actual["a"], expected["a"])
+    assert np.array_equal(actual["b"], expected["b"])
+
+  def test_sequence_in_sequence(self):
+    apply_fn = transform.map_each_fn(fn=add_two, input_feature="a/b")
+    example = {"a": [{"b": [1, 2]}, {"b": [2, 3]}]}
+    expected = {"a": [{"b": [3, 4]}, {"b": [4, 5]}]}
+    assert apply_fn(example) == expected
+
+  def test_sequence_of_dicts(self):
+    apply_fn = transform.map_each_fn(
+        fn=add_two, input_feature="a/b", output_feature="a/c")
+    example = {"a": [{"b": [1, 2]}, {"b": [2, 3]}]}
+    expected = {"a": [{"b": [1, 2], "c": [3, 4]}, {"b": [2, 3], "c": [4, 5]}]}
+    assert apply_fn(example) == expected
+
+  def test_sequence_of_images(self):
+    apply_fn = transform.map_each_fn(
+        fn=np.transpose, input_feature="a/img", levels=1)
+    example = {
+        "a": [{
+            "img": np.array([[0, 1], [2, 3]])
+        }, {
+            "img": np.array([[1, 2], [3, 4]])
+        }]
+    }
+    expected = {
+        "a": [{
+            "img": np.array([[0, 2], [1, 3]])
+        }, {
+            "img": np.array([[1, 3], [2, 4]])
+        }]
+    }
+    actual = apply_fn(example)
+    assert np.array_equal(actual["a"][0]["img"], expected["a"][0]["img"])
+    assert np.array_equal(actual["a"][1]["img"], expected["a"][1]["img"])
 
 
-def test_apply_fn_sequence_of_dicts():
-  add_two = functools.partial(add_number, increment=2)
-  apply_fn = transform.apply_fn(
-      fn=add_two, input_feature="a/b", output_feature="a/c")
-  example = {"a": [{"b": [1, 2]}, {"b": [2, 3]}]}
-  expected = {"a": [{"b": [1, 2], "c": [3, 4]}, {"b": [2, 3], "c": [4, 5]}]}
-  assert apply_fn(example) == expected
+class TestApplyTransformations:
 
-
-def test_apply_fn_different_ancestor():
-  add_two = functools.partial(add_number, increment=2)
-  with pytest.raises(
-      ValueError, match="The out-feature must have the same ancestor"):
-    transform.apply_fn(fn=add_two, input_feature="a/b", output_feature="c/d")
+  def test_simple(self):
+    transformations = [
+        transform.apply_fn(fn=add_two, input_feature="a", output_feature="b"),
+        transform.apply_fn(fn=add_two, input_feature="b", output_feature="c"),
+        transform.apply_fn(fn=add_two, input_feature="c", output_feature="d"),
+        transform.remove_feature("a")
+    ]
+    key = 1
+    example = {"a": 1, "z": "z"}
+    actual = transform.apply_transformations(
+        key=key, example=example, transformations=transformations)
+    assert actual == (1, {"b": 3, "c": 5, "d": 7, "z": "z"})
