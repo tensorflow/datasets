7b16bd238f4fb9bbc539ab72f8e3459f3a940a85
The TensorFlow Datasets Authors <no-reply@google.com>
2022-11-25 07:32:18 -0800

    Change check to support simple jnp.dtype comparison
    PiperOrigin-RevId: 490907064

--- tensorflow_datasets/core/example_parser.py
+++ tensorflow_datasets/core/example_parser.py
@@ -108,7 +108,7 @@ def _deserialize_single_field(example_data,
     example_data = tf.reshape(example_data, shape)
 
   # Restore dtype
-  if not utils.is_same_tf_dtype(example_data.dtype, tensor_info.dtype):
+  if example_data.dtype != tensor_info.dtype:
     example_data = tf.dtypes.cast(example_data, tensor_info.dtype)
   return example_data
 
