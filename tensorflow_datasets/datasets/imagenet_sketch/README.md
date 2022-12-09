843c4b827cc64379128464c6e01b68d0aa4b0e4e
Pierre Ruyssen <pierrot@google.com>
2022-12-09 08:10:50 -0800

    Move imagenet_sketch to 'datasets/' directory, using ConfigBasedBuilder.
    PiperOrigin-RevId: 494176154

---
+++ tensorflow_datasets/datasets/imagenet_sketch/README.md
@@ -0,0 +1,5 @@
+ImageNet-Sketch consists of 50,889 black and white sketch images, 50 for each of
+the 1000 ImageNet classes. These images were originally collected from Google
+Image Search for "sketch of __". 100 images were collected and then manually
+filtered. For classes with fewer than 50 good images, additional images were
+constructed by flip or rotation.
--- tensorflow_datasets/image_classification/imagenet_sketch/imagenet_sketch.py
+++ tensorflow_datasets/image_classification/imagenet_sketch/imagenet_sketch.py
@@ -13,71 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""ImageNet-Sketch dataset."""
+"""Dataset definition for imagenet_sketch.
 
-from tensorflow_datasets.datasets.imagenet2012 import imagenet_common
-import tensorflow_datasets.public_api as tfds
-
-_DESCRIPTION = """\
-ImageNet-Sketch consists of 50,889 black and white sketch images, 50 for each of
-the 1000 ImageNet classes. These images were originally collected from Google
-Image Search for "sketch of __". 100 images were collected and then manually
-filtered. For classes with fewer than 50 good images, additional images were
-constructed by flip or rotation.
+DEPRECATED!
+If you want to use the ImagenetSketch dataset builder class, use:
+tfds.builder_cls('imagenet_sketch')
 """
 
-_CITATION = """\
-@inproceedings{wang2019learning,
-        title={Learning Robust Global Representations by Penalizing Local Predictive Power},
-        author={Wang, Haohan and Ge, Songwei and Lipton, Zachary and Xing, Eric P},
-        booktitle={Advances in Neural Information Processing Systems},
-        pages={10506--10518},
-        year={2019}
-}
-"""
+from tensorflow_datasets.core import lazy_builder_import
 
-_BASE_URL = 'https://github.com/HaohanWang/ImageNet-Sketch'
-_IMAGENET_SKETCH_URL = 'https://drive.google.com/u/0/uc?id=1Mj0i5HBthqH1p_yeXzsg22gZduvgoNeA&export=download'
-
-
-class ImagenetSketch(tfds.core.GeneratorBasedBuilder):
-  """DatasetBuilder for imagenet_sketch dataset."""
-
-  VERSION = tfds.core.Version('1.0.0')
-  RELEASE_NOTES = {
-      '1.0.0': 'Initial release.',
-  }
-
-  def _info(self) -> tfds.core.DatasetInfo:
-    """Returns the dataset metadata."""
-    imagenet_names_file = imagenet_common.label_names_file()
-    return tfds.core.DatasetInfo(
-        builder=self,
-        description=(_DESCRIPTION),
-        features=tfds.features.FeaturesDict({
-            'image': tfds.features.Image(),
-            'label': tfds.features.ClassLabel(names_file=imagenet_names_file),
-            'file_name': tfds.features.Text(),
-        }),
-        supervised_keys=('image', 'label'),
-        homepage=_BASE_URL,
-        citation=_CITATION)
-
-  def _split_generators(self, dl_manager: tfds.download.DownloadManager):
-    """Returns SplitGenerators."""
-    path = dl_manager.download(_IMAGENET_SKETCH_URL)
-    return [
-        tfds.core.SplitGenerator(
-            name=tfds.Split.TEST,
-            gen_kwargs={
-                'archive': dl_manager.iter_archive(path),
-            },
-        ),
-    ]
-
-  def _generate_examples(self, archive):
-    """Generate images and labels for splits."""
-    for fname, fobj in archive:
-      assert fname.startswith('sketch/') and fname.endswith('.JPEG')
-      fname = fname[len('sketch/'):]
-      yield fname, {'image': fobj, 'file_name': fname, 'label': fname[:9]}
+ImagenetSketch = lazy_builder_import.LazyBuilderImport('imagenet_sketch')
