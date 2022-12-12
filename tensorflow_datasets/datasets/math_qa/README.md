949930c2269683016b5ca3cbcd3b5125b29d28da
Pierre Ruyssen <pierrot@google.com>
2022-12-12 15:44:41 -0800

    Move math_qa to 'datasets/' directory, using ConfigBasedBuilder.
    PiperOrigin-RevId: 494849856

---
+++ tensorflow_datasets/datasets/math_qa/README.md
@@ -0,0 +1,2 @@
+A large-scale dataset of math word problems and an interpretable neural math
+problem solver that learns to map problems to operation programs.
--- tensorflow_datasets/text/math_qa/math_qa.py
+++ tensorflow_datasets/text/math_qa/math_qa.py
@@ -13,97 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""MathQA dataset."""
+"""Dataset definition for math_qa.
 
-import json
-import os
-import re
-
-from etils import epath
-import tensorflow_datasets.public_api as tfds
-
-_DESCRIPTION = """
-A large-scale dataset of math word problems and an interpretable neural math problem solver that learns to map problems to operation programs.
+DEPRECATED!
+If you want to use the MathQa dataset builder class, use:
+tfds.builder_cls('math_qa')
 """
 
-_CITATION = """
-@misc{amini2019mathqa,
-      title={MathQA: Towards Interpretable Math Word Problem Solving with Operation-Based Formalisms},
-      author={Aida Amini and Saadia Gabriel and Peter Lin and Rik Koncel-Kedziorski and Yejin Choi and Hannaneh Hajishirzi},
-      year={2019},
-      eprint={1905.13319},
-      archivePrefix={arXiv},
-      primaryClass={cs.CL}
-}
-"""
+from tensorflow_datasets.core import lazy_builder_import
 
-_URL = 'https://math-qa.github.io/data/MathQA.zip'
-
-_FEATURES = [
-    'Problem', 'Rationale', 'options', 'correct', 'annotated_formula',
-    'linear_formula', 'category', 'correct_option'
-]
-
-
-class MathQa(tfds.core.GeneratorBasedBuilder):
-  """DatasetBuilder for math_qa dataset."""
-
-  VERSION = tfds.core.Version('1.0.0')
-  RELEASE_NOTES = {
-      '1.0.0': 'Initial release.',
-  }
-
-  def _info(self) -> tfds.core.DatasetInfo:
-    """Returns the dataset metadata."""
-    return tfds.core.DatasetInfo(
-        builder=self,
-        description=_DESCRIPTION,
-        features=tfds.features.FeaturesDict(
-            {k: tfds.features.Text() for k in _FEATURES}),
-        supervised_keys=None,
-        homepage='https://math-qa.github.io/',
-        citation=_CITATION,
-    )
-
-  def _split_generators(self, dl_manager: tfds.download.DownloadManager):
-    """Returns SplitGenerators."""
-    path = dl_manager.download_and_extract(_URL)
-    return {
-        tfds.Split.TRAIN:
-            self._generate_examples(os.path.join(path, 'train.json')),
-        tfds.Split.VALIDATION:
-            self._generate_examples(os.path.join(path, 'dev.json')),
-        tfds.Split.TEST:
-            self._generate_examples(os.path.join(path, 'test.json')),
-    }
-
-  def _generate_examples(self, filename: str):
-    """Yields examples."""
-    with epath.Path(filename).open() as f:
-      for i, ex in enumerate(json.load(f)):
-        ex['correct_option'] = extract_answer_text(ex['options'], ex['correct'])
-        yield i, ex
-
-
-def extract_answer_text(options_text: str, answer_tag: str):
-  """Extracts correct answer's text from all options.
-
-  Args:
-    options_text: all options as text in various format.
-    answer_tag: correct answers tag a, b, c, ...
-
-  Returns:
-    parsed option text corresponding to the correct answer.
-  """
-  if options_text.startswith('[') and options_text.endswith(']'):
-    options = eval(options_text)  # pylint: disable = eval-used
-    options = [re.sub('[abcde] \\)', '', x).strip() for x in options]
-  else:
-    options = re.split('[abcde] \\)', options_text)
-    if options[0]:
-      raise ValueError(f'Expects first segment to be empty in {options}.')
-    options = [x.strip().rstrip(',').strip() for x in options[1:]]
-  correct_id = ord(answer_tag) - ord('a')
-  if correct_id >= len(options):
-    raise ValueError(f'Ill parsed dictionary {options} from {options_text}.')
-  return options[correct_id]
+MathQa = lazy_builder_import.LazyBuilderImport('math_qa')
