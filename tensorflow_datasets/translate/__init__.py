94882dbca36bcdb8150b3d3a9baa0c12f147faf2
Pierre Ruyssen <pierrot@google.com>
2022-12-14 05:55:55 -0800

    Move para_crawl to 'datasets/' directory, using ConfigBasedBuilder.
    PiperOrigin-RevId: 495291725

--- tensorflow_datasets/translate/__init__.py
+++ tensorflow_datasets/translate/__init__.py
@@ -21,7 +21,6 @@ from tensorflow_datasets.translate.flores import FloresConfig
 from tensorflow_datasets.translate.mtnt import Mtnt
 from tensorflow_datasets.translate.opus import Opus
 from tensorflow_datasets.translate.para_crawl import ParaCrawl
-from tensorflow_datasets.translate.para_crawl import ParaCrawlConfig
 from tensorflow_datasets.translate.tatoeba import Tatoeba
 from tensorflow_datasets.translate.ted_hrlr import TedHrlrTranslate
 from tensorflow_datasets.translate.ted_multi import TedMultiTranslate
