6a564123cd8b27e2da8b2c5fc5b200fc7474d931
Pierre Ruyssen <pierrot@google.com>
2022-12-19 03:26:31 -0800

    Move quac to 'datasets/' directory, using ConfigBasedBuilder.
    PiperOrigin-RevId: 496357302

--- tensorflow_datasets/text/quac/quac.py
+++ tensorflow_datasets/text/quac/quac.py
@@ -13,115 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""quac dataset."""
+"""Dataset definition for quac.
 
-from __future__ import annotations
-
-import json
-
-from etils import epath
-import numpy as np
-import tensorflow_datasets.public_api as tfds
-
-_CITATION = """
-@article{choi2018quac,
-  title={Quac: Question answering in context},
-  author={Choi, Eunsol and He, He and Iyyer, Mohit and Yatskar, Mark and Yih, Wen-tau and Choi, Yejin and Liang, Percy and Zettlemoyer, Luke},
-  journal={arXiv preprint arXiv:1808.07036},
-  year={2018}
-}
+DEPRECATED!
+If you want to use the Quac dataset builder class, use:
+tfds.builder_cls('quac')
 """
 
-_DATA_URL = "https://s3.amazonaws.com/my89public/quac/"
+from tensorflow_datasets.core import lazy_builder_import
 
-_MODULE = "QuAC"
-
-_DESCRIPTION = """
-Question Answering in Context is a dataset for modeling, understanding,
-and participating in information seeking dialog. Data instances consist
-of an interactive dialog between two crowd workers: (1) a student who poses
-a sequence of freeform questions to learn as much as possible about a hidden
-Wikipedia text, and (2) a teacher who answers the questions by providing
-short excerpts (spans) from the text. QuAC introduces challenges not found
-in existing machine comprehension datasets: its questions are often more
-open-ended, unanswerable, or only meaningful within the dialog context."""
-
-
-class Quac(tfds.core.GeneratorBasedBuilder):
-  """DatasetBuilder for quac dataset."""
-
-  VERSION = tfds.core.Version("1.0.0")
-  RELEASE_NOTES = {
-      "1.0.0": "Initial release.",
-  }
-
-  def _info(self):
-    return tfds.core.DatasetInfo(
-        builder=self,
-        description=_DESCRIPTION,
-        features=tfds.features.FeaturesDict({
-            "context":
-                tfds.features.Text(),
-            "followup":
-                tfds.features.Text(),
-            "yesno":
-                tfds.features.Text(),
-            "question":
-                tfds.features.Text(),
-            "answers":
-                tfds.features.Sequence({
-                    "text": tfds.features.Text(),
-                    "answer_start": np.int32,
-                }),
-            "orig_answer":
-                tfds.features.FeaturesDict({
-                    "text": tfds.features.Text(),
-                    "answer_start": np.int32,
-                }),
-            "section_title":
-                tfds.features.Text(),
-            "background":
-                tfds.features.Text(),
-            "title":
-                tfds.features.Text()
-        }),
-        homepage="https://quac.ai/",
-        supervised_keys=("context", "answers"),
-        citation=_CITATION,
-    )
-
-  def _split_generators(self, dl_manager):
-    """Returns SplitGenerator."""
-
-    expected_paths = dl_manager.download_and_extract({
-        "train": _DATA_URL + "train_v0.2.json",
-        "val": _DATA_URL + "val_v0.2.json"
-    })
-
-    return {
-        "train": self._generate_examples(expected_paths["train"]),
-        "validation": self._generate_examples(expected_paths["val"])
-    }
-
-  def _generate_examples(self, filepath):
-    """Yields examples based on filepath."""
-    with epath.Path(filepath).open() as f:
-      row = json.loads(f.readline())
-      for data in row["data"]:
-        section_title = data["section_title"]
-        background = data["background"]
-        tilte = data["title"]
-        for paragraph in data["paragraphs"]:
-          context = paragraph["context"]
-          for qa in paragraph["qas"]:
-            yield qa["id"], {
-                "context": context,
-                "followup": qa["followup"],
-                "yesno": qa["yesno"],
-                "question": qa["question"],
-                "answers": qa["answers"],
-                "orig_answer": qa["orig_answer"],
-                "section_title": section_title,
-                "background": background,
-                "title": tilte
-            }
+Quac = lazy_builder_import.LazyBuilderImport('quac')
