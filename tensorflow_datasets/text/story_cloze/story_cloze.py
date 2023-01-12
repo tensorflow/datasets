f131fd200e482ebffcae0c24ce890cc28b241f06
Pierre Ruyssen <pierrot@google.com>
2023-01-12 09:29:19 -0800

    Move story_cloze to 'datasets/' directory, using ConfigBasedBuilder.
    PiperOrigin-RevId: 501586727

--- tensorflow_datasets/text/story_cloze/story_cloze.py
+++ tensorflow_datasets/text/story_cloze/story_cloze.py
@@ -13,119 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""story_cloze dataset."""
+"""Dataset definition for story_cloze.
 
-from __future__ import annotations
-
-import csv
-import os
-
-from etils import epath
-import numpy as np
-import tensorflow_datasets.public_api as tfds
-
-_DESCRIPTION = """Story Cloze Test is a new commonsense reasoning framework
-for evaluating story understanding, story generation, and script learning.
-This test requires a system to choose the correct ending to a four-sentence
-story.
+DEPRECATED!
+If you want to use the StoryCloze dataset builder class, use:
+tfds.builder_cls('story_cloze')
 """
 
-_CITATION = """@inproceedings{sharma-etal-2018-tackling,
-    title = "Tackling the Story Ending Biases in The Story Cloze Test",
-    author = "Sharma, Rishi  and
-      Allen, James  and
-      Bakhshandeh, Omid  and
-      Mostafazadeh, Nasrin",
-    booktitle = "Proceedings of the 56th Annual Meeting of the Association for Computational Linguistics (Volume 2: Short Papers)",
-    month = jul,
-    year = "2018",
-    address = "Melbourne, Australia",
-    publisher = "Association for Computational Linguistics",
-    url = "https://www.aclweb.org/anthology/P18-2119",
-    doi = "10.18653/v1/P18-2119",
-    pages = "752--757",
-    abstract = "The Story Cloze Test (SCT) is a recent framework for evaluating story comprehension and script learning. There have been a variety of models tackling the SCT so far. Although the original goal behind the SCT was to require systems to perform deep language understanding and commonsense reasoning for successful narrative understanding, some recent models could perform significantly better than the initial baselines by leveraging human-authorship biases discovered in the SCT dataset. In order to shed some light on this issue, we have performed various data analysis and analyzed a variety of top performing models presented for this task. Given the statistics we have aggregated, we have designed a new crowdsourcing scheme that creates a new SCT dataset, which overcomes some of the biases. We benchmark a few models on the new dataset and show that the top-performing model on the original SCT dataset fails to keep up its performance. Our findings further signify the importance of benchmarking NLP systems on various evolving test sets.",
-}
-"""
+from tensorflow_datasets.core import lazy_builder_import
 
-
-class StoryCloze(tfds.core.GeneratorBasedBuilder):
-  """DatasetBuilder for story_cloze dataset."""
-
-  VERSION = tfds.core.Version('1.0.0')
-  RELEASE_NOTES = {
-      '1.0.0': 'Initial release.',
-  }
-  BUILDER_CONFIGS = [
-      tfds.core.BuilderConfig(name='2016', description='2018 year'),
-      tfds.core.BuilderConfig(name='2018', description='2018 year')
-  ]
-  MANUAL_DOWNLOAD_INSTRUCTIONS = """\
-  Visit https://www.cs.rochester.edu/nlp/rocstories/ and fill out the google
-  form to obtain the datasets. You will receive an email with the link to
-  download the datasets. For the 2016 data, the validation and test file needs
-  to be renamed to cloze_test_val__spring2016.csv and
-  cloze_test_test__spring2016.csv respectively. For 2018 version, the validation
-  and test file needs to be renamed to cloze_test_val__winter2018.csv and
-  to cloze_test_test__winter2018.csv respectively. Move both these files
-  to the manual directory.
-  """
-
-  def _info(self) -> tfds.core.DatasetInfo:
-    """Returns the dataset metadata."""
-    return tfds.core.DatasetInfo(
-        builder=self,
-        description=_DESCRIPTION,
-        features=tfds.features.FeaturesDict({
-            'context': tfds.features.Text(),
-            'endings': tfds.features.Sequence(tfds.features.Text()),
-            'label': np.int32,
-        }),
-        supervised_keys=None,  # e.g. ('image', 'label')
-        homepage='https://www.cs.rochester.edu/nlp/rocstories/',
-        citation=_CITATION,
-    )
-
-  def _split_generators(self, dl_manager: tfds.download.DownloadManager):
-    """Returns SplitGenerators."""
-    if self.builder_config.name == '2016':
-      file_postfix = 'spring2016'
-    elif self.builder_config.name == '2018':
-      file_postfix = 'winter2018'
-
-    val_file = 'cloze_test_val__' + file_postfix + '.csv'
-    test_file = 'cloze_test_test__' + file_postfix + '.csv'
-
-    return [
-        tfds.core.SplitGenerator(
-            name=tfds.Split.VALIDATION,
-            gen_kwargs={
-                'filepath': os.path.join(dl_manager.manual_dir, val_file)
-            },
-        ),
-        tfds.core.SplitGenerator(
-            name=tfds.Split.TEST,
-            gen_kwargs={
-                'filepath': os.path.join(dl_manager.manual_dir, test_file)
-            },
-        ),
-    ]
-
-  def _generate_examples(self, filepath):
-    """Yields examples."""
-    with epath.Path(filepath).open() as f:
-      reader = csv.DictReader(f)
-      for row in reader:
-        context = ' '.join([
-            row['InputSentence1'], row['InputSentence2'], row['InputSentence3'],
-            row['InputSentence4']
-        ])
-        endings = [
-            row['RandomFifthSentenceQuiz1'], row['RandomFifthSentenceQuiz2']
-        ]
-
-        yield row['InputStoryid'], {
-            'context': context,
-            'endings': endings,
-            'label': row.get('AnswerRightEnding', -1),
-        }
+StoryCloze = lazy_builder_import.LazyBuilderImport('story_cloze')
