0db2ff0fa18c5541c662ee03ceaffed3a8802d86
Pierre Ruyssen <pierrot@google.com>
2023-01-17 06:06:47 -0800

    Move wiki_table_questions to 'datasets/' directory, using ConfigBasedBuilder.
    PiperOrigin-RevId: 502564268

--- tensorflow_datasets/question_answering/web_questions.py
+++ tensorflow_datasets/question_answering/web_questions.py
@@ -13,90 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""WebQuestions Benchmark for Question Answering."""
+"""Dataset definition for web_questions.
 
-import json
-import re
-
-from etils import epath
-import tensorflow_datasets.public_api as tfds
-
-_CITATION = """
-@inproceedings{berant-etal-2013-semantic,
-    title = "Semantic Parsing on {F}reebase from Question-Answer Pairs",
-    author = "Berant, Jonathan  and
-      Chou, Andrew  and
-      Frostig, Roy  and
-      Liang, Percy",
-    booktitle = "Proceedings of the 2013 Conference on Empirical Methods in Natural Language Processing",
-    month = oct,
-    year = "2013",
-    address = "Seattle, Washington, USA",
-    publisher = "Association for Computational Linguistics",
-    url = "https://www.aclweb.org/anthology/D13-1160",
-    pages = "1533--1544",
-}
-"""
-_SPLIT_DOWNLOAD_URL = {
-    'train': 'https://worksheets.codalab.org/rest/bundles/0x4a763f8cde224c2da592b75f29e2f5c2/contents/blob/',
-    'test': 'https://worksheets.codalab.org/rest/bundles/0xe7bac352fce7448c9ef238fb0a297ec2/contents/blob/',
-}
-
-_DESCRIPTION = """\
-This dataset consists of 6,642 question/answer pairs.
-The questions are supposed to be answerable by Freebase, a large knowledge graph.
-The questions are mostly centered around a single named entity.
-The questions are popular ones asked on the web (at least in 2013).
+DEPRECATED!
+If you want to use the WebQuestions dataset builder class, use:
+tfds.builder_cls('web_questions')
 """
 
+from tensorflow_datasets.core import lazy_builder_import
 
-class WebQuestions(tfds.core.GeneratorBasedBuilder):
-  """WebQuestions Benchmark for Question Answering."""
-
-  VERSION = tfds.core.Version('1.0.0')
-
-  def _info(self):
-    return tfds.core.DatasetInfo(
-        builder=self,
-        description=_DESCRIPTION,
-        features=tfds.features.FeaturesDict({
-            'url': tfds.features.Text(),
-            'question': tfds.features.Text(),
-            'answers': tfds.features.Sequence(tfds.features.Text()),
-        }),
-        supervised_keys=None,
-        homepage='https://worksheets.codalab.org/worksheets/0xba659fe363cb46e7a505c5b6a774dc8a',
-        citation=_CITATION,
-    )
-
-  def _split_generators(self, dl_manager):
-    """Returns SplitGenerators."""
-    file_paths = dl_manager.download(_SPLIT_DOWNLOAD_URL)
-
-    return [
-        tfds.core.SplitGenerator(
-            name=split, gen_kwargs={'file_path': file_path}
-        )
-        for split, file_path in file_paths.items()
-    ]
-
-  def _generate_examples(self, file_path):
-    """Parses split file and yields examples."""
-
-    def _target_to_answers(target):
-      target = re.sub(r'^\(list |\)$', '', target)
-      return [
-          ''.join(ans)
-          for ans in re.findall(
-              r'\(description (?:"([^"]+?)"|([^)]+?))\)\w*', target
-          )
-      ]
-
-    with epath.Path(file_path).open() as f:
-      examples = json.load(f)
-      for i, ex in enumerate(examples):
-        yield i, {
-            'url': ex['url'],
-            'question': ex['utterance'],
-            'answers': _target_to_answers(ex['targetValue']),
-        }
+WebQuestions = lazy_builder_import.LazyBuilderImport('web_questions')
