91458a5fbd63dd0e65d2cdf0044a6496fe550686
Pierre Ruyssen <pierrot@google.com>
2023-01-17 04:31:56 -0800

    Move tydi_qa to 'datasets/' directory, using ConfigBasedBuilder.
    PiperOrigin-RevId: 502548713

--- tensorflow_datasets/datasets/tydi_qa/__init__.py
+++ tensorflow_datasets/datasets/tydi_qa/__init__.py
@@ -13,13 +13,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Dataset definition for tydi_qa.
-
-DEPRECATED!
-If you want to use the TydiQA dataset builder class, use:
-tfds.builder_cls('tydi_qa')
-"""
-
-from tensorflow_datasets.core import lazy_builder_import
-
-TydiQA = lazy_builder_import.LazyBuilderImport('tydi_qa')
