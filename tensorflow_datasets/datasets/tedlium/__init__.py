e7eb1e6a786522080ff74446955fbb305c9512c8
Pierre Ruyssen <pierrot@google.com>
2023-02-10 05:48:36 -0800

    Move tedlium to 'datasets/' directory, using ConfigBasedBuilder.
    PiperOrigin-RevId: 508637913

--- tensorflow_datasets/datasets/tedlium/__init__.py
+++ tensorflow_datasets/datasets/tedlium/__init__.py
@@ -13,13 +13,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Dataset definition for tedlium.
-
-DEPRECATED!
-If you want to use the Tedlium dataset builder class, use:
-tfds.builder_cls('tedlium')
-"""
-
-from tensorflow_datasets.core import lazy_builder_import
-
-Tedlium = lazy_builder_import.LazyBuilderImport('tedlium')
