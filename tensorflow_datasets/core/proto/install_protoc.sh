1ac291acf8fdb7aac5a6db2aca30778a3e06d99f
Tim Semenov <timsemenov@google.com>
2023-03-30 05:25:34 -0700

    Update protoc compiler
    PiperOrigin-RevId: 520610834

--- tensorflow_datasets/core/proto/install_protoc.sh
+++ tensorflow_datasets/core/proto/install_protoc.sh
@@ -2,10 +2,12 @@
 # Install the .protoc compiler on Linux
 
 # Make sure you grab the latest version
-curl -OL https://github.com/google/protobuf/releases/download/v3.11.2/protoc-3.11.2-linux-x86_64.zip
+curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v22.2/protoc-22.2-linux-x86_64.zip
+curl -OL https://github.com/protocolbuffers/protobuf/archive/refs/tags/v22.2.zip
 
 # Unzip
-unzip protoc-3.11.2-linux-x86_64.zip -d protoc3
+unzip protoc-22.2-linux-x86_64.zip -d protoc3
+unzip v22.2.zip -d protoc3-source
 
 
 # Move protoc to /usr/local/bin/
@@ -13,3 +15,6 @@ sudo mv protoc3/bin/* /usr/local/bin/
 
 # Move protoc3/include to /usr/local/include/
 sudo mv protoc3/include/* /usr/local/include/
+
+# Move source files to /usr/local/include/
+sudo mv -n protoc3-source/protobuf-22.2/src/google/protobuf/* /usr/local/include/google/protobuf/
