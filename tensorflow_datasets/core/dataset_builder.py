8337e420ec4ff6a08122998457d69dfc437210b1
Tom van der Weide <weide@google.com>
2023-04-14 05:26:36 -0700

    add option to read_text_file for the encoding
    PiperOrigin-RevId: 524265720

--- tensorflow_datasets/core/dataset_builder.py
+++ tensorflow_datasets/core/dataset_builder.py
@@ -1551,11 +1551,13 @@ class GeneratorBasedBuilder(FileReaderBuilder):
     split_dict = splits_lib.SplitDict(split_infos)
     self.info.set_splits(split_dict)
 
-  def read_text_file(self, filename: epath.PathLike) -> str:
+  def read_text_file(
+      self, filename: epath.PathLike, encoding: Optional[str] = None
+  ) -> str:
     """Returns the text in the given file and records the lineage."""
     filename = epath.Path(filename)
     self.info.add_file_data_source_access(filename)
-    return filename.read_text()
+    return filename.read_text(encoding=encoding)
 
   def read_tfrecord_as_dataset(
       self, filenames: Union[str, Sequence[str]]
