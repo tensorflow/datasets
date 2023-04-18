3aa8120ec166bd16c77f0742d1a7476bb9719ead
Pierre Marcenac <pierremarcenac@google.com>
2023-04-18 01:43:46 -0700

    Lazy import TensorFlow in tf_compat.py.
    PiperOrigin-RevId: 525078912

--- tensorflow_datasets/core/tf_compat.py
+++ tensorflow_datasets/core/tf_compat.py
@@ -15,56 +15,15 @@
 
 """TensorFlow compatibility utilities."""
 
-# pylint: disable=g-import-not-at-top,g-direct-tensorflow-import
-
-import distutils.version
 import functools
 import os
 from typing import Optional
 
-MIN_TF_VERSION = "2.1.0"
-
-_ensure_tf_version_called = False
-
-
-# Ensure TensorFlow version is sufficiently recent. This needs to happen after
-# TensorFlow was imported, as it is referenced.
-def ensure_tf_version(tf):  # pylint: disable=g-statement-before-imports
-  """Ensures TensorFlow version is sufficient.
-
-  Args:
-    tf: Tensorflow module.
-
-  Raises:
-    ImportError: if tensorflow version is inadequate.
-  """
-  # Only check the first time.
-  global _ensure_tf_version_called
-  if _ensure_tf_version_called:
-    return
-  _ensure_tf_version_called = True
-
-  tf_version = distutils.version.LooseVersion(tf.__version__)
-  min_tf_version = distutils.version.LooseVersion(MIN_TF_VERSION)
-  if tf_version < min_tf_version:
-    raise ImportError(
-        "This version of TensorFlow Datasets requires TensorFlow "
-        f"version >= {MIN_TF_VERSION}; Detected an installation of version "
-        f"{tf.__version__}. Please upgrade TensorFlow to proceed."
-    )
-
-
-def is_dataset(ds):
-  """Whether ds is a Dataset. Compatible across TF versions."""
-  import tensorflow as tf  # pylint: disable=import-outside-toplevel
-
-  return isinstance(ds, (tf.data.Dataset, tf.compat.v1.data.Dataset))
+from tensorflow_datasets.core.utils.lazy_imports_utils import tensorflow as tf
 
 
 def get_single_element(ds):
   """Calls `tf.data.Dataset.get_single_element`."""
-  import tensorflow as tf  # pylint: disable=import-outside-toplevel
-
   if hasattr(ds, "get_single_element"):  # tf 2.6 and above
     return ds.get_single_element()
   else:
@@ -72,8 +31,6 @@ def get_single_element(ds):
 
 
 def _get_option_deterministic_field() -> str:
-  import tensorflow as tf  # pylint: disable=import-outside-toplevel
-
   if hasattr(tf.data.Options(), "deterministic"):
     return "deterministic"
   return "experimental_deterministic"
