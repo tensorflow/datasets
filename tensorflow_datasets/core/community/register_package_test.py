6680793f2cb22d595c8162d77f6aac3a80b978aa
Tom van der Weide <weide@google.com>
2024-01-22 05:19:21 -0800

    update hash of dummy dataset file in test
    PiperOrigin-RevId: 600427143

--- tensorflow_datasets/core/community/register_package_test.py
+++ tensorflow_datasets/core/community/register_package_test.py
@@ -28,7 +28,6 @@ from unittest import mock
 
 from etils import epath
 import pytest
-
 from tensorflow_datasets.core import dataset_builder
 from tensorflow_datasets.core import naming
 from tensorflow_datasets.core import registered
@@ -39,16 +38,14 @@ from tensorflow_datasets.core.community import register_package
 
 
 @contextlib.contextmanager
-def mock_cache_path(new_cache_dir: epath.PathLike) -> Iterator[None]:
+def mock_cache_path(new_cache_dir: epath.Path) -> Iterator[None]:
   """Mock which overwrite the cache path."""
-  new_dir = epath.Path(new_cache_dir)
-
   # Use `__wrapped__` to access the original function wrapped inside
   # `functools.lru_cache`
   new_cache_path = utils.memoize()(cache.cache_path.__wrapped__)
   new_module_path = utils.memoize()(cache.module_path.__wrapped__)
   with mock.patch.object(
-      cache, '_default_cache_dir', return_value=new_dir
+      cache, '_default_cache_dir', return_value=new_cache_dir
   ), mock.patch.object(cache, 'cache_path', new_cache_path), mock.patch.object(
       cache, 'module_path', new_module_path
   ):
@@ -60,7 +57,7 @@ def dummy_register():
   """Dummy register."""
 
   with tempfile.TemporaryDirectory() as tmp_path:
-    tmp_path = pathlib.Path(tmp_path)
+    tmp_path = epath.Path(tmp_path)
 
     source_path = utils.tfds_path() / 'testing/dummy_dataset/dummy_dataset.py'
 
@@ -76,13 +73,11 @@ def dummy_register():
 
     # Create the remote index content
     # Note the absence of `"` for the `src_multi_json` as it is parsed as `dict`
-    content = textwrap.dedent(
-        f"""\
+    content = textwrap.dedent(f"""\
         {{"name": "kaggle:dummy_dataset", "source": "{src_single.to_json()}"}}
         {{"name": "kaggle:ds1", "source": "{src_single.to_json()}"}}
         {{"name": "mlds:dummy_dataset", "source": {src_multi_json}}}
-        """
-    )
+        """)
     dummy_path = tmp_path / 'dummy-community-datasets.toml'
     dummy_path.write_text(content)
 
@@ -90,7 +85,9 @@ def dummy_register():
       yield register_package.PackageRegister(path=dummy_path)
 
 
-def test_list_dataset_references(dummy_register):  # pylint: disable=redefined-outer-name
+def test_list_dataset_references(
+    dummy_register: register_package.PackageRegister,
+):  # pylint: disable=redefined-outer-name
   assert sorted(dummy_register.list_dataset_references()) == [
       naming.DatasetReference(dataset_name='ds1', namespace='kaggle'),
       naming.DatasetReference(dataset_name='dummy_dataset', namespace='kaggle'),
@@ -98,7 +95,7 @@ def test_list_dataset_references(dummy_register):  # pylint: disable=redefined-o
   ]
 
 
-def test_builder_cls(dummy_register):  # pylint: disable=redefined-outer-name
+def test_builder_cls(dummy_register: register_package.PackageRegister):  # pylint: disable=redefined-outer-name
   # The dataset will be installed in the cache
   installed_path = cache.cache_path()
   installed_path /= 'modules/tfds_community/kaggle/dummy_dataset'
@@ -108,7 +105,7 @@ def test_builder_cls(dummy_register):  # pylint: disable=redefined-outer-name
   builder_cls = dummy_register.builder_cls(ds_name)
   assert builder_cls.name == 'dummy_dataset'
 
-  clshash = 'd183643ab2f6544350b6871f6cc2b0fba07297d8d30f4c04649a7717e44e531b'
+  clshash = 'd66db99e4dbd05066b84b97ff9528f404fc71e61d556add952a67416e70a1786'
   assert installed_path / f'{clshash}/dummy_dataset.py' == builder_cls.code_path
   assert 'kaggle' in builder_cls.code_path.parts
   assert issubclass(builder_cls, dataset_builder.DatasetBuilder)
@@ -167,7 +164,7 @@ def test_dataset_package():
 
 
 def test_mock_cache_path(tmp_path: pathlib.Path):
-  with mock_cache_path(tmp_path):
+  with mock_cache_path(epath.Path(tmp_path)):
     assert os.fspath(tmp_path) not in sys.path
     assert cache.cache_path() == tmp_path
     assert cache.module_path() == tmp_path / 'modules'
