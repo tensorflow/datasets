6aa3e10b4d8a11c7185dbc224b65e54d8489e71b
Emil Njor <emil.njor@gmail.com>
2024-05-14 15:18:22 +0000

    updated split naming in test file

--- tensorflow_datasets/image_classification/wake_vision/wake_vision_dataset_builder_test.py
+++ tensorflow_datasets/image_classification/wake_vision/wake_vision_dataset_builder_test.py
@@ -7,8 +7,8 @@ class WakeVisionTest(tfds.testing.DatasetBuilderTestCase):
   """Tests for wake_vision dataset."""
   DATASET_CLASS = wake_vision_dataset_builder.Builder
   SPLITS = {
-      'train_image': 16,  # Number of fake train image examples
-      'train_bbox': 4,  # Number of fake train bbox examples
+      'train_large': 16,  # Number of fake train image examples
+      'train_quality': 4,  # Number of fake train bbox examples
       'validation': 11,  # Number of fake validation examples
       'test': 10,  # Number of fake test examples
   }
@@ -17,8 +17,8 @@ class WakeVisionTest(tfds.testing.DatasetBuilderTestCase):
       'train_images': ['wake-vision-train-dummy-1.tar.gz', 'wake-vision-train-dummy-2.tar.gz'],
       'validation_images': ['wake-vision-validation-dummy.tar.gz'],
       'test_images': ['wake-vision-test-dummy.tar.gz'],
-      'train_image_metadata': 'wake_vision_train_image.csv',
-      'train_bbox_metadata': 'wake_vision_train_bbox.csv',
+      'train_large_metadata': 'wake_vision_train_image.csv',
+      'train_quality_metadata': 'wake_vision_train_bbox.csv',
       'validation_metadata': 'wake_vision_validation.csv',
       'test_metadata': 'wake_vision_test.csv',
   }
