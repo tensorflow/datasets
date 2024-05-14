591026b0207c4576d4efc293f5bf31680bc76b2d
Emil Njor <emil.njor@gmail.com>
2024-05-14 15:30:42 +0000

    slight change to readme and info text to clarify licenses

--- tensorflow_datasets/image_classification/wake_vision/wake_vision_dataset_builder.py
+++ tensorflow_datasets/image_classification/wake_vision/wake_vision_dataset_builder.py
@@ -45,9 +45,8 @@ class Builder(tfds.core.GeneratorBasedBuilder):
     """Returns the dataset metadata."""
     return self.dataset_info_from_configs(
         description=
-        """
-        "Wake Vision" is a large, high-quality dataset featuring over 6 million images, significantly exceeding the scale and diversity of current tinyML datasets (100x). This dataset includes images with annotations of whether each image contains a person. Additionally, it incorporates a comprehensive fine-grained benchmark to assess fairness and robustness, covering perceived gender, perceived age, subject distance, lighting conditions, and depictions. Hosted on Harvard Dataverse, it provides images, CSV files, and code to generate a Wake Vision TensorFlow Dataset. Annotations are published under a CC BY 4.0 license, and all images are sourced from the Open Images v7 dataset under a CC BY 2.0 license.
-        """
+        """Wake Vision" is a large, high-quality dataset featuring over 6 million images, significantly exceeding the scale and diversity of current tinyML datasets (100x). This dataset includes images with annotations of whether each image contains a person. Additionally, it incorporates a comprehensive fine-grained benchmark to assess fairness and robustness, covering perceived gender, perceived age, subject distance, lighting conditions, and depictions.
+The Wake Vision labels are derived from Open Image's annotations which are licensed by Google LLC under CC BY 4.0 license. The images are listed as having a CC BY 2.0 license. Note from Open Images: "while we tried to identify images that are licensed under a Creative Commons Attribution license, we make no representations or warranties regarding the license status of each image and you should verify the license for each image yourself."""
         ,
         features=tfds.features.FeaturesDict({
             # These are the features of your dataset like images, labels ...
