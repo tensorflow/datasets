44813448d533bfc5ef5617b8f0092bd7daa702b9
Sanjay Surendranath Girija <sanjay4victory@gmail.com>
2025-06-29 15:37:40 -0700

    Fix formatting

--- tensorflow_datasets/image_classification/caltech_birds.py
+++ tensorflow_datasets/image_classification/caltech_birds.py
@@ -234,7 +234,7 @@ class CaltechBirds2011(CaltechBirds2010):
     return CaltechBirdsInfo2011(
         name=self.name,
         images_url="https://data.caltech.edu/records/65de6-vp158/files/CUB_200_2011.tgz?download=1",
-        segmentations_url="https://data.caltech.edu/records/w9d68-gec53/files/segmentations.tgz?download=1"
+        segmentations_url="https://data.caltech.edu/records/w9d68-gec53/files/segmentations.tgz?download=1",
         # images_url="https://drive.google.com/uc?export=download&id=1hbzc_P1FuxMkcabkgn9ZKinBwW683j45",
         # split_url=None,
         # annotations_url="https://drive.google.com/uc?export=download&id=1EamOKGLoTuZdtcVYbHMWNpkn3iAVj8TP",
@@ -259,15 +259,13 @@ class CaltechBirds2011(CaltechBirds2010):
     )
 
   def _split_generators(self, dl_manager):
-    download_path = dl_manager.download(
-        [
-            self._caltech_birds_info.images_url,
-        ]
-    )
+    download_path = dl_manager.download([
+        self._caltech_birds_info.images_url,
+    ])
 
     extracted_path = dl_manager.download_and_extract([
         self._caltech_birds_info.images_url,
-        self._caltech_birds_info.segmentations_url
+        self._caltech_birds_info.segmentations_url,
     ])
 
     image_names_path = os.path.join(
